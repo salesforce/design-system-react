b4f3a58b247b44613ee01cee9f642f7b99d6c57a
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-07-09 14:14:19 +0200

    Refactor React code to use the core library

--- src/React/sample.js
+++ src/React/sample.js
@@ -1,6 +1,12 @@
+var React = require('react');
 import {Selectlist} from "./selectlist";
 
-var collection = [];
+var collection = [
+		{ id: 1, name: 'tacos', type: 'mexican' },
+		{ id: 2, name: 'burrito', type: 'mexican' },
+		{ id: 3, name: 'tostada', type: 'mexican' },
+		{ id: 4, name: 'hush puppies', type: 'southern' }
+	];
 var options = {};
-var selectlist = new Selectlist(collection, options);
-selectlist.Landmark.log('Running version ' + selectlist.Landmark.version);
+var Samplelist = new Selectlist(collection, options);
+React.render(<Samplelist.component/>, document.body, () => {});
--- src/React/selectlist.js
+++ src/React/selectlist.js
@@ -1,3 +1,20 @@
-import {SelectlistCore} from "../SelectlistCore";
+var React = require('react');
+import {SelectlistCore} from '../SelectlistCore';
 
-export class Selectlist extends SelectlistCore {};
+export class Selectlist extends SelectlistCore {
+	onBeforeInitialize (collection, options) {
+		var menuItems = collection.map((menuItem) => {
+			return <li>{menuItem.name}</li>; 
+		});
+		
+		this.component = React.createClass({
+			render () {
+				return (
+					<div>
+						<ul>{menuItems}</ul>
+					</div>
+				);
+			}
+		});
+	}
+}
