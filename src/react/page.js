206f231405f57507b420568c32d84245d8747a32
Gary Borton <gborton@exacttarget.com>
2015-07-28 11:28:48 -0400

    Remove dependency on _ from everything but the backbone version.

--- src/react/page.js
+++ src/react/page.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import _ from 'underscore';
 import {Selectlist} from './selectlist';
 
 var Page = React.createClass({
 	changeCollection () {
 		var models = this.props.models;
-		
-		_.each(models, function (selectlist) {
-			selectlist.disabled = !selectlist.disabled;
+		Object.keys(models).forEach(key => {
+			models[key].disabled = !models[key].disabled;
 		});
-		
+
 		this.setProps({
 			models: models
 		});
 	},
-	
+
 	getSelectionHandler: function (model) {
 		return function (selection) {
 			model.selection = selection;
@@ -26,19 +24,19 @@ var Page = React.createClass({
 		this.props.models.selectlist2.onSelected = this.getSelectionHandler(this.props.models.selectlist2);
 		this.props.models.selectlist3.onSelected = this.getSelectionHandler(this.props.models.selectlist3);
 		this.props.models.selectlist4.onSelected = this.getSelectionHandler(this.props.models.selectlist4);
-		
+
 		return (
 			<div>
 				<ul className="selectlist-examples">
 					<li>{React.createElement(Selectlist, this.props.models.selectlist1)}</li>
-					
+
 					<li>{React.createElement(Selectlist, this.props.models.selectlist2)}</li>
-					
+
 					<li>{React.createElement(Selectlist, this.props.models.selectlist3)}</li>
-					
+
 					<li>{React.createElement(Selectlist, this.props.models.selectlist4)}</li>
 				</ul>
-				
+
 				<button className="selectlist-action btn btn-primary" onClick={this.changeCollection}>Toggle Enabled / Disabled</button>
 			</div>
 		);
