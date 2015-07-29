ec7d03c3bebce5dbde007c1b577da48da9cb6c98
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-07-29 12:27:26 -0400

    Update the jQuery code to use the core library

--- src/jquery/selectlist.js
+++ src/jquery/selectlist.js
@@ -3,256 +3,103 @@ import SelectlistCore from '../core/selectlist';
 import $ from 'jquery';
 
 var old = $.fn.selectlist;
-var collection = {};
-var wrapper;
 
-// TO-DO: This is basically the FuelUX declarative-compatibility version. What I want to do is render from a collection and template when directly instantiated but still support that old declarative syntax. This will take some readjustment and an overloaded constructor
-
-var old = $.fn.selectlist;
 // SELECT CONSTRUCTOR AND PROTOTYPE
 
 var Selectlist = function (element, options) {
-	// define for instance, it would great if this didn't have to happen.
-	this.Landmark = Landmark;
-
-	this.$element = $(element);
 	this.options = $.extend({}, $.fn.selectlist.defaults, options);
-	this._collection = [];
-
-	if( Landmark.isFunction(this.options.dataSource) ) {
-		this.populate();
+	this.elements = {
+		wrapper: $(element)
+	};
+	
+	if (options.collection) {
+		this.rendered = false;
+	} else {
+		this.rendered = true;
+		// TO-DO: Build options.collection from the HTML
 	}
-
-	this.$button = this.$element.find('.btn.dropdown-toggle');
-	this.$hiddenField = this.$element.find('.hidden-field');
-	this.$label = this.$element.find('.selected-label');
-	this.$dropdownMenu = this.$element.find('.dropdown-menu');
-
-	this.$element.on('click.fu.selectlist', '.dropdown-menu a', $.proxy(this.itemClicked, this));
-	this.setDefaultSelection();
-
-	if (options.resize === 'auto' || this.$element.attr('data-resize') === 'auto') {
-		this.resize();
-	}
-
-	// if selectlist is empty or is one item, disable it
-	var items = this.$dropdownMenu.children('li');
-	if( items.length === 0) {
-		this.disable();
-		this.doSelect( $(this.options.emptyLabelHTML));
-	}
-
-	// support jumping focus to first letter in dropdown when key is pressed
-	this.$element.on('shown.bs.dropdown', function () {
-			var $this = $(this);
-			// attach key listener when dropdown is shown
-			$(document).on('keypress.fu.selectlist', function(e){
-
-				// get the key that was pressed
-				var key = String.fromCharCode(e.which);
-				// look the items to find the first item with the first character match and set focus
-				$this.find('li').each(function(idx,item){
-					if ($(item).text().charAt(0).toLowerCase() === key) {
-						$(item).children('a').focus();
-						return false;
-					}
-				});
-
-		});
-	});
-
-	// unbind key event when dropdown is hidden
-	this.$element.on('hide.bs.dropdown', function () {
-			$(document).off('keypress.fu.selectlist');
-	});
+	
+	this.__constructor(options);
 };
 
-Selectlist.prototype = Object.assign({}, SelectlistCore, {
-	constructor: Selectlist,
-
-	destroy: function () {
-		this.$element.remove();
-		// any external bindings
-		// [none]
-		// empty elements to return to original markup
-		// [none]
-		// returns string of markup
-		return this.$element[0].outerHTML;
+Object.assign(Selectlist.prototype, SelectlistCore, {
+	destroy () {
+		this.elements.wrapper.remove();
+		return this.elements.wrapper[0].outerHTML;
 	},
 
-	doSelect: function ($item) {
-		var $selectedItem;
-		this.$selectedItem = $selectedItem = $item;
-
-		this.$hiddenField.val(this.$selectedItem.attr('data-value'));
-		this.$label.html($(this.$selectedItem.children()[0]).html());
-
-		// clear and set selected item to allow declarative init state
-		// unlike other controls, selectlist's value is stored internal, not in an input
-		this.$element.find('li').each(function () {
-			if ($selectedItem.is($(this))) {
-				$(this).attr('data-selected', true);
-			} else {
-				$(this).removeData('selected').removeAttr('data-selected');
-			}
-		});
-	},
-
-	itemClicked: function (e) {
-		this.$element.trigger('clicked.fu.selectlist', this.$selectedItem);
-
+	itemClicked (e) {
 		e.preventDefault();
-		// ignore if a disabled item is clicked
-		if ($(e.currentTarget).parent('li').is('.disabled, :disabled')) { return; }
+		
+		var $li = $(e.currentTarget).parent('li');
+		
+		// Ignore if a disabled item is clicked
+		// Note that this feature isn't implemented in any other library yet
+		if ($li.is('.disabled, :disabled')) { return; }
 
-		// is clicked element different from currently selected element?
-		if (!($(e.target).parent().is(this.$selectedItem))) {
-			this.itemChanged(e);
+		this.setSelection($li.data());
+
+		// Return focus to control after selecting an option
+		this.elements.wrapper.find('.dropdown-toggle').focus();
+	},
+
+	onSelected (data) {
+		if (!this.rendered) return;
+		
+		this.elements.hiddenField.val(JSON.stringify(data));
+		this.elements.label.text(data.name);
+		
+		this.elements.wrapper.trigger('changed.fu.selectlist', data);
+	},
+
+	onInitialized (options) {
+		if (!this.rendered) {
+			this.render();
 		}
-
-		// return focus to control after selecting an option
-		this.$element.find('.dropdown-toggle').focus();
+		
+		// TO-DO: These should all be in the cssClasses of course
+		this.elements.button = this.elements.wrapper.find('.btn.dropdown-toggle');
+		this.elements.hiddenField = this.elements.wrapper.find('.hidden-field');
+		this.elements.label = this.elements.wrapper.find('.selected-label');
+		this.elements.dropdownMenu = this.elements.wrapper.find('.dropdown-menu');
+	
+		this.elements.wrapper.on('click.fu.selectlist', '.dropdown-menu a', $.proxy(this.itemClicked, this));
 	},
+	
+	render () {
+		// Prep for append
+		this.elements.wrapper.empty();
+		this.elements.wrapper.toggleClass(this.cssClasses.CONTROL, true);
+		this.elements.wrapper.toggleClass(this.cssClasses.BTN_GROUP, true);
+		
+		// TO-DO: Actually render this HTML from a template
+		var html = '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button">' +
+					'			<span class="selected-label">&nbsp;</span>' +
+					'			<span class="caret"></span>' +
+					'			<span class="sr-only">Toggle Dropdown</span>' +
+					'		</button>' +
+					'		<ul class="dropdown-menu" role="menu">' +
+					'			<li data-value="1"><a href="#">One</a>' +
+					'			</li>' +
+					'			<li data-value="2"><a href="#">Two</a>' +
+					'			</li>' +
+					'			<li data-selected="true" data-value="3"><a href="#">Three</a>' +
+					'			</li>' +
+					'			<li data-value="4"><a href="#">Buzz</a>' +
+					'			</li>' +
+					'			<li data-value="Item Five" data-foo="bar" data-fizz="buzz"><a href="#">Item Five</a>' +
+					'			</li>' +
+					'			<li class="disabled" disabled="disabled" data-value="disabled"><a href="#">Disabled item</a>' +
+					'			</li>' +
+					'		</ul>' +
+					'		<input class="hidden hidden-field" name="mySelectlist" readonly="readonly" aria-hidden="true" type="text" />';
 
-	itemChanged: function (e) {
-		//selectedItem needs to be <li> since the data is stored there, not in <a>
-		this.doSelect($(e.target).closest('li'));
-
-		// pass object including text and any data-attributes
-		// to onchange event
-		var data = this.selectedItem();
-		// trigger changed event
-		this.$element.trigger('changed.fu.selectlist', data);
-	},
-
-	populate: function () {
-		var self = this;
-		var selectlistData = this.$element.data();
-
-		// prep for append
-		this.$element.empty();
-		this.$element.addClass('btn-group');
-
-		this.options.dataSource(selectlistData ? selectlistData : {}, function (items) {
-			self._collection = items;
-			// temporary placeholder, need to build items out
-			var html = '<button class="btn btn-default dropdown-toggle" data-toggle="dropdown" type="button">' +
-						'			<span class="selected-label">&nbsp;</span>' +
-						'			<span class="caret"></span>' +
-						'			<span class="sr-only">Toggle Dropdown</span>' +
-						'		</button>' +
-						'		<ul class="dropdown-menu" role="menu">' +
-						'			<li data-value="1"><a href="#">One</a>' +
-						'			</li>' +
-						'			<li data-value="2"><a href="#">Two</a>' +
-						'			</li>' +
-						'			<li data-selected="true" data-value="3"><a href="#">Three</a>' +
-						'			</li>' +
-						'			<li data-value="4"><a href="#">Buzz</a>' +
-						'			</li>' +
-						'			<li data-value="Item Five" data-foo="bar" data-fizz="buzz"><a href="#">Item Five</a>' +
-						'			</li>' +
-						'			<li class="disabled" disabled="disabled" data-value="disabled"><a href="#">Disabled item</a>' +
-						'			</li>' +
-						'		</ul>' +
-						'		<input class="hidden hidden-field" name="mySelectlist" readonly="readonly" aria-hidden="true" type="text" />';
-			self.$element.append(html);
-		});
-	},
-
-	resize: function () {
-		var width = 0;
-		var newWidth = 0;
-		var sizer = $('<div/>').addClass('selectlist-sizer');
-
-
-		if (Boolean($(document).find('html').hasClass('fuelux'))) {
-			// default behavior for fuel ux setup. means fuelux was a class on the html tag
-			$(document.body).append(sizer);
-		} else {
-			// fuelux is not a class on the html tag. So we'll look for the first one we find so the correct styles get applied to the sizer
-			$('.fuelux:first').append(sizer);
-		}
-
-		sizer.append(this.$element.clone());
-
-		this.$element.find('a').each(function () {
-			sizer.find('.selected-label').text($(this).text());
-			newWidth = sizer.find('.selectlist').outerWidth();
-			newWidth = newWidth + sizer.find('.sr-only').outerWidth();
-			if (newWidth > width) {
-				width = newWidth;
-			}
-		});
-
-		if (width <= 1) {
-			return;
-		}
-
-		this.$button.css('width', width);
-		this.$dropdownMenu.css('width', width);
-
-		sizer.remove();
-	},
-
-	selectedItem: function () {
-		var txt = this.$selectedItem.text();
-		return $.extend({
-			text: txt
-		}, this.$selectedItem.data());
-	},
-
-	selectByText: function (text) {
-		var $item = $([]);
-		this.$element.find('li').each(function () {
-			if ((this.textContent || this.innerText || $(this).text() || '').toLowerCase() === (text || '').toLowerCase()) {
-				$item = $(this);
-				return false;
-			}
-		});
-		this.doSelect($item);
-	},
-
-	selectByValue: function (value) {
-		var selector = 'li[data-value="' + value + '"]';
-		this.selectBySelector(selector);
-	},
-
-	selectByIndex: function (index) {
-		// zero-based index
-		var selector = 'li:eq(' + index + ')';
-		this.selectBySelector(selector);
-	},
-
-	selectBySelector: function (selector) {
-		var $item = this.$element.find(selector);
-		this.doSelect($item);
-	},
-
-	setDefaultSelection: function () {
-		var $item = this.$element.find('li[data-selected=true]').eq(0);
-
-		if ($item.length === 0) {
-			$item = this.$element.find('li').has('a').eq(0);
-		}
-
-		this.doSelect($item);
-	},
-
-	enable: function () {
-		this.$element.removeClass('disabled');
-		this.$button.removeClass('disabled');
-	},
-
-	disable: function () {
-		this.$element.addClass('disabled');
-		this.$button.addClass('disabled');
+		this.elements.wrapper.append(html);
+		this.rendered = true;
 	}
 });
 
 // SELECT PLUGIN DEFINITION
-
-
 
 $.fn.selectlist = function (option) {
 	var args = Array.prototype.slice.call(arguments, 1);
@@ -262,13 +109,13 @@ $.fn.selectlist = function (option) {
 		var $this = $(this);
 		var data = $this.data('fu.selectlist');
 
-		// if object, this is an initialization, only overwrite options and init if no data exists
+		// If object, this is an initialization, only overwrite options and init if no data exists
 		var options = typeof option === 'object' && option;
 		if (!data) {
 			$this.data('fu.selectlist', (data = new Selectlist(this, options)));
 		}
 
-		// if string, this is a method cal, and apply with args
+		// If string, this is a method call, and apply with args
 		if (typeof option === 'string') {
 			methodReturn = data[option].apply(data, args);
 		}
@@ -277,6 +124,7 @@ $.fn.selectlist = function (option) {
 	return (methodReturn === undefined) ? $set : methodReturn;
 };
 
+// TO-DO: Should this really be here?
 $.fn.selectlist.defaults = {
 	emptyLabelHTML: '<li data-value=""><a href="#">No items</a></li>'
 };
