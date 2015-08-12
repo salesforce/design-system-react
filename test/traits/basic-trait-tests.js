c6563d3ebca74ca28ef71d5f1ebca132f4f3842d
9299a9d26df283b611e5495f4111c6547c1975c2 <jchen@exacttarget.com>
2015-08-12 11:39:43 -0400

    started base tests

--- test/traits/basic-trait-tests.js
+++ test/traits/basic-trait-tests.js
@@ -2,15 +2,34 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-import disableable from '../../src/traits/disableable';
-import selectable from '../../src/traits/selectable';
-
 describe('FuelUX Facade basic trait tests', function () {
+    var disableable = require('../../src/traits/disableable');
+    var selectable = require('../../src/traits/selectable');
+    // create a new selectable object, and then add a _collection member to it
+    // so we can test the selection options.
+    selectable._collection = [{
+        title: 'Slaughterhouse Five',
+        author: 'Kurt Vonnegut'
+    }, {
+        title: 'The Stand',
+        author: 'Steven King'
+    }, {
+        title: 'On The Road',
+        author: 'Jack Kerouac'
+    }];
+
     it('can require disableable, and it is defined', function () {
         assert.equal(!!disableable, true);
     });
 
     it('can require selectable, and it is defined', function () {
         assert.equal(!!selectable, true);
+    });
+
+    it('returns the proper item at a given index', function () {
+        assert.ok(selectable._collection.every(function (item, index) {
+            selectable.setSelectionByIndex(index);
+            return selectable.getSelection() === item;
+        }));
     });
 });
