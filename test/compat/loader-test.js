613a03fb12fca03c7da282ca53962650bbab754f
Keenan Jaenicke <kjaenicke@exacttarget.com>
2015-08-12 16:39:59 -0400

    fix linting errors for loader tests

--- test/compat/loader-test.js
+++ test/compat/loader-test.js
@@ -1,74 +1,74 @@
-var fs = require('fs');
-var $ = require('jquery');
+const fs = require('fs');
+const $ = require('jquery');
 window.$ = window.jQuery = $;
 
-var html = fs.readFileSync('test/compat/markup/loader-markup.html', 'utf8');
-var chai = require('chai');
-var assert = chai.assert;
+let html = fs.readFileSync('test/compat/markup/loader-markup.html', 'utf8');
+const chai = require('chai');
+const assert = chai.assert;
 
 require('../../src/jquery/loader/loader');
 
 /* FOR DEV TESTING */
-//var html = require('text!dev.html!strip');
-html = $('<div>'+html+'</div>');
-var $testFixture = $('body #test-fixture');
+// var html = require('text!dev.html!strip');
+html = $('<div>' + html + '</div>');
+const $testFixture = $('body #test-fixture');
 $testFixture.append(html);
 
 describe('FuelUX Loader', function () {
 	after(function () {
-		$testFixture.find(".loader").remove();
+		$testFixture.find('.loader').remove();
 	});
 
-	it("should be defined on jquery object", function () {
+	it('should be defined on jquery object', function () {
 		assert.ok($().loader, 'loader method is defined');
 	});
 
 	it('should return an element', function () {
-		var $loader = $(html).find('.loader').loader();
+		const $loader = $(html).find('.loader').loader();
 		assert.ok($loader.loader() === $loader, 'loader is initialized');
 	});
 
-	it('should play on init', function(){
-		var $loader = $(html).find('.loader').loader();
+	it('should play on init', function () {
+		const $loader = $(html).find('.loader').loader();
 
 		$loader.loader();
-		setTimeout(function(){
+		setTimeout(function () {
 			start();
-			assert.ok($loader.attr('data-frame')!==1, 'loader playing on init');
+			assert.ok($loader.attr('data-frame') !== 1, 'loader playing on init');
 			done();
 		}, 160);
 	});
 
-	it('pause should function as expected', function(){
-		var $loader = $(html).find('.loader').loader();
+	it('pause should function as expected', function () {
+		const $loader = $(html).find('.loader').loader();
 
 		$loader.loader();
 		$loader.loader('pause');
-		setTimeout(function(){
+		setTimeout(function () {
 			start();
 			assert.equal($loader.attr('data-frame'), 1, 'pause halts frame progression');
 			done();
 		}, 160);
 	});
 
-	it('play should function as expected', function(){
-		var $loader = $(html).find('.loader').loader();
+	it('play should function as expected', function () {
+		const $loader = $(html).find('.loader').loader();
 
 		$loader.loader();
 		$loader.loader('pause');
 		$loader.loader('play');
-		setTimeout(function(){
+		setTimeout(function () {
 			start();
-			assert.ok($loader.attr('data-frame')!==1, 'play continues frame progression');
+			assert.ok($loader.attr('data-frame') !== 1, 'play continues frame progression');
 			done();
 		}, 160);
 	});
 
-	it('reset should function as expected', function(){
-		var $loader = $(html).find('.loader').loader();
+	it('reset should function as expected', function () {
+		const $loader = $(html).find('.loader').loader();
 
 		$loader.loader();
-		setTimeout(function(){
+		setTimeout(function () {
 			start();
 			$loader.loader('reset');
 			assert.equal($loader.attr('data-frame'), 1, 'reset reverts frame to beginning');
@@ -76,8 +76,8 @@ describe('FuelUX Loader', function () {
 		}, 160);
 	});
 
-	it('next should function as expected', function(){
-		var $loader = $(html).find('.loader').loader();
+	it('next should function as expected', function () {
+		const $loader = $(html).find('.loader').loader();
 
 		$loader.loader();
 		$loader.loader('pause');
@@ -86,24 +86,24 @@ describe('FuelUX Loader', function () {
 		assert.equal($loader.attr('data-frame'), 2, 'next increments frame by 1');
 	});
 
-	it('prev should function as expected', function(){
-		var $loader = $(html).find('.loader').loader();
+	it('prev should function as expected', function () {
+		const $loader = $(html).find('.loader').loader();
 
 		$loader.loader();
 		$loader.loader('pause');
 		$loader.loader('next');
 
-		var v = $loader.attr('data-frame')
+		const v = $loader.attr('data-frame');
 
 		$loader.loader('previous');
 
-		assert.equal($loader.attr('data-frame'), parseInt(v-1, 10), 'prev decrements frame by 1');
+		assert.equal($loader.attr('data-frame'), parseInt(v - 1, 10), 'prev decrements frame by 1');
 	});
 
-	it("should destroy control", function () {
-		var $el = $(html);
+	it('should destroy control', function () {
+		const $el = $(html);
 
-		assert.equal(typeof( $el.loader('destroy')) , 'string', 'returns string (markup)');
+		assert.equal(typeof( $el.loader('destroy')), 'string', 'returns string (markup)');
 		assert.equal( $el.parent().length, false, 'control has been removed from DOM');
 	});
 });
