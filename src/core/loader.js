6c166eb1ee18d782ef6a68447de426e4547822e6
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-08-12 14:45:16 -0400

    Loader consistency wip

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,4 +1,6 @@
-import Lib from './lib';
+// LOADER CONTROL
+
+import * as Lib from './lib';
 import Base from './base';
 
 export const CONTROL = 'loader';
@@ -6,6 +8,20 @@ export const CONTROL = 'loader';
 const LoaderCore = Lib.extend({}, Base, {
 	cssClasses: {
 		CONTROL: CONTROL
+	},
+	
+	// Set the defaults
+	__getInitialState () {
+		return {
+			begin: 1,
+			delay: 150,
+			end: 8,
+			fame: 1
+		};
+	},
+	
+	__initializeOptions (options) {
+		this.__setState(options);
 	}
 });
 
--- src/jquery/loader/example.js
+++ src/jquery/loader/example.js
@@ -1,4 +1,4 @@
-import Lib from '../../core/lib';
+import * as Lib from '../../core/lib';
 import Loader from './loader';
 
 // TO-DO: This might not work with require, need to confirm that it does
@@ -12,19 +12,19 @@ const exampleOptions = [
 
 $(function () {
 	// Declarative
+	const loaderDataAttrs = $('.loader-data-attrs').loader();
+	
+	// Imperative
 	$('.declarative .loader1').loader();
 	$('.declarative .loader2').loader(exampleOptions[0]);
 	$('.declarative .loader3').loader(exampleOptions[1]);
 	$('.declarative .loader4').loader(exampleOptions[2]);
 
-	// Imperative
+	// New syntax
 	const loader1 = new Loader($('.imperative .loader1'));
 	const loader2 = new Loader($('.imperative .loader2'), exampleOptions[0]);
 	const loader3 = new Loader($('.imperative .loader3'), exampleOptions[1]);
 	const loader4 = new Loader($('.imperative .loader4'), exampleOptions[2]);
-
-	// Data Attrs
-	const loaderDataAttrs = new Loader($('.loader-data-attrs'));
 
 	void(loader1);
 	void(loader2);
