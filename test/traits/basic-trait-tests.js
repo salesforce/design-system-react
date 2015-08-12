4b4fbe65889c51c706b0eac83f5059f82c5d9bdb
9299a9d26df283b611e5495f4111c6547c1975c2 <jchen@exacttarget.com>
2015-08-12 09:42:11 -0400

    housekeeping

--- test/traits/basic-trait-tests.js
+++ test/traits/basic-trait-tests.js
@@ -2,14 +2,15 @@
 var chai = require('chai');
 var assert = chai.assert;
 
+import disableable from '../../src/traits/disableable';
+import selectable from '../../src/traits/selectable';
+
 describe('FuelUX Facade basic trait tests', function () {
     it('can require disableable, and it is defined', function () {
-        var disableable = require('../../src/traits/disableable');
         assert.equal(!!disableable, true);
     });
 
     it('can require selectable, and it is defined', function () {
-        var selectable = require('../../src/traits/selectable');
         assert.equal(!!selectable, true);
     });
 });
