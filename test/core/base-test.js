22a98755960e9c9d5386f633ff225f472b4afb35
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-08-13 16:48:14 -0400

    Remove tests which are no longer valid

--- test/core/base-test.js
+++ test/core/base-test.js
@@ -7,18 +7,4 @@ describe('FuelUX Facade Base Module tests', function () {
 	it('can be required', function () {
 		assert.ok(!!Base);
 	});
-
-	it('is able to save and retrieve its state', function () {
-		const state = {
-			isReady: false,
-			name: 'base-test'
-		};
-		Base.__constructor();
-		// need to monkey patch the state here, since the base doesn't have a getinitialstate
-		Base._state = {};
-		Base.__setState(state);
-		assert.ok(Object.keys(state).every(function (member) {
-			return Base.__getState(member) === state[member];
-		}));
-	});
 });
