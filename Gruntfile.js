527533e2031d5ab664fa1702f7a59944e16906e6
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-08-15 10:12:49 -0400

    Add lodash and try (but fail) to speed up build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,32 +5,6 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		port: defaultPort,
-		browserify: {
-			examples: {
-				options: {
-					transform: [['babelify', {
-						'stage': 0,
-						'modules': 'umd'
-					}], ['brfs'], ['browserify-versionify']]
-				},
-				files: {
-					'examples/jquery/examples.js': 'src/jquery/examples.js',
-					'examples/marionette/examples.js': 'src/marionette/examples.js',
-					'examples/react/examples.js': 'src/react/examples.js'
-				}
-			},
-			tests: {
-				options: {
-					transform: [['babelify', {
-						'stage': 0,
-						'modules': 'umd'
-					}], ['brfs'], ['browserify-versionify']]
-				},
-				files: {
-					'test/tests-compiled.js': 'test/tests.js'
-				}
-			}
-		},
 		eslint: {
 			target: [
 				'Gruntfile.js',
@@ -39,6 +13,35 @@ module.exports = function (grunt) {
 				'!test/tests-compiled.js',
 				'!test/tests.js'
 			]
+		},
+		browserify: {
+			options: {
+				transform: [['babelify', {
+					'stage': 0,
+					'modules': 'umd'
+				}], ['brfs'], ['browserify-versionify']],
+				watch: true
+			},
+			jqueryExamples: {
+				files: {
+					'examples/jquery/examples.js': 'src/jquery/examples.js'
+				}
+			},
+			marionetteExamples: {
+				files: {
+					'examples/marionette/examples.js': 'src/marionette/examples.js'
+				}
+			},
+			reactExamples: {
+				files: {
+					'examples/react/examples.js': 'src/react/examples.js'
+				}
+			},
+			tests: {
+				files: {
+					'test/tests-compiled.js': 'test/tests.js'
+				}
+			}
 		},
 		mocha: {
 			main: {
@@ -50,23 +53,22 @@ module.exports = function (grunt) {
 				}
 			}
 		},
-		uglify: {
-			examples: {
-				files: {
-					'examples/jquery/examples.min.js': ['examples/jquery/examples.js'],
-					'examples/marionette/examples.min.js': ['examples/marionette/examples.js'],
-					'examples/react/examples.min.js': ['examples/react/examples.js']
-				}
-			}
-		},
 		watch: {
-			examples: {
-				files: ['src/**/*.*', 'sample-data/**/*.*'],
-				tasks: ['eslint', 'browserify:examples']
+			jqueryExamples: {
+				files: ['src/**/*.*', 'sample-data/**/*.*', '!src/marionette/**/*.*', '!src/react/**/*.*'],
+				tasks: ['eslint', 'browserify:jqueryExamples']
+			},
+			marionetteExamples: {
+				files: ['src/**/*.*', 'sample-data/**/*.*', '!src/jquery/**/*.*', '!src/react/**/*.*'],
+				tasks: ['eslint', 'browserify:marionetteExamples']
+			},
+			reactExamples: {
+				files: ['src/**/*.*', 'sample-data/**/*.*', '!src/jquery/**/*.*', '!src/marionette/**/*.*'],
+				tasks: ['eslint', 'browserify:reactExamples']
 			},
 			tests: {
 				files: ['src/**/*.*', 'test/**/*.*', '!test/tests.js', '!test/tests-compiled.js'],
-				tasks: ['compileTests', 'browserify:tests']
+				tasks: ['eslint', 'compileTests', 'browserify:tests']
 			}
 		},
 		connect: {
@@ -93,12 +95,11 @@ module.exports = function (grunt) {
 
 	grunt.loadNpmTasks('grunt-browserify');
 	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-eslint');
 	grunt.loadNpmTasks('grunt-mocha');
 
-	grunt.registerTask('default', ['eslint', 'browserify']);
-	grunt.registerTask('serve', ['connect:server', 'eslint', 'browserify:examples', 'watch:examples']);
-	grunt.registerTask('test', ['connect:server', 'eslint', 'compileTests', 'browserify:tests', 'mocha']);
+	grunt.registerTask('default', ['eslint', 'compileTests', 'browserify']);
+	grunt.registerTask('serve', ['connect:server', 'default', 'watch']);
+	grunt.registerTask('test', ['eslint', 'compileTests', 'browserify:tests', 'connect:server', 'mocha']);
 };
