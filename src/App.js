0effc29e168758fe600c9a489515df5d52014b5a
ibogdanov <ibogdanov@salesforce.com>
2015-08-16 12:58:22 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SLDSPopover from './slds/SLDSPopover';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <section>
+        <h1>!!!!Hello, world!!!</h1>
+        <SLDSPopover>AAA</SLDSPopover>
+      </section>
+    );
+  }
+}
---
+++ src/slds/SLDSPopover.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SLDSPopover extends Component {
+  render() {
+    return (
+      <div>AAA</div>
+    );
+  }
+}
