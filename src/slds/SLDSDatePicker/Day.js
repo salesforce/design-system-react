d7fda86b7ee973cf3991557e6790853e7f468cf7
ibogdanov <ibogdanov@salesforce.com>
2015-08-17 15:59:12 -0700

    polish day renderee

--- src/slds/SLDSDatePicker/Day.js
+++ src/slds/SLDSDatePicker/Day.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
 var Day = React.createClass({
+
+
   render: function() {
 
-    return <td role="gridcell" aria-disabled="true">
-        <span 
-          key={this.props.day.date.toString()} 
-          className={"sds-day" + (this.props.day.isToday ? " sds-is-selected" : "") + (this.props.day.isCurrentMonth ? "" : " different-month") + (this.props.day.date.isSame(this.props.selected) ? " selected" : "")} 
-          ariaSelected={this.props.day.isToday}
-          onClick={this.props.onSelect.bind(null, this.props.day)}>
+    return <td 
+              key={this.props.day.date.toString()}
+              role="gridcell" 
+              aria-disabled={this.props.day.isCurrentMonth}
+              aria-selected={this.props.day.isToday}
+              className={"sds-day" + (this.props.day.isToday ? " sds-is-today" : "") + (this.props.day.isCurrentMonth ? "" : " sds-disabled-text") + (this.props.day.date.isSame(this.props.selected) ? " sds-is-selected" : "")} 
+              onClick={this.props.onSelect.bind(null, this.props.day)}
+            >
+          <span className="sds-day">
             {this.props.day.number}
           </span>
       </td>
