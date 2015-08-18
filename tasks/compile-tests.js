9a06d0da43cf41532fdf0e0b34c25d106c3d48e8
Jason Schmidt <JSchmidt@ExactTarget.com>
2015-08-18 08:20:23 -0400

    Literal tabs instead of spaces

--- tasks/compile-tests.js
+++ tasks/compile-tests.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
-  grunt.registerTask('compileTests', 'Compiles and saves the full list of tests.', function() {
-    var files = grunt.file.expand(['test/**/*.js'].concat(grunt.config('excludePatternGeneratedTestFiles')));
-    files = files.map(function(file) {
-      return 'require(\'./' + file.substring(5) + '\');';
-    });
+	grunt.registerTask('compileTests', 'Compiles and saves the full list of tests.', function() {
+		var files = grunt.file.expand(['test/**/*.js'].concat(grunt.config('excludePatternGeneratedTestFiles')));
+		files = files.map(function(file) {
+			return 'require(\'./' + file.substring(5) + '\');';
+		});
 
-    grunt.file.write('test/tests.js', files.join('\n')+'\n');
-  });
+		grunt.file.write('test/tests.js', files.join('\n')+'\n');
+	});
 };
