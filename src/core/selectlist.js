efbac05d18eee927de1aa4bae354a8442a3dca70
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-08-26 11:43:30 -0400

    Allow arrow nav in React without bootstrap

--- src/core/selectlist.js
+++ src/core/selectlist.js
@@ -45,7 +45,8 @@ const SelectlistCore = Lib.merge({}, Base, Disableable, Selectable, {
 	},
 	
 	_defaultState: {
-		isOpen: false
+		isOpen: false,
+		focusedIndex: -1
 	},
 
 	_initializer (options) {
@@ -138,40 +139,54 @@ const SelectlistCore = Lib.merge({}, Base, Disableable, Selectable, {
 	},
 
 	// For keyboard nav
-	_jumpToLetter (input) {
-		let letter = input;
-		let selection;
-		let pattern;
-		let consecutive = 0;
-
-		if (Lib.isNumber(letter)) {
-			letter = String.fromCharCode(letter);
-		}
-
-		if (letter.length !== 1) {
-			return;
-		}
+	_keyboardNav (input, menuItems) {
+		const isOpen = this.getState('isOpen');
 		
-		// Combine subsequent keypresses
-		pattern = this._keyBuffer(letter).toLowerCase();
-		
-		// Support for navigating to the next option of the same letter with repeated presses of the same key
-		if (pattern.length > 1 && new RegExp('^[' + letter.replace('\\', '\\\\') + ']+$').test(pattern)) {
-			consecutive = pattern.length;
-		}
-		
-		const menu = this.elements.dropdownMenu[0];
-		const menuItems = [].slice.call(menu.getElementsByTagName('a'));
-		
-		menuItems.forEach(function compareMenuItem (menuItem) {
-			if ((!selection && menuItem.textContent.substr(0, pattern.length).toLowerCase() === pattern) ||
-				(consecutive > 0 && menuItem.textContent.substr(0, 1).toLowerCase() === letter)) {
-				consecutive--;
-				selection = menuItem;
+		if (!isOpen) {
+			this.setState({
+				isOpen: true,
+				focusedIndex: this._defaultState.focusedIndex
+			});
+		} else {
+			let index = this.getState('focusedIndex');
+			let selection;
+			
+			if (input.length === 1) {
+				// Combine subsequent keypresses
+				const pattern = this._keyBuffer(input).toLowerCase();
+				let consecutive = 0;
+				
+				// Support for navigating to the next option of the same letter with repeated presses of the same key
+				if (pattern.length > 1 && new RegExp('^[' + input.replace('\\', '\\\\') + ']+$').test(pattern)) {
+					consecutive = pattern.length;
+				}
+				
+				menuItems.forEach(function compareMenuItem (menuItem, i) {
+					if ((!selection && menuItem.textContent.substr(0, pattern.length).toLowerCase() === pattern) ||
+						(consecutive > 0 && menuItem.textContent.substr(0, 1).toLowerCase() === input)) {
+						consecutive--;
+						index = i;
+						selection = menuItem;
+					}
+				});
+			} else if (/(ArrowDown)/.test(input)) {
+				if (index < menuItems.length - 1) {
+					index++;
+					selection = menuItems[index];
+				}
+			} else if (/(ArrowUp)/.test(input)) {
+				if (index > this._defaultState.focusedIndex) {
+					index--;
+					selection = menuItems[index];
+				}
 			}
-		});
-
-		if (selection) selection.focus();
+			
+			this.setState({
+				focusedIndex: index
+			});
+	
+			if (selection) selection.focus();
+		}
 	}
 });
 
--- src/react/selectlist/selectlist.js
+++ src/react/selectlist/selectlist.js
@@ -50,7 +50,7 @@ const Selectlist = React.createClass(Lib.merge({}, SelectlistCore, {
 		openClass[this.cssClasses.OPEN] = this.state.isOpen;
 
 		return (
-			<div className={classNames(this.cssClasses.CONTROL, this.cssClasses.BTN_GROUP, disabledClass, openClass)} onKeyPress={this._handleKeyPressed}>
+			<div className={classNames(this.cssClasses.CONTROL, this.cssClasses.BTN_GROUP, disabledClass, openClass)} onKeyDown={this._handleKeyPressed} onKeyPress={this._handleKeyPressed}>
 				<button className={classNames(this.cssClasses.BTN_DEFAULT, this.cssClasses.TOGGLE, disabledClass)} type="button" disabled={this.props.disabled} style={styles} aria-haspopup="true" aria-expanded={this.state.isOpen} onClick={this._handleClicked}>
 					<span className={this.cssClasses.LABEL}>{item.getText() || this.strings.NONE_SELECTED}</span>
 					<span className={this.cssClasses.CARET}></span>
@@ -65,10 +65,30 @@ const Selectlist = React.createClass(Lib.merge({}, SelectlistCore, {
 	
 	componentDidMount () {
 		document.addEventListener('click', this._closeMenu, false);
+		this._findElements();
+	},
+	
+	componentDidUpdate () {
+		this._findElements();
 	},
 	
 	componentWillUnmount () {
 		document.removeEventListener('click', this._closeMenu, false);
+	},
+	
+	_findElements () {
+		this.elements.dropdownMenu = Lib.wrapElement(React.findDOMNode(this.refs[this.cssClasses.MENU]));
+		
+		this.elements.menuItems = [];
+		const menuItems = this.elements.dropdownMenu[0].getElementsByTagName('li');
+		
+		for (let i = 0; i < menuItems.length; i++) {
+			const menuItem = menuItems[i].getElementsByTagName('a');
+			
+			if (!menuItems[i].disabled && menuItem.length === 1) {
+				this.elements.menuItems.push(menuItem[0]);
+			}
+		}
 	},
 	
 	_closeMenu (e) {
@@ -100,10 +120,10 @@ const Selectlist = React.createClass(Lib.merge({}, SelectlistCore, {
 	},
 
 	_handleKeyPressed (e) {
-		this.elements.dropdownMenu = this.elements.dropdownMenu || Lib.wrapElement(React.findDOMNode(this.refs[this.cssClasses.MENU]));
-		
-		const key = e.key || e.keyIdentifier;
-		if (key) this._jumpToLetter(key);
+		if (e.key && (/(ArrowUp|ArrowDown)/.test(e.key) || e.key.length === 1)) {
+			e.preventDefault();
+			this._keyboardNav(e.key, this.elements.menuItems);
+		}
 	}
 }));
 
