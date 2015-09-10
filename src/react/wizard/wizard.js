5a3220bdf506e0e192f287033a99bb3263ee17b4
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-09-10 15:00:33 -0400

    Allow app developers to override the steps and actions

--- src/react/wizard/wizard.js
+++ src/react/wizard/wizard.js
@@ -17,15 +17,29 @@ import WizardActions from './wizard-actions';
 let Wizard = Lib.extend({}, WizardCore, {
 	mixins: [State, Events, genericWillMount],
 
+	propTypes: {
+		step: React.PropTypes.object,
+		collection: React.PropTypes.oneOfType([
+			React.PropTypes.array,
+			React.PropTypes.object
+		]).isRequired,
+		stepElement: React.PropTypes.element,
+		actionsElement: React.PropTypes.element
+	},
+
 	_steps (currentIndex) {
+		const Step = this.props.stepElement || WizardStep;
+		
 		return this._collection.map((item, index) => {			
-			return <WizardStep key={index} item={item} index={index + 1} currentIndex={currentIndex} onClicked={this._setSelection.bind(this, item._item)} />;
+			return <Step strings={this.state.strings} key={index} item={item} index={index + 1} currentIndex={currentIndex} onClicked={this._setSelection.bind(this, item._item)} />;
 		});
 	},
 
 	render () {
 		const currentIndex = this.getIndex();
 		const isLast = currentIndex === this._collection.length();
+		
+		const Actions = this.props.actionsElement || WizardActions;
 		
 		return (
 			<div className="wizard">
@@ -34,7 +48,7 @@ let Wizard = Lib.extend({}, WizardCore, {
 						{this._steps(currentIndex)}
 					</ul>
 				</div>
-				<WizardActions strings={this.state.strings} isLast={isLast} onPrevClicked={this.previousStep} onNextClicked={this.nextStep} onFinished={this._onFinished} onJumpToStep={this.setStepByIndex} />
+				<Actions strings={this.state.strings} isLast={isLast} onPrevClicked={this.previousStep} onNextClicked={this.nextStep} onFinished={this._onFinished} onJumpToStep={this.setStepByIndex} />
 				<div className="step-content">
 					{this.props.children}
 				</div>
