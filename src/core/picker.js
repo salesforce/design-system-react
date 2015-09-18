775d27dbbcb0dfe0d8bf747452bad18fc9b15892
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-09-18 13:09:28 -0600

    Add positioning methods to the picker

--- src/core/picker.js
+++ src/core/picker.js
@@ -9,6 +9,24 @@ const PickerCore = Lib.merge({}, Base, {
 	// CSS classes used within this control
 	cssClasses: {
 		CONTROL: CONTROL
+	},
+	
+	_getPositionAbove (element) {
+		const position = element.offset();
+		
+		position.top = position.top - this.elements.wrapper.outerHeight(true);
+		return position;
+	},
+
+	_getPositionBelow (element) {
+		const position = element.offset();
+		
+		position.top = position.top + element.outerHeight(true);
+		return position;
+	},
+	
+	_getCenteredPosition () {
+		// TO-DO: Implement this to support modals
 	}
 });
 
