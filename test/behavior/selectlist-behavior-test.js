51c395a09fe1264deea57febd3bd27c82cd438e1
Stephen James <llama@interactivellama.com>
2015-09-22 01:30:21 -0400

    Selectlist jquery tests - Create/destroy

---
+++ test/behavior/selectlist-behavior-test.js
@@ -0,0 +1,175 @@
+/*
+ * SELECTLIST TESTS
+ * Imports all the facades-specific implementations of the tests listed below and runs them
+ * Do not run facade (framework) specific tests directly. Use this file.
+ * View at http://localhost:8080/test/
+ */
+
+// Library allowing DOM elements to be checked for
+import * as expectDom from '../lib/expect-dom';
+// Confirm that the API test libraries export ALL the behaviors and approaches
+import { verifyFacadeProvidesBehaviorCallbacks, registerBehaviorTestCombinations } from '../lib/behavior-test-runner';
+// import the needed component and all it's facade-specific functions
+// (ex.- jQuery-specific implementation of createComponent)
+import { selectlist as componentFacadeTestLib } from '../tests-api';
+
+// Core file for helpful things like CSS class names
+import { CONTROL as controlName } from '../../src/core/selectlist';
+import SelectlistCore from '../../src/core/selectlist';
+const Core = SelectlistCore;
+
+const $ = require('jquery');
+const chai = require('chai');
+const expect = chai.expect;
+
+
+// For jQuery facade, "declarative" implies jQuery plugin usage
+
+describe(controlName + ' component', function () {
+	verifyFacadeProvidesBehaviorCallbacks(componentFacadeTestLib, [
+		'createComponent',
+		'createComponentDeclarative',
+		'getComponentElement',
+		'destroyComponent',
+		'destroyComponentDeclarative'
+	]);
+
+	registerBehaviorTestCombinations(componentFacadeTestLib, [
+		// behaviors being tested
+		'createComponent',
+		'createComponentDeclarative',
+		'getComponentElement',
+		'destroyComponent',
+		'destroyComponentDeclarative',
+	], [
+		// other behaviors required for tests
+	], function (testingBehaviorHandlers) {
+		let container = null;
+
+		beforeEach(function () {
+			container = $('<div />');
+			$('body').append(container); // Container must be on the body and visible for tests to work
+		});
+
+		afterEach(function () {
+			container.remove(); // Clean up container
+			container = null;
+		});
+
+		describe('create and destroy component', function () {
+			it('should create a ' + controlName + ' on the DOM within the container', function () {
+
+				function rendered (component) {
+					expect(component.options.container.find('.' + Core.cssClasses.CONTROL).length).to.equal(1);
+					testingBehaviorHandlers.destroyComponent(component);
+				}
+
+				const component = testingBehaviorHandlers.createComponent( {
+					container: container,
+					collection: []
+				}, rendered );
+				
+			});
+
+			it('should return an object representing the component', function () {
+				const component = testingBehaviorHandlers.createComponent( {
+					container: container,
+					collection: []
+				} );
+
+				expect(component).to.be.an('object');
+				testingBehaviorHandlers.destroyComponent(component);
+			});
+
+			it('destroy should remove ' + controlName + ' from container', function () {
+
+				function rendered (component) {
+					testingBehaviorHandlers.destroyComponent(component);
+					expect(component.options.container.find('.' + Core.cssClasses.CONTROL).length).to.equal(0);
+				}
+
+				const component = testingBehaviorHandlers.createComponent( {
+					container: container,
+					collection: [],
+				}, rendered );
+
+			});
+		});
+
+		describe('create and destroy component (declarative)', function () {
+			it('should create a ' + controlName + ' on the DOM within the container', function () {
+				const component = testingBehaviorHandlers.createComponentDeclarative({
+					container: container
+				});
+
+				expect(container.find('.' + Core.cssClasses.CONTROL).length).to.equal(1);
+				testingBehaviorHandlers.destroyComponentDeclarative(component);
+			});
+
+			it('destroy should remove ' + controlName + ' from container (declarative)', function () {
+				const component = testingBehaviorHandlers.createComponentDeclarative({
+					container: container
+				});
+
+				testingBehaviorHandlers.destroyComponentDeclarative(component);
+				expect(container.find('.' + Core.cssClasses.CONTROL).length).to.equal(0);
+			});
+		});
+
+		// async needs work
+
+		// describe('DOM expectations', function () {
+		// 	let component = null;
+		// 	let componentElement = null;
+
+		// 	const collection = [
+		// 		{ _itemType: 'header', text: 'One thing' },
+		// 		{ id: 0, text: 'One', value: '1' },
+		// 		{ _itemType: 'divider' },
+		// 		{ _itemType: 'header', text: 'All the things' },
+		// 		{ id: 1, text: 'Two', value: '2' },
+		// 		{ id: 2, text: 'Three', value: '3'  },
+		// 		{ id: 3, text: 'Buzz', value: '4'  },
+		// 		{ id: 4, text: 'Item Five', value: 'Item Five', fizz: 'buzz', foo: 'bar'  },
+		// 		{ id: 5, text: 'Disabled Item', disabled: true, value: 'disabled' }
+		// 	];
+
+		// 	const initDataTemplate = {
+		// 		collection: collection,
+		// 		selection: { value: '4' }
+		// 	};
+
+		// 	function rendered (component) {
+		// 		expectDom.matches(getComponentElement, 'div');
+		// 	}
+
+		// 	beforeEach(function () {
+		// 		const initData = JSON.parse( JSON.stringify( initDataTemplate ) );
+		// 		initData.container = container;
+		// 		rendered();
+
+		// 		component = testingBehaviorHandlers.createComponent( initData, rendered );
+		// 		componentElement = testingBehaviorHandlers.getComponentElement(component);
+
+		// 	});
+
+		// 	afterEach(function () {
+		// 		testingBehaviorHandlers.destroyComponent(component);
+		// 		component = null;
+		// 		componentElement = null;
+		// 	});
+
+		// 	function getComponentElement () {
+		// 		return componentElement;
+		// 	}
+
+		// 	expectDom.matches(getComponentElement, 'div');
+		// 	// expectDom.matches(getComponentElement, '.' + Core.cssClasses.CONTROL);
+		// 	// expectDom.found(getComponentElement,   '> ul.dropdown-menu');
+		// 	// expectDom.found(getComponentElement,   'ul.dropdown-menu > li:eq(0)[data-value=1]');
+		// 	// expectDom.found(getComponentElement,   'ul.dropdown-menu > li[data-value=1] > a');
+
+		// });
+	
+	});
+});
---
+++ test/jquery/selectlist-jquery-test.js
@@ -0,0 +1,235 @@
+/*
+ * JQUERY FACADE API FOR SELECTLIST TESTS
+ * Facade (or framework specific) implementation of tests defined in behaviors folder
+ */
+
+// Core file for helpful things like CSS class names
+import { CONTROL as controlName } from '../../src/core/selectlist';
+import SelectlistCore from '../../src/core/selectlist';
+import * as Lib from '../../src/lib/lib';
+
+// Run in legacy plugin mode
+import '../../src/helpers/create-jquery-plugin';
+import Selectlist from '../../src/jquery/selectlist/selectlist';
+
+const $ = Lib.global.jQuery || Lib.global.$;
+
+export const behaviorHandlers = {
+	createComponentDeclarative: {
+		default: function (initData) {
+			let declarativeMarkup = `<div id="mySelectlist" class="btn-group selectlist" data-resize="auto">
+				<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+					<span class="selected-label">&nbsp;</span>
+					<span class="caret"></span>
+					<span class="sr-only">Toggle Dropdown</span>
+				</button>
+				<ul class="dropdown-menu" role="menu">
+					<li data-value="1"><a href="#">One</a></li>
+					<li data-value="2" data-selected="true"><a href="#">Two</a></li>
+					<li data-value="3"><a href="#">Three</a></li>
+					<li class="dropdown-header">Dropdown header</li>
+					<li data-value="4" data-foo="bar" data-fizz="buzz"><a href="#">Buzz</a></li>
+					<li data-value="Item Five"><a href="#">Item Five</a></li>
+					<li class="disabled"><a href="#">Disabled item</a>
+					</li>
+				</ul>
+				<input class="hidden hidden-field" name="my-selectlist" readonly="readonly" aria-hidden="true" type="text"/>
+			</div>`;
+
+			// initialize
+			$(initData.container).append(declarativeMarkup);
+			let $selectlist = $(initData.container).find('.selectlist').selectlist();
+			return $selectlist;
+		}
+	},
+
+	createComponent: {
+		default: function (initData, rendered) {
+			let imperativeMarkup = '<div id="my-selectlist"></div>';
+			$(initData.container).append(imperativeMarkup);
+			const $selectlist = new Selectlist($('#my-'+ controlName), initData);
+
+			$('#my-selectlist').on('rendered.fu.selectlist', function (event, data) {
+				rendered($selectlist);
+			});
+
+			return $selectlist;
+		}
+	},
+
+	getComponentElement: {
+		default: function (createdComponent) {
+			return createdComponent.options.container.find('.selectlist')[0];
+		}
+	},
+
+	destroyComponent: {
+		default: function (createdComponent) {
+			createdComponent.destroy();
+		}
+	},
+
+	destroyComponentDeclarative: {
+		default: function (createdComponent) {
+			createdComponent.selectlist('destroy');
+		}
+	}
+};
+
+
+/* original tests */
+
+/* FOR DEV TESTING */
+// var html = require('text!dev.html!strip');
+// html = $('<div><div id="mainSelectlist"></div></div>');
+// const $testFixture = $('body #test-fixture');
+// $testFixture.append(html);
+
+// const defaultOptions = {
+// 	collection: [
+// 		{ _itemType: 'header', text: 'One thing' },
+// 		{ id: 0, text: 'One', value: '1'  },
+// 		{ _itemType: 'divider' },
+// 		{ _itemType: 'header', text: 'All the things' },
+// 		{ id: 1, text: 'Two', value: '2'  },
+// 		{ id: 2, text: 'Three', value: '3' },
+// 		{ id: 3, text: 'Buzz', value: '4' },
+// 		{ id: 4, text: 'Item Five', value: 'Item Five', fizz: 'buzz', foo: 'bar' },
+// 		{ id: 5, text: 'A Disabled Item', disabled: true, value: 'disabled' }
+// 	]
+// };
+
+// describe('Fuel UX Selectlist - jQuery facade', function () {
+// 	after(function () {
+// 		$testFixture.empty();
+// 	});
+
+// 	it('should be disablable and then re-enableable', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		selectlist.disable();
+// 		assert.equal(selectlist.elements.wrapper.hasClass('disabled'), true, 'element disabled');
+
+// 		selectlist.enable();
+// 		assert.equal(selectlist.elements.wrapper.hasClass('disabled'), false, 'element re-enabled');
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should set the default selection', function () {
+// 		const options = $.extend( defaultOptions, { selection: { id: 4 } } );
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), options );
+// 		const expectedItem = { id: 4, text: 'Item Five', value: 'Item Five', fizz: 'buzz', foo: 'bar' };
+
+// 		console.log(selectlist.getSelection());
+
+// 		assert.deepEqual( selectlist.getSelection(), expectedItem, 'default item selected' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should select by value', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 1, text: 'Two', value: '2' };
+// 		selectlist.setSelection( { value: '2' } );
+
+// 		assert.deepEqual( selectlist.getSelection(), selectingItem, 'selected by value' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should select by value with whitespace', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 4, text: 'Item Five', value: 'Item Five', fizz: 'buzz', foo: 'bar'  };
+// 		selectlist.setSelection( { value: 'Item Five' } );
+
+// 		assert.deepEqual( selectlist.getSelection(), selectingItem, 'selected by value with whitespace' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should select by text', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 2, text: 'Three', value: '3'  };
+// 		selectlist.setSelection( { text: 'Three' } );
+
+// 		assert.deepEqual( selectlist.getSelection(), selectingItem, 'selected by name' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should select by text with whitespace', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 4, text: 'Item Five', value: 'Item Five', fizz: 'buzz', foo: 'bar'  };
+// 		selectlist.setSelection( { text: 'Item Five' } );
+
+// 		assert.deepEqual( selectlist.getSelection(), selectingItem, 'selected by text with whitespace' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should select by index', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 3, text: 'Buzz', value: '4'  };
+// 		selectlist.setSelectionByIndex( 6 );
+
+// 		assert.deepEqual( selectlist.getSelection(), selectingItem, 'selected by index' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should select by any arbitrary property on an item', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 4, text: 'Item Five', value: 'Item Five', fizz: 'buzz', foo: 'bar'  };
+// 		selectlist.setSelection( { fizz: 'buzz' } );
+
+// 		assert.deepEqual( selectlist.getSelection(), selectingItem, 'selected by arbitrary property' );
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should fire change event', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		const selectingItem = { id: 0, text: 'One', value: '1' };
+// 		let eventFired = false;
+// 		let selectedText;
+// 		let selectedValue;
+
+// 		selectlist.elements.wrapper.on( 'changed.fu.selectlist', function ( $event, data ) {
+// 			eventFired = true;
+// 			selectedText = data.text;
+// 			selectedValue = data.value;
+// 		});
+
+// 		// simulate changed event
+// 		selectlist.elements.dropdownMenu.find('a:first').click();
+
+// 		assert.equal(eventFired, true, 'change event fired');
+// 		assert.equal(selectedText, selectingItem.text, 'text passed in from change event');
+// 		assert.equal(selectedValue, selectingItem.value, 'value passed in from change event');
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should not fire changed event on disabled items', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+// 		let eventFired = false;
+
+// 		selectlist.elements.wrapper.on('changed.fu.selectlist', function (/* $event, data */) {
+// 			eventFired = true;
+// 		});
+
+// 		// simulate changed event
+// 		selectlist.elements.dropdownMenu.find('.disabled a').click();
+
+// 		assert.equal(eventFired, false, 'change event not fired');
+
+// 		selectlist.elements.wrapper.empty();
+// 	});
+
+// 	it('should destroy control', function () {
+// 		const selectlist = new Selectlist( $( html ).find( '#mainSelectlist' ), defaultOptions );
+
+// 		assert.equal( typeof( selectlist.destroy() ), 'string', 'returns string (markup)');
+// 		assert.equal( $( html ).find( '#mainSelectlist' ).length, 0, 'control has been removed from DOM');
+// 	});
+// });
