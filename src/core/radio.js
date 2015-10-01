e78d1d804630c172552438c544d2c22fe06701ad
kevinparkerson <kparkerson@exacttarget.com>
2015-10-01 15:25:46 -0400

    initial radio core

---
+++ src/core/radio.js
@@ -0,0 +1,22 @@
+// RADIO CONTROL
+
+import * as Lib from '../lib/lib';
+import CheckboxCore from './checkbox';
+
+export const CONTROL = 'radio';
+
+const RadioCore = Lib.merge({}, CheckboxCore, {
+	/*
+	cssClasses: {
+		CONTROL: CONTROL + '-custom',
+		CHECKED: 'checked',
+		BLOCK: CONTROL,
+		INLINE: CONTROL + '-inline',
+		LABEL: CONTROL + '-label',
+		ADDON: 'input-group-addon',
+		HIGHLIGHT: 'highlight'
+	}
+	*/
+});
+
+export default RadioCore;
