9195d01176e20a12551e7aaf5223891a873a5637
kevinparkerson <kparkerson@exacttarget.com>
2015-10-02 16:10:15 -0400

    radio jquery completed with examples done

--- src/jquery/radio/example.js
+++ src/jquery/radio/example.js
@@ -8,5 +8,105 @@ $(function () {
 	$('#myRadio1').radio();
 	$('#myRadio2').radio();
 
-	void Radio;
+	// "imperative" controls
+	$('#myRadio3').radio({
+		name: 'radioGroup3',
+		text: 'Custom radio unchecked on initialization',
+		toggleSelector: '.radioToggleSelector2',
+		value: 'value7'
+	});
+	$('#myRadio4').radio({
+		name: 'radioGroup3',
+		text: 'Custom radio unchecked on initialization',
+		value: 'value8'
+	});
+	$('#myRadio5').radio({
+		checked: true,
+		disabled: true,
+		name: 'radioGroup3',
+		text: 'Custom radio checked disabled on initialization',
+		value: 'value9'
+	});
+	$('#myRadio6').radio({
+		highlight: true,
+		inline: true,
+		name: 'radioGroup4',
+		text: 'Custom inline radio unchecked on initialization',
+		value: 'value10'
+	});
+	$('#myRadio7').radio({
+		inline: true,
+		name: 'radioGroup4',
+		text: 'Custom inline radio unchecked on initialization',
+		value: 'value11'
+	});
+	$('#myRadio8').radio({
+		checked: true,
+		disabled: true,
+		inline: true,
+		name: 'radioGroup4',
+		text: 'Custom inline radio checked disabled on initialization',
+		value: 'value12'
+	});
+
+	// new api controls
+	const radioNA1 = new Radio($('#myRadio9'), {
+		name: 'radioGroup5',
+		text: 'Custom radio unchecked on initialization',
+		toggleSelector: '.radioToggleSelector3',
+		value: 'value7'
+	});
+	const radioNA2 = new Radio($('#myRadio10'), {
+		name: 'radioGroup5',
+		text: 'Custom radio unchecked on initialization',
+		value: 'value8'
+	});
+	const radioNA3 = new Radio($('#myRadio11'), {
+		checked: true,
+		disabled: true,
+		name: 'radioGroup5',
+		text: 'Custom radio checked disabled on initialization',
+		value: 'value9'
+	});
+	const radioNA4 = new Radio($('#myRadio12'), {
+		highlight: true,
+		inline: true,
+		name: 'radioGroup6',
+		text: 'Custom inline radio unchecked on initialization',
+		value: 'value10'
+	});
+	const radioNA5 = new Radio($('#myRadio13'), {
+		inline: true,
+		name: 'radioGroup6',
+		text: 'Custom inline radio unchecked on initialization',
+		value: 'value11'
+	});
+	const radioNA6 = new Radio($('#myRadio14'), {
+		checked: true,
+		disabled: true,
+		inline: true,
+		name: 'radioGroup6',
+		text: 'Custom inline radio checked disabled on initialization',
+		value: 'value12'
+	});
+
+	// events
+	const $radioExamples = $('.checkbox-examples .checkbox, .checkbox-examples .checkbox-custom');
+
+	$radioExamples.on('changed.fu.checkbox', function (event, data) {
+		Lib.log('changed.fu.checkbox: ', event, data);
+	});
+	$radioExamples.on('checked.fu.checkbox', function (event) {
+		Lib.log('checked.fu.checkbox: ', event);
+	});
+	$radioExamples.on('unchecked.fu.checkbox', function (event) {
+		Lib.log('unchecked.fu.checkbox: ', event);
+	});
+
+	void radioNA1;
+	void radioNA2;
+	void radioNA3;
+	void radioNA4;
+	void radioNA5;
+	void radioNA6;
 });
--- src/jquery/radio/radio.js
+++ src/jquery/radio/radio.js
@@ -14,6 +14,8 @@ const $ = Lib.global.jQuery || Lib.global.$;
 // Template imports
 import template from './radio-template';
 
+const radioGroups = {};	// declared at this scope to keep a unified list of linked radios
+
 // Constructor
 let Radio = function Radio (element, options) {
 	this.options = Lib.extend({}, options);
@@ -21,55 +23,75 @@ let Radio = function Radio (element, options) {
 		wrapper: $(element)
 	};
 
+	this.addedToGroup = false;
 	this.inputSelector = 'input[type="radio"]';
 	this.namespace = 'fu.radio';
 	this.rendered = (this.elements.wrapper.find(this.inputSelector).length > 0);
-	this.template = $(template);
+	this.template = $('<i />').append(template);
 
 	this._initializeState();
 	this._initialize(this.options);
 };
 
+// TODO: consider cases of `CheckboxObject._someFunction.call(this);` and whether it's a solid pattern vs:
+// 1) overriding the whole function, redundant code and all (yuck) or
+// 2) providing hooks within the facade layer to allow additional functionality to be leveraged in the extending control
+//		like the cores do (not a terrible idea... ex: `if (this._evenMoreStuff) this._evenMoreStuff();`)
+
 // Prototype extension object
 const RadioObject = Lib.merge({}, CheckboxObject, {
+	_getGroup () {
+		const name = this.getProperty('name');
+
+		if (!this.addedToGroup) {
+			if (!radioGroups[name]) radioGroups[name] = [];
+			radioGroups[name].push(this);
+			this.addedToGroup = true;
+		}
+
+		return radioGroups[name] || [];
+	},
+
 	_handleInputChange () {
 		this.check();
 	},
 
+	// TODO: with this approach, if they were to change the name after instantiation it would behave inconsistently,
+	// though that's a very uncommon use-case so I doubt it's a concern...
 	_onToggled () {
-		const self = this;
-
 		if (this.getProperty('checked')) {
-			$('input[name="' + this.getProperty('name') + '"]').each(function (index, item) {
-				let count = 0;
-				let $item = $(item);
-
-				if ($item.is(self.elements.input)) return;
-
-				while (!$item.data(self.namespace) && count < 4) {
-					$item = $item.parent();
-					count++;
+			const group = this._getGroup();
+			for (let index = 0, length = group.length; index < length; index++) {
+				if (group[index] && group[index] !== this) {
+					group[index].uncheck();
 				}
-
-				if ($item.data(self.namespace)) {
-					$item.radio('uncheck');
-				}
-			});
+			}
 		}
-
-		CheckboxObject._onToggled.call(this);	// TODO: tried super, broke... better syntax for this?
+		CheckboxObject._onToggled.call(this);
 	},
 
 	_renderDressings (elements) {
 		elements.input.attr('name', this.getProperty('name'));
-		CheckboxObject._renderDressings.call(this, elements);	// TODO: tried super, broke... better syntax for this?
+		CheckboxObject._renderDressings.call(this, elements);
 	},
 
 	_syncProperties () {
 		this.setProperties({
 			name: this.elements.input.attr('name')
 		});
-		CheckboxObject._syncProperties.call(this);	// TODO: tried super, broke... better syntax for this?
+		CheckboxObject._syncProperties.call(this);
+	},
+
+	destroy () {
+		const self = this;
+		const group = this._getGroup();
+		const index = group.findIndex(function (item) {
+			return (item === self);
+		});
+
+		if (index > -1) group.splice(index, 1);
+		this.elements.wrapper.remove();
+		return this.elements.wrapper[0].outerHTML;
 	}
 });
 
