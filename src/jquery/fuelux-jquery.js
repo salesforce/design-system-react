5698c8544afd74eeb30ae58cec7e6d146a077922
Ryan Moore <ryan.moore@salesforce.com>
2015-10-06 14:06:17 -0400

    refactor most of react component to core and add jquery facade

--- src/jquery/fuelux-jquery.js
+++ src/jquery/fuelux-jquery.js
@@ -2,3 +2,4 @@ import './combobox/combobox';
 import './loader/loader';
 import './selectlist/selectlist';
 import './tree/tree';
+import './search/search';
---
+++ src/jquery/search/search-template.js
@@ -0,0 +1,11 @@
+export default String.raw`
+<div class="search input-group" role="search">
+	<input type="search" class="form-control" placeholder="Search"/>
+	<span class="input-group-btn">
+		<button class="btn btn-default" type="button">
+			<span class="glyphicon glyphicon-search"></span>
+			<span class="sr-only">Search</span>
+		</button>
+	</span>
+</div>
+`;
