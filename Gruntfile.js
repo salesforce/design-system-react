c309b6d69bcf9ebe1cb8932ece99b03e1deea2d2
Dave Woodward <dwoodward@exacttarget.com>
2015-10-08 21:09:01 -0400

    Shoots nyancat in the head. Rainbows everywhere. Also removes compat tests from test suite for now until we add Fuel UX compatibility back in at a later day.  Left some breadcrumbs so Hansel and Grettle can find their way back to Fuel UX 3.

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
 					urls: ['http://localhost:<%= port %>/test/index.html'],
 					run: true,
 					log: true,
-					reporter: 'Nyan'
+					reporter: 'Dot'
 				}
 			}
 		},
