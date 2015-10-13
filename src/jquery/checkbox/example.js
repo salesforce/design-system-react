f5d2b61c25dabd8c567dabf583ec70d625e44885
Christopher McCulloh <cmcculloh@exacttarget.com>
2015-10-13 11:50:42 -0400

    removing superfluous examples

--- src/jquery/checkbox/example.js
+++ src/jquery/checkbox/example.js
@@ -4,76 +4,11 @@ import Checkbox from './checkbox';
 const $ = Lib.global.jQuery || Lib.global.$;
 
 $(function () {
-	// "declarative" control (the others use data-initialize)
-	$('#myCheckbox1').checkbox();
-
-	// "imperative" controls
-	$('#myCheckbox2').checkbox({
-		text: 'Custom checkbox unchecked on initialization',
-		toggleSelector: '.checkboxToggleSelector2',
-		value: 'value5'
-	});
-	$('#myCheckbox3').checkbox({
-		checked: true,
-		disabled: true,
-		text: 'Custom checkbox checked disabled on initialization',
-		value: 'value6'
-	});
-	$('#myCheckbox4').checkbox({
-		checked: true,
-		highlight: true,
-		inline: true,
-		text: 'Custom inline checkbox checked on initialization',
-		value: 'value7'
-	});
-	$('#myCheckbox5').checkbox({
-		disabled: true,
-		inline: true,
-		text: 'Custom inline checkbox disabled on initialization',
-		value: 'value8'
-	});
-
 	// new api controls
-	const checkboxNA1 = new Checkbox($('#myCheckbox6'), {
+	const checkboxNA1 = new Checkbox($('#myCheckbox'), {
 		text: 'Custom checkbox unchecked on initialization',
-		toggleSelector: '.checkboxToggleSelector3',
 		value: 'value9'
-	});
-	const checkboxNA2 = new Checkbox($('#myCheckbox7'), {
-		checked: true,
-		disabled: true,
-		text: 'Custom checkbox checked disabled on initialization',
-		value: 'value10'
-	});
-	const checkboxNA3 = new Checkbox($('#myCheckbox8'), {
-		checked: true,
-		highlight: true,
-		inline: true,
-		text: 'Custom inline checkbox checked on initialization',
-		value: 'value11'
-	});
-	const checkboxNA4 = new Checkbox($('#myCheckbox9'), {
-		disabled: true,
-		inline: true,
-		text: 'Custom inline checkbox disabled on initialization',
-		value: 'value12'
-	});
-
-	// events
-	const $checkboxExamples = $('.checkbox-examples .checkbox, .checkbox-examples .checkbox-custom');
-
-	$checkboxExamples.on('changed.fu.checkbox', function (event, data) {
-		Lib.log('changed.fu.checkbox: ', event, data);
-	});
-	$checkboxExamples.on('checked.fu.checkbox', function (event) {
-		Lib.log('checked.fu.checkbox: ', event);
-	});
-	$checkboxExamples.on('unchecked.fu.checkbox', function (event) {
-		Lib.log('unchecked.fu.checkbox: ', event);
 	});
 
 	void checkboxNA1;
-	void checkboxNA2;
-	void checkboxNA3;
-	void checkboxNA4;
 });
