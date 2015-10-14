09b92b9ba2f1bb9dc65642fc5ec9963be6b0da6f
Stephen James <llama@interactivellama.com>
2015-10-14 17:33:58 -0400

    uppercase CSS class "states"
    And remove type property

--- src/core/button.js
+++ src/core/button.js
@@ -7,9 +7,9 @@ export const CONTROL = 'button';
 
 const ButtonCore = Lib.merge({}, Base, {
 	cssClasses: {
-		'base': Base.cssClasses.NAMESPACE + CONTROL,
-		'not-selected': Base.cssClasses.NAMESPACE + 'not-selected',
-		'selected': Base.cssClasses.NAMESPACE + 'is-selected',
+		'BASE': Base.cssClasses.NAMESPACE + CONTROL,
+		'NOT_SELECTED': Base.cssClasses.NAMESPACE + 'not-selected',
+		'SELECTED': Base.cssClasses.NAMESPACE + 'is-selected',
 		'small': Base.cssClasses.NAMESPACE + CONTROL + '--small',
 		'neutral': Base.cssClasses.NAMESPACE + CONTROL + '--neutral',
 		'brand': Base.cssClasses.NAMESPACE + CONTROL + '--brand',
@@ -19,10 +19,6 @@ const ButtonCore = Lib.merge({}, Base, {
 		'icon-border': Base.cssClasses.NAMESPACE + CONTROL + '--icon-border',
 		'icon-border-filled': Base.cssClasses.NAMESPACE + CONTROL + '--icon-border-filled',
 		'icon-small': Base.cssClasses.NAMESPACE + CONTROL + '--icon-small'
-	},
-
-	_defaultProperties: {
-		type: CONTROL
 	}
 });
 
