19edddc9d6790d11be04792e5b7f3f3646792ae4
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-10-15 12:24:04 -0400

    Tiny bit of checkbox cleanup

--- src/core/checkbox.js
+++ src/core/checkbox.js
@@ -11,7 +11,7 @@ export const CONTROL = 'checkbox';
 const CheckboxCore = Lib.merge({}, Base, Disableable, {
 	cssClasses: {
 		CONTROL: Base.cssClasses.NAMESPACE + CONTROL,
-		CONTROL_FAUX: Base.cssClasses.NAMESPACE + CONTROL + '--faux'
+		FAUX: Base.cssClasses.NAMESPACE + CONTROL + '--faux'
 	},
 
 	_defaultProperties: {
@@ -41,7 +41,7 @@ const CheckboxCore = Lib.merge({}, Base, Disableable, {
 			return;
 		}
 
-		this.setProperties({ 'checked': checked });
+		this.setProperties({ checked });
 
 		if (Lib.isFunction(this._onToggled)) this._onToggled();
 
