c9d5d9157507764dc9b77808eea137df33f3a59a
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-10-15 12:16:46 -0400

    Switch badge to use children instead of text, and add a themes object

--- src/core/badge.js
+++ src/core/badge.js
@@ -3,38 +3,29 @@
 import * as Lib from '../lib/lib';
 import Base from './base';
 
+// Third party
+import classNames from 'classnames';
+
 export const CONTROL = 'badge';
 
 const BadgeCore = Lib.merge({}, Base, {
 	cssClasses: {
-		BASE: 'slds-badge',
-		THEME_DEFAULT: 'slds-theme--default',
-		THEME_SHADE: 'slds-theme--shade',
-		THEME_INVERSE: 'slds-theme--inverse'
+		BASE: 'slds-badge'
+	},
+	
+	themes: {
+		'default': 'slds-theme--default',
+		shade: 'slds-theme--shade',
+		inverse: 'slds-theme--inverse'
 	},
 
 	_defaultProperties: {
 		text: 'label',
-		theme: ''
+		theme: null
 	},
 
-	_getClassNameByTheme: function (theme) {
-		const classNames = [];
-
-		// add base class
-		classNames.push(this.cssClasses.BASE);
-
-		// add class for specific theme
-		switch (theme.toUpperCase()) {
-			case 'DEFAULT':
-			case 'SHADE':
-			case 'INVERSE':
-			default:
-				classNames.push(this.cssClasses['THEME_' + theme.toUpperCase()]);
-				break;
-		}
-
-		return classNames.join(' ');
+	_getClassNames: function () {
+		return classNames(this.cssClasses.BASE, this.themes[this.getProperty('theme')]);
 	}
 });
 
--- src/react/badge/example.js
+++ src/react/badge/example.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Badge from './badge';
 
 export default function () {
-	ReactDOM.render(<Badge text="label1" />, document.getElementById('badge'));
-	ReactDOM.render(<Badge text="label2" theme="default" />, document.getElementById('badge-default'));
-	ReactDOM.render(<Badge text="label3" theme="shade" />, document.getElementById('badge-shade'));
-	ReactDOM.render(<Badge text="label4" theme="inverse" />, document.getElementById('badge-inverse'));
+	ReactDOM.render(<Badge>label1</Badge>, document.getElementById('badge'));
+	ReactDOM.render(<Badge theme="default">label2</Badge>, document.getElementById('badge-default'));
+	ReactDOM.render(<Badge theme="shade">label3</Badge>, document.getElementById('badge-shade'));
+	ReactDOM.render(<Badge theme="inverse">label4</Badge>, document.getElementById('badge-inverse'));
 }
