6e3810849390c3343784846d64feafd329ea5aa6
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-10-15 10:37:55 -0400

    Commented out controls that aren't finished and moved them to the top

--- src/react/examples.js
+++ src/react/examples.js
@@ -1,30 +1,31 @@
 // imports
-import badge from './badge/example';
-import button from './button/example';
-import buttonGroup from './button-group/example';
 // import selectlist from './selectlist/example';
 // import combobox from './combobox/example';
 // import tree from './tree/example';
 // import wizard from './wizard/example';
 // import spinner from './spinner/example';
+// import search from './search/example';
+
+import badge from './badge/example';
+import button from './button/example';
+import buttonGroup from './button-group/example';
 import notification from './notification/example';
 import checkbox from './checkbox/example';
 import radio from './radio/example';
-// import search from './search/example';
 import pillbox from './pillbox/example';
 
 // base components
+// selectlist(document.getElementById('selectlist'));
+// combobox(document.getElementById('combobox'));
+// tree(document.getElementById('tree'));
+// wizard(document.getElementById('wizard'));
+// spinner(document.getElementById('spinner'));
+// search(document.getElementById('search'));
+
 badge();
 button();
 buttonGroup();
 notification();
-
-selectlist(document.getElementById('selectlist'));
-combobox(document.getElementById('combobox'));
-tree(document.getElementById('tree'));
-// wizard(document.getElementById('wizard'));
-spinner(document.getElementById('spinner'));
 checkbox(document.getElementById('slds-checkbox'));
 radio(document.getElementById('slds-radio'));
-// search(document.getElementById('search'));
 pillbox(document.getElementById('pillbox'));
