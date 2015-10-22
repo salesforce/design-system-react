69db8a2aa908463e1f18a9f825a352932dc30a3a
Stephen James <llama@interactivellama.com>
2015-10-22 16:39:09 -0400

    Remove _getIconClassNames from jquery SVG
    It is now in button view core

--- src/jquery/svg.js
+++ src/jquery/svg.js
@@ -6,23 +6,7 @@ import * as Lib from '../lib/lib';
 // Framework specific
 const $ = Lib.global.jQuery || Lib.global.$;
 
-// Third party
-import classNames from 'classnames';
-
 export const SvgObject = {
-
-	_getIconClassNames () {
-		let iconBaseClass;
-
-		if ( this.getProperty('view') ) {
-			iconBaseClass = this.cssClasses.STATEFUL_ICON;
-		} else {
-			iconBaseClass = this.cssClasses.ICON;
-		}
-
-		return classNames(iconBaseClass,
-			!!this.getProperty('text') && this.childIconStyles[ this.getProperty('iconPosition') ]);
-	},
 
 	_renderIcon () {
 		let $icon = undefined;
