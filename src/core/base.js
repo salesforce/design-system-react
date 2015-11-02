b0036f22edc8f2aa5d62116161df5353f089d6be
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-11-02 16:12:37 -0500

    Add a few more comments

--- src/core/base.js
+++ src/core/base.js
@@ -24,7 +24,7 @@ const Base = {
 		strings: {}
 	},
 
-	// We can count on exactly how each facade will handle object construction, so instead we ask that this function be called at an appropriate point by the facade.
+	// We can't count on exactly how each facade will handle object construction, so instead we ask that this function be called at an appropriate point by the facade.
 	_initialize (options) {
 		// `_onBeforeInitialize` is an optional lifecycle event that individual controls / facades may choose to implement. During this step the options are still available and still free to be modified.
 		if (Lib.isFunction(this._onBeforeInitialize)) this._onBeforeInitialize(options);
@@ -38,21 +38,26 @@ const Base = {
 		// Set the current value of the props based on the options passed in. After this point the options are no longer available and any future code should reference only the props.
 		this.setProperties(options);
 
+		// Many controls make use of a collection to define their contents (list items, table rows, etc). If one has been provided for this control we want to wrap it in a `dataAdapter` and save result to `this._collection`.
 		const collection = this.getProperty('collection');
 		if (collection) this._collection = this._getDataAdapter(collection);
 
-		// Run any initializers provided by the control and/or the traits
+		// Run any initializers provided by the control and/or the traits. The special merge function used to mix together controls allows multiple `_onBeforeInitialize`, `_initializer`, and `_onInitialized` hooks to be defined, which will then run in sequence. More information can be found in `Lib.merge`.
 		if (Lib.isFunction(this._initializer)) this._initializer();
 
+		// Kick off the internationalization logic.
 		this._getStrings(strings => {
 			this.setState({
 				strings
 			});
 
+			// Any `_onInitialized` functions defined by the controls or traits will be executed asynchronously after internationalization completes.
 			if (Lib.isFunction(this._onInitialized)) this._onInitialized();
 		});
 	},
-	
+
+	// `onInitialized` is part of the public API. If it has been provided, run it after initialization is complete.
+	// TODO: Determine how to delineate and document API methods and options so that they can be easily collected and displayed together.
 	_onInitialized () {
 		const onInitialized = this.getProperty('onInitialized');
 		
