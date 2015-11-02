49dfea7a9493af3c7a79b252e21a55b6bf78185a
Chris Corwin <ccorwin@salesforce.com>
2015-11-02 18:45:32 -0500

    Removed logging from jquery tree

--- src/jquery/tree/tree.js
+++ src/jquery/tree/tree.js
@@ -71,8 +71,6 @@ let Tree = function Tree () {
 Lib.merge(Tree.prototype, TreeCore, Events, DOM, State, {
 	_initializer () {
 		this.element = this.$el = this.elements.control = this.template.clone().find('.tree');
-
-		console.log('this.element', this.element);
 	},
 	
 	_onInitialized () {
