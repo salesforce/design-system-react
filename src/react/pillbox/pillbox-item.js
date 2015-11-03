6841d8f6c00b08f166dd27b39c9111baab62af41
Stephen James <llama@interactivellama.com>
2015-11-03 14:03:16 -0500

    Audit propTypes: Pillbox

--- src/react/pillbox/pillbox-item.js
+++ src/react/pillbox/pillbox-item.js
@@ -4,7 +4,15 @@ import React from 'react';
 const PillboxItem = React.createClass({
 
 	propTypes: {
-		item: React.PropTypes.object,
+		// TODO: explore if item PropTypes can be done better
+		item: React.PropTypes.shape({
+			// getType: React.PropTypes.func.isRequired,
+			// getDisabled: React.PropTypes.func.isRequired,
+			// getIcon: React.PropTypes.func.isRequired,
+			// getId: React.PropTypes.func.isRequired,
+			getText: React.PropTypes.func.isRequired
+			// getValue: React.PropTypes.func.isRequired
+		}).isRequired,
 		onClick: React.PropTypes.func.isRequired
 	},
 
