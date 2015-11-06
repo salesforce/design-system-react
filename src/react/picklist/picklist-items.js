ac307fe91946745423405219569eb67b010c3174
Stephen James <llama@interactivellama.com>
2015-11-06 17:51:08 -0500

    Update react picklist to SLDS 0.11.0

--- src/react/picklist/picklist-items.js
+++ src/react/picklist/picklist-items.js
@@ -25,9 +25,9 @@ const PicklistItems = React.createClass({
 
 	cssClasses: {
 		DROPDOWN: 'slds-dropdown',
-		LEFT: 'slds-dropdown--left',
+		LEFT: 'slds-dropdown--left slds-dropdown--small',
 		MENU: 'slds-dropdown--menu',
-		LIST: 'slds-dropdown__list'
+		LIST: 'dropdown__list'
 	},
 
 	_menuItems () {
@@ -40,7 +40,7 @@ const PicklistItems = React.createClass({
 
 	render () {
 		return (
-			<div className={classNames(this.cssClasses.DROPDOWN, this.cssClasses.LEFT, this.cssClasses.MENU, {'slds-hide': !this.props.show})}>
+			<div className={classNames(this.cssClasses.DROPDOWN, this.cssClasses.LEFT, {'slds-hide': !this.props.show})}>
 				<ul className={this.cssClasses.LIST} role="menu" ref={this.cssClasses.LIST}>
 				{this._menuItems()}
 				</ul>
