24c4e6158f0c49183f6ad95db61e48febccd8aa1
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2015-11-09 09:33:48 -0500

    Update jQuery picklist template

--- src/jquery/picklist/picklist-template.js
+++ src/jquery/picklist/picklist-template.js
@@ -1,18 +1,19 @@
 export default String.raw`
-<div class="slds-form-element">
-  <div aria-expanded="true" class="slds-picklist">
-    <button class="slds-button slds-button--neutral slds-picklist__label" aria-haspopup="true">
-      <span class="slds-truncate">Select an Option</span>
-      <svg aria-hidden="true" class="slds-icon">
-        <use xlink:href="/assets/design-system/icons/utility-sprite/svg/symbols.svg#down"></use>
-      </svg>
-    </button>
-    <div class="slds-dropdown slds-dropdown--left slds-dropdown--small slds-dropdown--menu slds-hide">
-      <ul class="slds-dropdown__list" role="menu">
-		<li href="#" class="slds-dropdown__item slds-has-icon--left"><a href="#" class="slds-truncate" role="menuitemradio"></a></li>
-      </ul>
-    </div>
-	<input class="slds-hide" readonly aria-hidden="true" type="text"></input>
-  </div>
+<div class="slds-picklist" aria-expanded="true">
+	<button class="slds-button slds-button--neutral slds-picklist__label" aria-haspopup="true">
+		<span class="slds-truncate">Select an Option</span>
+		<svg aria-hidden="true" class="slds-icon">
+			<use xlink:href="/assets/design-system/icons/utility-sprite/svg/symbols.svg#down"></use>
+		</svg>
+	</button>
+	<div class="slds-dropdown slds-dropdown--left slds-dropdown--menu slds-hide">
+		<ul class="slds-dropdown__list" role="menu">
+			<li class="slds-dropdown__item">
+				<a href="#" role="menuitemradio">
+					<p class="slds-truncate"></p>
+				</a>
+			</li>
+		</ul>
+	</div>
 </div>
 `;
