5d76d86411ca498f30a4ccb0bf013391e4c9f679
Donielle Berg <donielle.berg@gmail.com>
2015-11-17 17:24:48 -0800

    Tweak SLDSButton in Modal for icon-inverse variant

--- components/SLDSModal/index.jsx
+++ components/SLDSModal/index.jsx
@@ -213,13 +213,13 @@ module.exports = React.createClass( {
           {this.props.toast}
           <h2 className={cx(titleClass)}>{this.props.title}</h2>
           {this.props.tagline ? <p className="slds-m-top--x-small">{this.props.tagline}</p>:null}
-          <SLDSButton label='Close' variant='icon' iconName='close' iconSize='large' inverse={true} className='slds-modal__close' onClick={this.closeModal} />
+          <SLDSButton label='Close' variant='icon-inverse' iconName='close' iconSize='large' className='slds-modal__close' onClick={this.closeModal} />
         </div>
       )
     }else{
       header = (
         <div style={{position: 'relative'}}>
-          <SLDSButton label='Close' variant='icon' iconName='close' iconSize='large' inverse={true} className='slds-modal__close' onClick={this.closeModal} />
+          <SLDSButton label='Close' variant='icon-inverse' iconName='close' iconSize='large' className='slds-modal__close' onClick={this.closeModal} />
         </div>
       )
     }
