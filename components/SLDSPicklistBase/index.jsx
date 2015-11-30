1f390a532f18dbe0e6eed56fd76fd6ba7b039f86
Donielle Berg <donielle.berg@gmail.com>
2015-11-30 10:39:52 -0800

    Change Proptypes to React.Proptypes in piclist

--- components/SLDSPicklistBase/index.jsx
+++ components/SLDSPicklistBase/index.jsx
@@ -7,7 +7,7 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-import React, {PropTypes} from 'react';
+import React from 'react';
 import isEqual from 'lodash.isequal';
 
 import SLDSPopover from '../SLDSPopover';
@@ -20,9 +20,9 @@ import {KEYS,EventUtil} from '../utils';
 
 const displayName = "SLDSPicklist";
 const propTypes = {
-  onClick: PropTypes.func,
-  onSelect: PropTypes.func.isRequired,
-  onUpdateHighlighted: PropTypes.func
+  onClick: React.PropTypes.func,
+  onSelect: React.PropTypes.func.isRequired,
+  onUpdateHighlighted: React.PropTypes.func
 };
 const defaultProps = {
   className: '',
