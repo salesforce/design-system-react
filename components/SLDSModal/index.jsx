8910d0689249cbd584f0d4662c7c44b8e891d07f
Donielle Berg <donielle.berg@gmail.com>
2015-12-14 15:40:00 -0800

    Update Modal to ES6

--- components/SLDSModal/index.jsx
+++ components/SLDSModal/index.jsx
@@ -7,15 +7,10 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-'use strict';
 
 import React from 'react';
 import SLDSButton from '../SLDSButton';
-import {EventUtil} from '../utils';
-import SLDSSettings from '../SLDSSettings';
-import cx from 'classnames';
-
-
+import classNames from 'classnames';
 import Modal from 'react-modal';
 
 const customStyles = {
@@ -38,34 +33,33 @@ const customStyles = {
   }
 };
 
-module.exports = React.createClass( {
+const displayName = "SLDSModal";
+const propTypes = {
+  align: React.PropTypes.oneOf(['top', 'center']),
+  size: React.PropTypes.oneOf(['medium', 'large']),
+  prompt: React.PropTypes.oneOf(['', 'success', 'warning', 'error', 'wrench', 'offline', 'info'])
+};
+const defaultProps = {
+  align:'center',
+  title:'',
+  tagline:'',
+  isOpen:false,
+  content:[],
+  footer:[],
+  returnFocusTo:null,
+  prompt:'', //if prompt !== '', it renders modal as prompt
+  directional: false
+};
 
-  propTypes: {
-    align: React.PropTypes.oneOf(['top', 'center']),
-    size: React.PropTypes.oneOf(['medium', 'large']),
-    prompt: React.PropTypes.oneOf(['', 'success', 'warning', 'error', 'wrench', 'offline', 'info'])
-  },
+class SLDSModal extends React.Component {
 
-  getDefaultProps () {
-    return {
-      align:'center',
-      title:'',
-      tagline:'',
-      isOpen:false,
-      content:[],
-      footer:[],
-      returnFocusTo:null,
-      prompt:'', //if prompt !== '', it renders modal as prompt
-      directional: false
-    };
-  },
-
-  getInitialState () {
-    return {
+  constructor(props) {
+    super(props);
+    this.state = {
       isClosing: false,
       revealed: false
     };
-  },
+  }
 
   componentDidMount () {
     //console.log('!!! window.activeElement !!! ',document.activeElement);
@@ -76,7 +70,7 @@ module.exports = React.createClass( {
       });
     }
     this.updateBodyScroll();
-  },
+  }
 
   closeModal () {
     this.setState({isClosing: true});
@@ -86,11 +80,11 @@ module.exports = React.createClass( {
     if(this.props.onRequestClose){
       this.props.onRequestClose();
     }
-  },
+  }
 
   handleSubmitModal () {
     this.closeModal();
-  },
+  }
 
   updateBodyScroll () {
     if(window && document && document.body){
@@ -101,23 +95,25 @@ module.exports = React.createClass( {
         document.body.style.overflow = 'inherit';
       }
     }
-  },
+  }
 
-  clearBodyScroll: function updateBodyScroll() {
-    if (window && document && document.body) {
-      document.body.style.overflow = 'inherit';
+  clearBodyScroll() {
+    return function updateBodyScroll() {
+      if (window && document && document.body) {
+        document.body.style.overflow = 'inherit';
+      }
     }
-  },
+  }
 
   handleModalClick(event) {
     if(event && event.stopPropagation){
       event.stopPropagation();
     }
-  },
+  }
 
   isPrompt(){
     return this.props.prompt !== '';
-  },
+  }
 
   getModal() {
     const modalClass = {
@@ -129,7 +125,7 @@ module.exports = React.createClass( {
     const modalStyle = this.props.align === "top"?{ "justify-content": "flex-start" }:null;
 
     return <div
-            className={cx(modalClass)}
+            className={classNames(modalClass)}
             style={{pointerEvents:'inherit'}}
             onClick={this.isPrompt() ? undefined : this.closeModal}
           >
@@ -153,7 +149,7 @@ module.exports = React.createClass( {
           </div>
 
         </div>;
-  },
+  }
 
   render() {
     const overlayClasses = {
@@ -166,11 +162,11 @@ module.exports = React.createClass( {
         isOpen={this.props.isOpen}
         onRequestClose={this.closeModal}
         style={customStyles}
-        overlayClassName={cx(overlayClasses)} >
+        overlayClassName={classNames(overlayClasses)} >
         {this.getModal()}
       </Modal>
     );
-  },
+  }
 
   footerComponent() {
     let footer;
@@ -184,23 +180,23 @@ module.exports = React.createClass( {
     const hasFooter = this.props.footer && this.props.footer.length > 0;
 
     if (hasFooter ) {
-      footer = (<div className={cx(footerClass)}>{this.props.footer}</div>);
+      footer = (<div className={classNames(footerClass)}>{this.props.footer}</div>);
     }
 
     return footer;
-  },
+  }
 
   renderTitle(headingClasses) {
     if(this.props.title){
-      return <h2 className={cx(headingClasses)}>{this.props.title}</h2>;
+      return <h2 className={classNames(headingClasses)}>{this.props.title}</h2>;
     }
-  },
+  }
 
   renderTagline() {
     if(this.props.tagline){
       return <p className="slds-m-top--x-small">{this.props.tagline}</p>;
     }
-  },
+  }
 
   headerComponent() {
     let header;
@@ -219,23 +215,23 @@ module.exports = React.createClass( {
 
     if(hasHeader) {
       header = (
-        <div className={cx(headerClass)}>
-          <SLDSButton assistiveText='Close' variant='icon-inverse' iconName='close' iconSize='large' className='slds-modal__close' onClick={this.closeModal} />
+        <div className={classNames(headerClass)}>
+          <SLDSButton assistiveText='Close' variant='icon-inverse' iconName='close' iconSize='large' className='slds-modal__close' onClick={this.closeModal.bind(this)} />
           {this.props.toast}
-          <h2 className={cx(titleClass)}>{this.props.title}</h2>
+          <h2 className={classNames(titleClass)}>{this.props.title}</h2>
           {this.props.tagline ? <p className="slds-m-top--x-small">{this.props.tagline}</p>:null}
         </div>
       )
     }else{
       header = (
         <div style={{position: 'relative'}}>
-          <SLDSButton assistiveText='Close' variant='icon-inverse' iconName='close' iconSize='large' className='slds-modal__close' onClick={this.closeModal} />
+          <SLDSButton assistiveText='Close' variant='icon-inverse' iconName='close' iconSize='large' className='slds-modal__close' onClick={this.closeModal.bind(this)} />
         </div>
       )
     }
 
     return header;
-  },
+  }
 
   componentDidUpdate (prevProps, prevState) {
 
@@ -262,11 +258,18 @@ module.exports = React.createClass( {
     }
 
 
-  },
+  }
 
   componentWillUnmount () {
     this.clearBodyScroll();
   }
 
 
-});
+}
+
+SLDSModal.displayName = displayName;
+SLDSModal.propTypes = propTypes;
+SLDSModal.defaultProps = defaultProps;
+
+module.exports = SLDSModal;
+
