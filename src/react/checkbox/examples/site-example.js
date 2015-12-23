b8ef1c1ac08a4eef197ba7828b3bb8404421688a
Stephen James <llama@interactivellama.com>
2015-12-23 00:44:11 -0500

    Move react checkbox buttons out into own component

--- src/react/checkbox/examples/site-example.js
+++ src/react/checkbox/examples/site-example.js
@@ -1,73 +1,90 @@
 import React from 'react';
 import {Checkbox} from 'design-system-react';
+import {ExampleEvents} from 'design-system-utilities';
 
 // https://www.lightningdesignsystem.com/components/forms#checkbox
 
 export default React.createClass({
+	componentDidMount () {
+		ExampleEvents.registerEventListener(this, 'checkbox', 'exampleMethod');
+	},
+
 	getInitialState () {
-		const checkboxen = new Map();
-
-		checkboxen.set('checkbox', {disabled: false, checked: true});
-		checkboxen.set('checkbox2', {disabled: false, checked: false});
-
-		return {checkboxen};
+		return {
+			model: {
+				checkbox1: {
+					disabled: false,
+					checked: true
+				},
+				checkbox2: {
+					disabled: false,
+					checked: false
+				}
+			}
+		};
 	},
 
 	render () {
 		return (
-			<div>
-				<div className="slds-col example">
-					<fieldset className="slds-form-element">
-						<legend className="slds-form-element__label slds-form-element__label--top"></legend>
-						<div className="slds-form-element__control">
-							<Checkbox ref="checkbox"
-								checked={this.state.checkboxen.get('checkbox').checked}
-								disabled={this.state.checkboxen.get('checkbox').disabled}
-								labelText="Checked"
-								onChanged={this._handleChange.bind(this, 'checkbox')}
-								onDisabled={this._handleDisable.bind(this, 'checkbox', true)}
-								onEnabled={this._handleDisable.bind(this, 'checkbox', false)}
-								value="checkbox"/>
-						</div>
-						<div className="slds-form-element__control">
-							<Checkbox ref="checkbox2"
-								checked={this.state.checkboxen.get('checkbox2').checked}
-								disabled={this.state.checkboxen.get('checkbox2').disabled}
-								labelText="Unchecked"
-								onChanged={this._handleChange.bind(this, 'checkbox2')}
-								onDisabled={this._handleDisable.bind(this, 'checkbox2', true)}
-								onEnabled={this._handleDisable.bind(this, 'checkbox2', false)}
-								value="checkbox2"/>
-						</div>
-					</fieldset>
+			<fieldset className="slds-form-element">
+				<legend className="slds-form-element__label slds-form-element__label--top"></legend>
+				<div className="slds-form-element__control">
+					<Checkbox ref="checkbox1"
+						checked={this.state.model.checkbox1.checked}
+						disabled={this.state.model.checkbox1.disabled}
+						labelText="Checked"
+						onChanged={this._handleChange.bind(this, 'checkbox1')}
+						onDisabled={this._handleDisable.bind(this, 'checkbox1', true)}
+						onEnabled={this._handleDisable.bind(this, 'checkbox1', false)}
+						value="checkbox"/>
 				</div>
-				<div className="slds-col demo-controls">
-					<div className="slds-button-group" role="group">
-						<button type="button" className="slds-button slds-button--neutral slds-button--x-small" onClick={this._handleClick}>Check</button>
-						<button type="button" className="slds-button slds-button--neutral slds-button--x-small" onClick={this._handleClick}>Uncheck</button>
-						<button type="button" className="slds-button slds-button--neutral slds-button--x-small" onClick={this._handleClick}>Disable</button>
-						<button type="button" className="slds-button slds-button--neutral slds-button--x-small" onClick={this._handleClick}>Enable</button>
-					</div>
+				<div className="slds-form-element__control">
+					<Checkbox ref="checkbox2"
+						checked={this.state.model.checkbox2.checked}
+						disabled={this.state.model.checkbox2.checked.disabled}
+						labelText="Unchecked"
+						onChanged={this._handleChange.bind(this, 'checkbox2')}
+						onDisabled={this._handleDisable.bind(this, 'checkbox2', true)}
+						onEnabled={this._handleDisable.bind(this, 'checkbox2', false)}
+						value="checkbox2"/>
 				</div>
-			</div>
+			</fieldset>
 		);
 	},
 
-	_handleClick (e) {
-		console.log(e.target.firstChild.data.toLowerCase());
-		// translate text of button into method call
-		this.refs.checkbox[e.target.firstChild.data.toLowerCase()]();
-	},
-
 	_handleChange (checkboxName, checkedValue) {
-		const checkbox = this.state.checkboxen.get(checkboxName);
-		checkbox.checked = checkedValue;
-		this.setState(checkbox);
+		const model = this.state.model;
+		model[checkboxName].checked = checkedValue;
+		this.setState(model);
 	},
 
 	_handleDisable (checkboxName, disabledValue) {
-		const checkbox = this.state.checkboxen.get(checkboxName);
-		checkbox.disabled = disabledValue;
-		this.setState(checkbox);
+		const model = this.state.model;
+		model[checkboxName].disabled = disabledValue;
+		this.setState(model);
+	},
+
+	enable () {
+		const model = this.state.model;
+		model.checkbox1.disabled = false;
+		this.setState({model});
+	},
+
+	disable () {
+		const model = this.state.model;
+		model.checkbox1.disabled = true;
+		this.setState({model});
+	},
+
+	check () {
+		const model = this.state.model;
+		model.checkbox1.checked = true;
+		this.setState({model});
+	},
+
+	uncheck () {
+		const model = this.state.model;
+		model.checkbox1.checked = false;
+		this.setState({model});
 	}
 });
