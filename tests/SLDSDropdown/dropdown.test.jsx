09f485f79449dbffef9122108ff6b031c35cec76
Brian Lonsdorf <brian@looprecur.com>
2016-01-11 10:46:17 -0800

    lots of logging

--- tests/SLDSDropdown/dropdown.test.jsx
+++ tests/SLDSDropdown/dropdown.test.jsx
@@ -7,6 +7,7 @@ import {SLDSDropdown} from '../../components'
 
 describe('SLDSDropdown: ',  function(){
 
+  let body;
   const options = [
     {label:'A Option Option Super Super Long',value:'A0'},
     {label:'B Option',value:'B0'},
@@ -20,36 +21,57 @@ describe('SLDSDropdown: ',  function(){
     {label:'E2 Option Super Super Long',value:'E1'}
   ]
 
-  const renderDropdown = inst => TestUtils.renderIntoDocument(inst)
+  const renderDropdown = inst => {
+    body = document.createElement('div');
+    document.body.appendChild(body)
+    const cls = props => <div>{inst}</div>
+    return React.render(React.createElement(cls), body)
+  }
+
   const defaultProps = {label: "Contacts",
                         modal: false,
                         options: options,
                         placeholder: "Select a contact",
                         value: 'C0'}
 
-  const createDropdown = props => React.createElement(SLDSDropdown, props)
+  const createDropdown = props => React.createElement(SLDSDropdown, defaultProps)
 
   const dropItDown = ps => renderDropdown(createDropdown(ps))
 
-  describe('Rendering', function() {
-    let cmp, btn;
+  describe('Rendering', () => {
+    let btn;
+    const getMenu = () => body.querySelector('.slds-dropdown--menu')
 
     beforeEach(() => {
-      cmp = dropItDown({})
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button')
+      dropItDown({})
+      console.log('\n\n\n\n\n\n\n\n ------cmp', body, '\n\n\n\n\n\n')
+      btn = body.querySelector('.slds-button')
     })
 
-    it('gives the button correct aria properties', function() {
-      expect(btn.props['aria-haspopup']).to.equal('true')
+    it('gives the button correct aria properties', () => {
+      expect(btn.props['aria-haspopup']).to.equal('aria-haspopup="true'')
     })
 
-    it('expands the dropdown on hover', function() {
-      const getMenu = () => document.querySelectorAll('slds-dropdown--menu')[0]
+    it('expands the dropdown on hover', () => {
       expect(getMenu()).to.be.falsy
-      Simulate.mouseEnter(btn, {});
+      Simulate.mouseEnter(btn, {})
+      console.log('\n\n\n\n\n\n 1111', getMenu(), '\n\n\n\n\n\n')
       expect(getMenu()).to.be.truthy
-      Simulate.mouseLeave(btn, {});
+      Simulate.mouseLeave(btn, {})
+      console.log('\n\n\n\n\n\n 22222', getMenu(), '\n\n\n\n\n\n')
       expect(getMenu()).to.be.falsy
+    })
+
+    it('selects an option', () => {
+      Simulate.mouseEnter(btn, {})
+      console.log('\n\n\n\n\n\n 3333', getMenu(), '\n\n\n\n\n\n')
+      const items = getMenu().querySelectorAll('.slds-dropdown__item')
+      console.log('\n\n\n\n\n\n', items[2], '\n\n\n\n\n\n')
+      expect(items[2].className).to.not.include('slds-is-selected')
+      Simulate.click(items[2], {})
+      expect(items[2].className).to.include('slds-is-selected')
+      Simulate.click(items[3], {})
+      expect(items[2].className).to.not.include('slds-is-selected')
     })
   })
 })
