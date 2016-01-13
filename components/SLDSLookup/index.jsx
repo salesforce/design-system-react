2e7e088de99d7cc480ca49d16e12f0e84b88f1d7
ibogdanov <ibogdanov@salesforce.com>
2016-01-13 15:56:06 -0800

    minor fix

--- components/SLDSLookup/index.jsx
+++ components/SLDSLookup/index.jsx
@@ -75,7 +75,7 @@ const propTypes = {
 /**
  * A function that takes a term string and an item and returns a truthy value if the item should be kept.
  */
-const defaultProps = (term, item) => {
+const defaultFilter = (term, item) => {
   if(!term) return true;
   return item.label.match(new RegExp(escapeRegExp(term), "ig"));
 };
