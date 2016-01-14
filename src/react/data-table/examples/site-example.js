09103c76f60a862bbbfc902d9f9992d57df5fdfa
Stephen James <llama@interactivellama.com>
2016-01-14 11:47:10 -0500

    Make dataTable use sampleData utility object

--- src/react/data-table/examples/site-example.js
+++ src/react/data-table/examples/site-example.js
@@ -1,65 +1,9 @@
 import React from 'react';
 import {DataTable} from 'design-system-react';
+import {sampleData} from 'design-system-utilities';
+
+// Third-party
 import _ from 'underscore';
-
-const collection = [
-	{
-		id: '8IKZHZZV80',
-		name: 'Item One',
-		count: 100976,
-		lastModified: 'Yesterday',
-		modifiedBy: 'Ashley McDougal'
-	},
-	{
-		id: '5GJOOOPWU7',
-		name: 'Item Two',
-		count: 54976,
-		lastModified: 'Today',
-		modifiedBy: 'Ashley McDougal'
-	},
-	{
-		id: 'Q8Z71ZUCEZ',
-		name: 'Item Three',
-		count: 10128,
-		lastModified: 'Today',
-		modifiedBy: 'Ashley McDougal'
-	},
-	{
-		id: 'WA0Q0XARAR',
-		name: 'Item Four',
-		count: 63616,
-		lastModified: 'Yesterday',
-		modifiedBy: 'Ashley McDougal'
-	},
-	{
-		id: 'N8M7CMNU39',
-		name: 'Item Five',
-		count: 25615,
-		lastModified: 'Yesterday',
-		modifiedBy: 'Steve Daniels'
-	}
-];
-
-const columns = [
-	{
-		propertyName: 'name',
-		displayName: 'Campaign Name',
-		sortable: false,
-		hintParent: false
-	},
-	{
-		propertyName: 'count',
-		displayName: 'Count',
-		sortable: true,
-		hintParent: false
-	},
-	{
-		propertyName: 'lastModified',
-		displayName: 'Last Modified',
-		sortable: true,
-		hintParent: false
-	}
-];
 
 export default React.createClass({
 	propTypes: {
@@ -68,8 +12,8 @@ export default React.createClass({
 
 	getInitialState () {
 		return {
-			collection: collection,
-			columns: columns,
+			collection: sampleData.dataTable.collection,
+			columns: sampleData.dataTable.columns,
 			selection: []
 		};
 	},
