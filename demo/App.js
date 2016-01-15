197a2e636f8f9f0762fc380c7d468c0eb0c74eac
Donielle Berg <donielle.berg@gmail.com>
2016-01-15 14:31:39 -0800

    Update copy in Getting Started

--- demo/App.js
+++ demo/App.js
@@ -5,9 +5,14 @@ const { Route, DefaultRoute, RouteHandler, Link } = Router;
 require('./index.css')
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
-        <RouteHandler/>
+        <RouteHandler />
     );
   }
 }
