6220f44101f048cf2bdc6eae189ad3236afb2802
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-02-02 18:19:21 -0500

    Revert data-table changes for now

--- src/traits/multiselectable.js
+++ src/traits/multiselectable.js
@@ -117,7 +117,7 @@ const Multiselectable = {
 		const selection = Multiselectable.getWrappedImmutableData(controlContext, currentSelection);
 		selection.reset(null);
 
-		Eventable.trigger(controlContext, 'deselect', [], selection);
+		Eventable.trigger(controlContext, 'deselect', null, selection);
 	},
 	
 	toggleItem (controlContext, item, currentSelection) {
