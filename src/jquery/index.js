417ff5a5bab0e872dbdb7dbea03e70d6c07b85f5
Chris Corwin <ccorwin@salesforce.com>
2016-02-17 17:01:24 -0500

    Docs renamed Documentation. Noticed the index page wasn't styled correctly, and that led to me doing the navbar too

--- src/jquery/index.js
+++ src/jquery/index.js
@@ -9,21 +9,139 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-export Badge from './badge/badge';
-export Button from './button/button';
-export ButtonGroup from './button-group/button-group';
-export Checkbox from './checkbox/checkbox';
-export Combobox from './combobox/combobox';
-export DataTable from './data-table/data-table';
-export Datepicker from './datepicker/datepicker';
-export Dropdown from './dropdown/dropdown';
-export Modal from './modal/modal';
-export Notification from './notification/notification';
-export Picklist from './picklist/picklist';
-export Pills from './pills/pills';
-export Popover from './popover/popover';
-export Radio from './radio/radio';
-export Radios from './radio/radios';
-export Spinner from './spinner/spinner';
-export Tooltip from './tooltip/tooltip';
-export Tree from './tree/tree';
+// # jQuery Façade Source Files
+
+// Implements several components from the the [Salesforce Lightning Design System](https://www.lightningdesignsystem.com) in jQuery.
+
+// > See [live examples](/jquery/) of the jQuery Design System Façade in action.
+
+// ## Badges
+// * [Documentation](./badge/badge.html)
+// * [Live Example](/jquery/badge)
+// * [<img src="/assets/demo-site/images/component-examples/badge.png" style="max-height: 200px;" alt="Badge Screenshot">](/jquery/badge)
+export Badge        from './badge';
+// _______________________________________________________________________________________________________
+
+// ## Buttons
+// * [Documentation](./button/button.html)
+// * [Live Example](/jquery/button)
+// * [<img src="/assets/demo-site/images/component-examples/button.png" style="max-height: 200px;" alt="Button Screenshot">](/jquery/button)
+export Button       from './button';
+// _______________________________________________________________________________________________________
+
+// ## Button Views
+// * [Documentation](./button-view/button-view.html)
+export ButtonView   from './button';
+// _______________________________________________________________________________________________________
+
+// ## Button Groups
+// * [Documentation](./button-group/button-group.html)
+// * [Live Example](/jquery/button-group)
+// * [<img src="/assets/demo-site/images/component-examples/button-group.png" style="max-height: 200px;" alt="ButtonGroup Screenshot">](/jquery/button-group)
+export ButtonGroup  from './button-group';
+// _______________________________________________________________________________________________________
+
+// ## Checkbox
+// * [Documentation](./checkbox/checkbox.html)
+// * [Live Example](/jquery/checkbox)
+// * [<img src="/assets/demo-site/images/component-examples/checkbox.png" style="max-height: 200px;" alt="Checkbox Screenshot">](/jquery/checkbox)
+export Checkbox     from './checkbox';
+// _______________________________________________________________________________________________________
+
+// ## Combobox
+// * [Documentation](./combobox/combobox.html)
+// * [Live Example](/jquery/combobox)
+// * [<img src="/assets/demo-site/images/component-examples/combobox.png" style="max-height: 200px;" alt="Combobox Screenshot">](/jquery/combobox)
+export Combobox     from './combobox';
+// _______________________________________________________________________________________________________
+
+// ## Data Tables
+// * [Documentation](./data-table/data-table.html)
+// * [Live Example](/jquery/data-table)
+// * [<img src="/assets/demo-site/images/component-examples/data-table.png" style="max-height: 200px;" alt="DataTable Screenshot">](/jquery/data-table)
+export DataTable    from './data-table';
+// _______________________________________________________________________________________________________
+
+// ## Datepickers
+// * [Documentation](./datepicker/datepicker.html)
+// * [Live Example](/jquery/datepicker)
+// * [<img src="/assets/demo-site/images/component-examples/datepicker.png" style="max-height: 200px;" alt="Datepicker Screenshot">](/jquery/datepicker)
+export Datepicker   from './datepicker';
+// _______________________________________________________________________________________________________
+
+// ## Dropdowns
+// * [Documentation](./dropdown/dropdown.html)
+// * [Live Example](/jquery/dropdown)
+// * [<img src="/assets/demo-site/images/component-examples/dropdown.png" style="max-height: 200px;" alt="Dropdown Screenshot">](/jquery/dropdown)
+export Dropdown     from './dropdown';
+// _______________________________________________________________________________________________________
+
+// ## Modals
+// * [Documentation](./modal/modal.html)
+// * [Live Example](/jquery/modal)
+// * [<img src="/assets/demo-site/images/component-examples/modal.png" style="max-height: 200px;" alt="Modal Screenshot">](/jquery/modal)
+export Modal        from './modal';
+// _______________________________________________________________________________________________________
+
+// ## Notifications
+// * [Documentation](./notification/notification.html)
+// * [Live Example](/jquery/notification)
+// * [<img src="/assets/demo-site/images/component-examples/notification.png" style="max-height: 200px;" alt="Notification Screenshot">](/jquery/notification)
+export Notification from './notification';
+// _______________________________________________________________________________________________________
+
+// ## Picklists
+// * [Documentation](./picklist/picklist.html)
+// * [Live Example](/jquery/picklist)
+// * [<img src="/assets/demo-site/images/component-examples/picklist.png" style="max-height: 200px;" alt="Picklist Screenshot">](/jquery/picklist)
+export Picklist     from './picklist';
+// _______________________________________________________________________________________________________
+
+// ## Pillss
+// * [Documentation](./pills/pills.html)
+// * [Live Example](/jquery/pills)
+// * [<img src="/assets/demo-site/images/component-examples/pills.png" style="max-height: 200px;" alt="Pills Screenshot">](/jquery/pills)
+export Pills        from './pills';
+// _______________________________________________________________________________________________________
+
+// ## Popovers
+// * [Documentation](./popover/popover.html)
+// * [Live Example](/jquery/popover)
+// * [<img src="/assets/demo-site/images/component-examples/popover.png" style="max-height: 200px;" alt="Popover Screenshot">](/jquery/popover)
+export Popover      from './popover';
+// _______________________________________________________________________________________________________
+
+// ## Radio
+// * [Documentation](./radio/radio.html)
+// * [Live Example](/jquery/radio)
+// * [<img src="/assets/demo-site/images/component-examples/radio.png" style="max-height: 200px;" alt="Radio Screenshot">](/jquery/radio)
+export Radio        from './radio';
+// _______________________________________________________________________________________________________
+
+// ## Spinners
+// * [Documentation](./spinner/spinner.html)
+// * [Live Example](/jquery/spinner)
+// * [<img src="/assets/demo-site/images/component-examples/spinner.png" style="max-height: 200px;" alt="Spinner Screenshot">](/jquery/spinner)
+export Spinner      from './spinner';
+// _______________________________________________________________________________________________________
+
+// ## Svgs
+// * [Documentation](./svg/svg.html)
+// * [Live Example](/jquery/svg)
+// * [<img src="/assets/demo-site/images/component-examples/svg.png" style="max-height: 200px;" alt="Svg Screenshot">](/jquery/svg)
+export Svg          from './svg';
+// _______________________________________________________________________________________________________
+
+// ## Tooltips
+// * [Documentation](./tooltip/tooltip.html)
+// * [Live Example](/jquery/tooltip)
+// * [<img src="/assets/demo-site/images/component-examples/tooltip.png" style="max-height: 200px;" alt="Tooltip Screenshot">](/jquery/tooltip)
+export Tooltip      from './tooltip';
+// _______________________________________________________________________________________________________
+
+// ## Trees
+// * [Documentation](./tree/tree.html)
+// * [Live Example](/jquery/tree)
+// * [<img src="/assets/demo-site/images/component-examples/tree.png" style="max-height: 200px;" alt="Tree Screenshot">](/jquery/tree)
+export Tree         from './tree';
+
