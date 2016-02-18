3b8ef91d1d9a6e6af8153274fe2805dd416139c3
Stephen James <llama@interactivellama.com>
2016-02-18 11:18:29 -0500

    Remove old heroku webpack config file

--- webpack.heroku.config.js
+++
@@ -1,4 +0,0 @@
-/* `webpack.heroku.config` is an alias of `webpack.config.site-build`. It is needed by the Heroku build pack located at https://github.com/tweettypography/heroku-buildpack-webpack */
-
-const config = require('./webpack.config.site-build');
-module.exports = config;
