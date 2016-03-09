3f08545f23bfd9ab8ffdc9e7cae2c1d500f27ccc
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-03-09 10:57:28 -0500

    Update the dist readme

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -5,10 +5,6 @@ const coreConfig = require('design-system-facades/webpack.config.dist')
 const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
-const StringReplacePlugin = require('string-replace-webpack-plugin');
-const packageJson = require('./package.json');
-const header = packageJson.humanReadableName + '\n' + 'v' + packageJson.version + '\n';
-const license = fs.readFileSync('./LICENSE', 'utf8');
 
 // `fs.realpathSync` is present to allow symlinked folders to work (such as when using `npm link`) 
 const coreRealPath = fs.realpathSync(path.join(__dirname, 'node_modules/design-system-facades'));
@@ -17,7 +13,7 @@ const coreRealPath = fs.realpathSync(path.join(__dirname, 'node_modules/design-s
 const config = coreConfig;
 
 config.entry = {
-	'slds-for-react': ['./src/dist.js']
+	'slds.bundle': ['./src/dist.js']
 };
 
 // Temporary aliases needed until design-system-facades is renamed
@@ -30,22 +26,6 @@ config.resolve.alias = {
 // Folders to check for ES6 modules
 config.module.loaders[0].include.push(path.join(__dirname, 'node_modules/design-system-facades'));
 config.module.loaders[0].include.push(path.join(__dirname, 'src'));
-
-// Insert version number from package.json into library
-config.module.loaders[0].loaders = ['babel', StringReplacePlugin.replace({
-	replacements: [{
-		pattern: /__VERSION__/g,
-		replacement: function (match, p1, offset, string) {
-			return packageJson.version;
-		}
-	}]
-})];
-
-// Add banner and license to built and bundled files
-config.plugins = [
-	new StringReplacePlugin(),
-	new webpack.BannerPlugin(header + license)
-];
 
 module.exports = config;
 
