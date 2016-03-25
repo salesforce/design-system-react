67410f42cc563e502beb9730cfc7d730d45b5de2
Christopher McCulloh <cmcculloh@exacttarget.com>
2016-03-25 16:03:53 -0400

    (GH47) allows icon background to be set as circular

--- src/icon/examples.js
+++ src/icon/examples.js
@@ -9,6 +9,10 @@ const IconExample = React.createClass({
 		<div className="slds-grid slds-grid--vertical">
 			<div className="slds-col | slds-m-bottom--medium">
 				<Icon category="utility" name="email" inverse={false} />
+				<Icon category="action" name="email" inverse={false} />
+				<Icon category="action" name="email" inverse={false} circle={true} />
+				<Icon category="action" name="email" inverse={false} circle={true} className="slds-icon-text-warning" />
+				<Icon category="standard" name="email" inverse={false} circle={true} className="slds-icon-text-warning" />
 			</div>
 		</div>
 		);
