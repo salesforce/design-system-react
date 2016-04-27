e11c10164ac9b9a6c2b3236dd8a5317d19d2594e
Stephen James <llama@interactivellama.com>
2016-04-27 11:56:19 -0400

    Add Card to demo app

---
+++ demo/code-snippets/CardExamples.js
@@ -0,0 +1,61 @@
+const sampleItems = [{
+  name: 'Cloudhub'}, {
+  name: 'Cloudhub + Anypoint Connectors'},{
+  name: 'Cloud City'}];
+
+const CardExample = React.createClass({
+  displayName: 'CardExample',
+
+  getInitialState () {
+    return {
+      items: sampleItems
+    };
+  },
+
+  render () {
+    const isEmpty = (this.state.items.length === 0);
+
+    return (
+      <div className="slds-grid slds-grid--vertical">
+
+        <SLDSCard
+          id="ExampleCard"
+          filter={
+            !isEmpty && <SLDSCardFilter onChange={this.handleFilterChange} />
+          }
+          headerActions={
+            !isEmpty && <SLDSButton label="Delete All Items" onClick={this.handleDeleteAllItems} />
+          }
+          heading="Releated Items"
+          icon={<SLDSIcon category="standard" name="default" size="small" />}
+          empty={isEmpty ? <SLDSCardEmpty heading="No Related Items">
+              <SLDSButton label="Add Item" onClick={this.handleAddItem} />
+            </SLDSCardEmpty> : null}
+        >
+          <SLDSDataTable items={this.state.items} id="SLDSDataTableExample-1" bordered>
+            <SLDSDataTableColumn
+              label="Opportunity Name"
+              property="name"
+              truncate
+            />
+          </SLDSDataTable>
+        </SLDSCard>
+
+      </div>
+    );
+  },
+
+  handleFilterChange () {
+    console.log('Update Collection');
+  },
+
+  handleDeleteAllItems () {
+    this.setState({items: []});
+  },
+
+  handleAddItem () {
+    this.setState({items: sampleItems});
+  },
+});
+
+ReactDOM.render(<CardExample />, mountNode);
