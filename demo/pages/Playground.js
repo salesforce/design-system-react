cba908edc4fbb61b79fed295c17879abf9783a41
ibogdanov <ibogdanov@salesforce.com>
2016-04-27 12:13:00 -0700

    playground progress

--- demo/pages/Playground.js
+++ demo/pages/Playground.js
@@ -3,6 +3,7 @@ import Router from 'react-router';
 const { Link } = Router;
 import GlobalVariables from 'demo/docs/globalVariables';
 
+import {SLDSLookup} from 'design-system-react';
 
 module.exports = React.createClass({
   render(){
@@ -11,6 +12,22 @@ module.exports = React.createClass({
         <h1 className='slds-text-heading--large'>
           Playground!
         </h1>
+        <hr />
+        <SLDSLookup
+          emptyMessage="No items found"
+          hasError={false}
+          label="Account"
+          onChange={function(newValue){console.log("New search term: ", newValue)}}
+          onSelect={function(item){console.log(item , " Selected")}}
+          options={[
+            {label: "Paddy\"s Pub"},
+            {label: "Tyrell Corp"},
+            {label: "Paper St. Soap Company"},
+            {label: "Nakatomi Investments"},
+            {label: "Acme Landscaping"},
+            {label: "Acme Construction"}
+          ]}
+        />
       </section>
     );
   }
