bc48ca5c9e28f04846f410ca4873c651a9e53b87
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-05-16 11:54:39 -0400

    Point to the correct version of lodash.isFunction

--- components/data-table/header-cell.jsx
+++ components/data-table/header-cell.jsx
@@ -17,7 +17,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 // ### isFunction
-import isFunction from 'lodash/lang/isFunction';
+import isFunction from 'lodash.isfunction';
 
 // ## Children
 
