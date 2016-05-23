dda9f473ebbcfd74631dd91fcd1106c710072a2a
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-05-23 16:12:53 -0400

    Use the standard webpack config for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,41 +1,5 @@
-var path = require('path');
+const baseConfig = require('../webpack.config');
 
-module.exports = {
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-    alias: {
-      'design-system-react': __dirname + '/components',
-      'components': __dirname + '/components',
-      'demo': __dirname + '/demo',
-      'docs': __dirname + '/demo/docs'
-    }
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loaders: ['babel'],
-        include: [
-          path.join(__dirname, 'demo'),
-          path.join(__dirname, 'components')
-        ]
-      },
-      {
-        test: /\.(js|jsx)?$/,
-        loaders: ['babel'],
-        include: [
-          path.join(__dirname, 'tests')
-        ]
-      },
-      {
-        test: /\.css$/,
-        loaders: ['style-loader', 'raw-loader']
-      },
-      {
-        test: /\.(woff|woff2|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }
-    ]
-  },
-  node: { fs: 'empty' }
-};
+const config = Object.assign({}, baseConfig);
+
+module.exports = config;
