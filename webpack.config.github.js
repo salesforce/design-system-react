0975afe8d291fdba5b6e3b823b7b916945e6ed47
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-05-23 12:06:43 -0400

    Remove the demo site files

--- webpack.config.github.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-var webpack = require('webpack');
-var config = require('./webpack.config')();
-
-config.output.path = path.resolve(__dirname, 'dist');
-
-module.exports = config;
