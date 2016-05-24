e71c45b4e6a53bab12b41184a7599a1883f20fd8
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-05-24 11:31:11 -0400

    Icon buttons

--- stories/button/index.jsx
+++ stories/button/index.jsx
@@ -13,9 +13,17 @@ const getButton = props => (
 	/>
 );
 
+const getIconButton = props => getButton({ variant: 'icon', ...props });
+
 storiesOf(BUTTON, module)
 	.addDecorator(getStory => <div className="slds-p-around--medium">{getStory()}</div>)
 	.add('Base', () => getButton({ label: 'Base', variant: 'base' }))
 	.add('Neutral', () => getButton({ label: 'Neutral' }))
 	.add('Neutral Icon', () => getButton({ label: 'Neutral Icon', iconName: 'download', 'iconPosition': 'left' }))
 	.add('Disabled', () => getButton({ label: 'Disabled', variant: 'brand', disabled: true }))
+	.add('Icon Container Small', () => getIconButton({
+		assistiveText: 'Icon Container Small',
+		iconName: 'settings',
+		iconSize: 'small',
+		iconVariant: 'container'
+	}))
