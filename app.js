75a84e1a07f8b2df4a7f0fea9332f2a47b281fa1
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-05-24 13:02:13 -0400

    Switch to a completely custom server

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-var server = require('auth-static');
+var auth = require('basic-auth');
+var compression = require('compression');
+var express = require('express');
 
-server({
-	options: {
-		gzip: true
-	},
-	node_env: process.env.NODE_ENV,
-	username: process.env.AUTH_USERNAME,
-	password: process.env.AUTH_PASSWORD,
-	root: './storybook',
-	port: 9001
+var port = process.env.PORT || 9001;
+
+// Create server
+var app = express();
+
+if (process.env.NODE_ENV === 'production') {
+	// Authentication
+	app.use(function (req, res, next) {
+	  var user = auth(req)
+	  var username = process.env.AUTH_USERNAME;
+	  var password = process.env.AUTH_PASSWORD;
+
+	  var unauthorized = function () {
+	    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+	    res.sendStatus(401);
+	  }
+
+	  if (!username || !password) return unauthorized();
+	  if (!user || user.name !== username || user.pass !== password) {
+	    return unauthorized();
+	  }
+
+	  next();
+	});
+}
+
+// Compress all files
+app.use(compression());
+
+// Static directory
+app.use(express.static(__dirname + '/storybook'));
+
+// Listen
+var server = app.listen(port, function() {
+	console.log('Server listening on port ', server.address().port);
 });
