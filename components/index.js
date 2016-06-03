a92547d27485470c6351a171e348b444cc305656
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-06-03 01:41:42 -0400

    Release commit for 0.0.30-commonjs [ci skip]

---
+++ components/index.js
@@ -0,0 +1,180 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.UtilityIcon = exports.SLDSUtilityIcon = exports.Timepicker = exports.SLDSTimepicker = exports.PopoverTooltip = exports.SLDSPopoverTooltip = exports.PageHeader = exports.SLDSPageHeader = exports.Notification = exports.SLDSNotification = exports.ModalTrigger = exports.SLDSModalTrigger = exports.Modal = exports.SLDSModal = exports.SLDSMenuPicklist = exports.Dropdown = exports.SLDSMenuDropdown = exports.Lookup = exports.SLDSLookup = exports.Input = exports.SLDSInput = exports.ButtonIcon = exports.SLDSButtonIcon = exports.Icon = exports.SLDSIcon = exports.Datepicker = exports.SLDSDatepicker = exports.DataTableRowActions = exports.SLDSDataTableRowActions = exports.DataTableColumn = exports.SLDSDataTableColumn = exports.DataTableCell = exports.SLDSDataTableCell = exports.DataTable = exports.SLDSDataTable = exports.Checkbox = exports.SLDSCheckbox = exports.CardFilter = exports.SLDSCardFilter = exports.CardEmpty = exports.SLDSCardEmpty = exports.Card = exports.SLDSCard = exports.BreadCrumb = exports.SLDSBreadCrumb = exports.ButtonStateful = exports.SLDSButtonStateful = exports.ButtonGroup = exports.SLDSButtonGroup = exports.Button = exports.SLDSButton = exports.SLDSSettings = undefined;
+
+var _SLDSSettings2 = require('./SLDSSettings');
+
+var _SLDSSettings3 = _interopRequireDefault(_SLDSSettings2);
+
+var _button = require('./button');
+
+var _button2 = _interopRequireDefault(_button);
+
+var _buttonGroup = require('./button-group');
+
+var _buttonGroup2 = _interopRequireDefault(_buttonGroup);
+
+var _buttonStateful = require('./button-stateful');
+
+var _buttonStateful2 = _interopRequireDefault(_buttonStateful);
+
+var _breadCrumb = require('./bread-crumb');
+
+var _breadCrumb2 = _interopRequireDefault(_breadCrumb);
+
+var _card = require('./card');
+
+var _card2 = _interopRequireDefault(_card);
+
+var _empty = require('./card/empty');
+
+var _empty2 = _interopRequireDefault(_empty);
+
+var _filter = require('./card/filter');
+
+var _filter2 = _interopRequireDefault(_filter);
+
+var _checkbox = require('./forms/checkbox');
+
+var _checkbox2 = _interopRequireDefault(_checkbox);
+
+var _dataTable = require('./data-table');
+
+var _dataTable2 = _interopRequireDefault(_dataTable);
+
+var _cell = require('./data-table/cell');
+
+var _cell2 = _interopRequireDefault(_cell);
+
+var _column = require('./data-table/column');
+
+var _column2 = _interopRequireDefault(_column);
+
+var _rowActions = require('./data-table/row-actions');
+
+var _rowActions2 = _interopRequireDefault(_rowActions);
+
+var _datePicker = require('./date-picker');
+
+var _datePicker2 = _interopRequireDefault(_datePicker);
+
+var _icon = require('./icon');
+
+var _icon2 = _interopRequireDefault(_icon);
+
+var _buttonIcon = require('./icon/button-icon');
+
+var _buttonIcon2 = _interopRequireDefault(_buttonIcon);
+
+var _input = require('./forms/input');
+
+var _input2 = _interopRequireDefault(_input);
+
+var _lookup = require('./lookup');
+
+var _lookup2 = _interopRequireDefault(_lookup);
+
+var _menuDropdown = require('./menu-dropdown');
+
+var _menuDropdown2 = _interopRequireDefault(_menuDropdown);
+
+var _menuPicklist = require('./menu-picklist');
+
+var _menuPicklist2 = _interopRequireDefault(_menuPicklist);
+
+var _modal = require('./modal');
+
+var _modal2 = _interopRequireDefault(_modal);
+
+var _trigger = require('./modal/trigger');
+
+var _trigger2 = _interopRequireDefault(_trigger);
+
+var _notification = require('./notification');
+
+var _notification2 = _interopRequireDefault(_notification);
+
+var _pageHeader = require('./page-header');
+
+var _pageHeader2 = _interopRequireDefault(_pageHeader);
+
+var _popoverTooltip = require('./popover-tooltip');
+
+var _popoverTooltip2 = _interopRequireDefault(_popoverTooltip);
+
+var _timePicker = require('./time-picker');
+
+var _timePicker2 = _interopRequireDefault(_timePicker);
+
+var _utilityIcon = require('./utilities/utility-icon');
+
+var _utilityIcon2 = _interopRequireDefault(_utilityIcon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.SLDSSettings = _SLDSSettings3.default; /*
+                                               Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+                                               
+                                               Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+                                               Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+                                               Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+                                               Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+                                               
+                                               THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+                                               */
+
+exports.SLDSButton = _button2.default;
+exports.Button = _button2.default;
+exports.SLDSButtonGroup = _buttonGroup2.default;
+exports.ButtonGroup = _buttonGroup2.default;
+exports.SLDSButtonStateful = _buttonStateful2.default;
+exports.ButtonStateful = _buttonStateful2.default;
+exports.SLDSBreadCrumb = _breadCrumb2.default;
+exports.BreadCrumb = _breadCrumb2.default;
+exports.SLDSCard = _card2.default;
+exports.Card = _card2.default;
+exports.SLDSCardEmpty = _empty2.default;
+exports.CardEmpty = _empty2.default;
+exports.SLDSCardFilter = _filter2.default;
+exports.CardFilter = _filter2.default;
+exports.SLDSCheckbox = _checkbox2.default;
+exports.Checkbox = _checkbox2.default;
+exports.SLDSDataTable = _dataTable2.default;
+exports.DataTable = _dataTable2.default;
+exports.SLDSDataTableCell = _cell2.default;
+exports.DataTableCell = _cell2.default;
+exports.SLDSDataTableColumn = _column2.default;
+exports.DataTableColumn = _column2.default;
+exports.SLDSDataTableRowActions = _rowActions2.default;
+exports.DataTableRowActions = _rowActions2.default;
+exports.SLDSDatepicker = _datePicker2.default;
+exports.Datepicker = _datePicker2.default;
+exports.SLDSIcon = _icon2.default;
+exports.Icon = _icon2.default;
+exports.SLDSButtonIcon = _buttonIcon2.default;
+exports.ButtonIcon = _icon2.default;
+exports.SLDSInput = _input2.default;
+exports.Input = _input2.default;
+exports.SLDSLookup = _lookup2.default;
+exports.Lookup = _lookup2.default;
+exports.SLDSMenuDropdown = _menuDropdown2.default;
+exports.Dropdown = _menuDropdown2.default;
+exports.SLDSMenuPicklist = _menuPicklist2.default;
+exports.SLDSMenuPicklist = _menuPicklist2.default;
+exports.SLDSModal = _modal2.default;
+exports.Modal = _modal2.default;
+exports.SLDSModalTrigger = _trigger2.default;
+exports.ModalTrigger = _trigger2.default;
+exports.SLDSNotification = _notification2.default;
+exports.Notification = _notification2.default;
+exports.SLDSPageHeader = _pageHeader2.default;
+exports.PageHeader = _pageHeader2.default;
+exports.SLDSPopoverTooltip = _popoverTooltip2.default;
+exports.PopoverTooltip = _popoverTooltip2.default;
+exports.SLDSTimepicker = _timePicker2.default;
+exports.Timepicker = _timePicker2.default;
+exports.SLDSUtilityIcon = _utilityIcon2.default;
+exports.UtilityIcon = _utilityIcon2.default;
---
+++ utilities/constants.js
@@ -0,0 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// ## Components
+var BREAD_CRUMB = exports.BREAD_CRUMB = 'SLDSBreadCrumb';
+var BUTTON = exports.BUTTON = 'SLDSButton';
+var BUTTON_GROUP = exports.BUTTON_GROUP = 'SLDSButtonGroup';
+var BUTTON_STATEFUL = exports.BUTTON_STATEFUL = 'SLDSButtonStateful';
+var CARD = exports.CARD = 'SLDSCard';
+var CARD_BODY = exports.CARD_BODY = 'SLDSCardBody';
+var CARD_EMPTY = exports.CARD_EMPTY = 'SLDSCardEmpty';
+var CARD_FILTER = exports.CARD_FILTER = 'SLDSCardFilter';
+var CARD_FOOTER = exports.CARD_FOOTER = 'SLDSCardFooter';
+var CARD_HEADER = exports.CARD_HEADER = 'SLDSCardHeader';
+var CONTEXT_BAR = exports.CONTEXT_BAR = 'SLDSContextBar';
+var CONTEXT_BAR_DROPDOWN = exports.CONTEXT_BAR_DROPDOWN = 'SLDSContextBarDropdown';
+var CONTEXT_BAR_ITEM_WRAPPER = exports.CONTEXT_BAR_ITEM_WRAPPER = 'SLDSContextBarItemWrapper';
+var CONTEXT_BAR_LINK = exports.CONTEXT_BAR_LINK = 'SLDSContextBarLink';
+var CONTEXT_BAR_NAV_GROUP = exports.CONTEXT_BAR_NAV_GROUP = 'SLDSContextBarNavGroup';
+var CONTEXT_BAR_TITLE = exports.CONTEXT_BAR_TITLE = 'SLDSContextBarTitle';
+var DATA_TABLE = exports.DATA_TABLE = 'SLDSDataTable';
+var DATA_TABLE_CELL = exports.DATA_TABLE_CELL = 'SLDSDataTableCell';
+var DATA_TABLE_COLUMN = exports.DATA_TABLE_COLUMN = 'SLDSDataTableColumn';
+var DATA_TABLE_HEAD = exports.DATA_TABLE_HEAD = 'SLDSDataTableHead';
+var DATA_TABLE_HEADER_CELL = exports.DATA_TABLE_HEADER_CELL = 'SLDSDataTableHeaderCell';
+var DATA_TABLE_ROW_ACTIONS = exports.DATA_TABLE_ROW_ACTIONS = 'SLDSDataTableRowActions';
+var DATA_TABLE_ROW = exports.DATA_TABLE_ROW = 'SLDSDataTableRow';
+var DATE_PICKER = exports.DATE_PICKER = 'SLDSDatePicker';
+var FORMS_CHECKBOX = exports.FORMS_CHECKBOX = 'SLDSCheckbox';
+var FORMS_INPUT = exports.FORMS_INPUT = 'SLDSInput';
+var GRID = exports.GRID = 'SLDSGrid';
+var ICON = exports.ICON = 'SLDSIcon';
+var ICON_BUTTON = exports.ICON_BUTTON = 'SLDSIconButton';
+var ICON_INPUT = exports.ICON_INPUT = 'SLDSIconInput';
+var LOOKUP = exports.LOOKUP = 'SLDSLookup';
+var MEDIA_OBJECT = exports.MEDIA_OBJECT = 'SLDSMediaObject';
+var MENU_DROPDOWN = exports.MENU_DROPDOWN = 'SLDSMenuDropdown';
