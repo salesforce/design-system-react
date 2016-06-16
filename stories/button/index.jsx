329cb4b071fc9af4b3f400e4ececd355ed93bb67
ibogdanov <ibogdanov@salesforce.com>
2016-06-16 13:20:59 -0700

    better markup for disabled button with tooltip

--- stories/button/index.jsx
+++ stories/button/index.jsx
@@ -5,12 +5,22 @@ import { storiesOf, action } from '@kadira/storybook';
 
 import { BUTTON } from '../../utilities/constants';
 import Button from '../../components/button';
+import ButtonGroup from '../../components/button-group';
+import Tooltip from '../../components/popover-tooltip';
 
 const getButton = props => (
 	<Button
 		{...props}
 		onClick={action('click')}
 	/>
+);
+
+const wrapWithButtonGroup = props => (
+	<ButtonGroup>
+		{ getButton({ label: 'Neutral Icon', iconName: 'download', 'iconPosition': 'left' }) }
+		{ getButton(props) }
+		{ getButton({ label: 'Neutral Icon', iconName: 'download', 'iconPosition': 'left' }) }
+	</ButtonGroup>
 );
 
 const getIconButton = props => getButton({ variant: 'icon', ...props });
@@ -21,6 +31,8 @@ storiesOf(BUTTON, module)
 	.add('Neutral', () => getButton({ label: 'Neutral' }))
 	.add('Neutral Icon', () => getButton({ label: 'Neutral Icon', iconName: 'download', 'iconPosition': 'left' }))
 	.add('Disabled', () => getButton({ label: 'Disabled', variant: 'brand', disabled: true }))
+	.add('Disabled With Tooltip', () => getButton({ label: 'Disabled', variant: 'brand', disabled: true, tooltip:<Tooltip content="YES!" align="bottom" /> }))
+	.add('Disabled With Tooltip inside ButtonGroup', () => wrapWithButtonGroup({ label: 'Disabled', variant: 'brand', disabled: true, tooltip:<Tooltip content="YES!" align="bottom" /> } ))
 	.add('Icon Container Small', () => getIconButton({
 		assistiveText: 'Icon Container Small',
 		iconName: 'settings',
