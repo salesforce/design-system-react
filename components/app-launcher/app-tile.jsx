c04e451ce753af15480c0083ba6f57fe918931c3
Eric Willhoit <iowillhoit@gmail.com>
2016-07-06 11:44:30 -0500

    app launcher tiles refactor

--- components/app-launcher/app-tile.jsx
+++ components/app-launcher/app-tile.jsx
@@ -1,16 +1,84 @@
-import Icon from 'slds-for-react/icon';
-import IconUtility from '../slds-for-react/icon-utility.js';
-import Tooltip from '../slds-for-react/tooltip.js';
-import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// # App Launcher Component
+
+// ## Dependencies
+
+// ### React
 import React from 'react';
-import reactMixin from 'react-mixin';
+
+// Removes the need for `React.PropTypes.prop`
+const { PropTypes } = React;
+
+// ## Children
+import Icon from '../icon';
+// import IconUtility from '../slds-for-react/icon-utility.js';
+import Tooltip from '../popover-tooltip';
+// import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
+// import reactMixin from 'react-mixin';
 import classNames from 'classnames';
 
+// ## Constants
+import { APP_LAUNCHER_TILE } from '../../utilities/constants';
 
-class AppTile extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {
+const AppTile = React.createClass({
+	// ### Display Name
+	// Always use the canonical component name as the React display name.
+	displayName: APP_LAUNCHER_TILE,
+
+	// ### Prop Types
+	propTypes: {
+		/**
+		 * The localized text for the "More information" tooltip.
+		 */
+		localizedShowMoreTooltip: PropTypes.string,
+		/**
+		 * The localized text for an uninstalled app's "Learn More" link.
+		 */
+		localizedLearnMoreLink: PropTypes.string,
+		/**
+		 * The localized text for the "Not Installed" modal section.
+		 */
+		localizedNotInstalledText: PropTypes.string,
+		appName: PropTypes.string.isRequired,
+		appDescription: PropTypes.string.isRequired,
+		appTileClicked: PropTypes.func,
+		categoryName: PropTypes.string.isRequired,
+		categoryIcon: PropTypes.object.isRequired,
+		hasAccess: PropTypes.bool,
+		isProvisioned: PropTypes.bool,
+		isVisible: PropTypes.bool,
+		onClick: PropTypes.func,
+		applicationUrl: PropTypes.string,
+		productUrl: PropTypes.string
+	},
+	// constructor (props) {
+	// 	super(props);
+	// 	// this.state = {
+	// 	// 	tooltip: {
+	// 	// 		content: this.props.appDescription,
+	// 	// 		trigger: 'click',
+	// 	// 		positionedTargetVerticalAttachment: 'bottom-left',
+	// 	// 		isOpen: false,
+	// 	// 		modal: false,
+	// 	// 		container: document.querySelector('body'),
+	// 	// 		positionedOffset: 20
+	// 	// 	}
+	// 	// };
+	// }
+
+	getInitialState () {
+		return {
+			isOpen: false,
 			tooltip: {
 				content: this.props.appDescription,
 				trigger: 'click',
@@ -21,32 +89,31 @@ class AppTile extends React.Component {
 				positionedOffset: 20
 			}
 		};
+	},
 
-		this._moreRendered = this._moreRendered.bind(this);
-		this._handleMoreClick = this._handleMoreClick.bind(this);
-		this._getDescriptionClass = this._getDescriptionClass.bind(this);
-		this._handleTileClick = this._handleTileClick.bind(this);
-		this._pageClick = this._pageClick.bind(this);
-		this._mouseDownHandler = this._mouseDownHandler.bind(this);
-		this._mouseUpHandler = this._mouseUpHandler.bind(this);
-	}
+	getDefaultProps () {
+		return {
+			localizedShowMoreTooltip: 'More',
+			localizedLearnMoreLink: 'Learm More'
+		};
+	},
 
-	_moreRendered (element) {
+	moreRendered (element) {
 		const tooltipState = this.state.tooltip;
 		tooltipState.alignmentTarget = element;
 		this.setState(tooltipState);
-	}
+	},
 
-	_handleMoreClick (e) {
+	handleMoreClick (e) {
 		e.stopPropagation();
 		e.nativeEvent.stopImmediatePropagation();
 
 		const tooltipState = this.state.tooltip;
 		tooltipState.isOpen = !(tooltipState.isOpen);
 		this.setState(tooltipState);
-	}
+	},
 
-	_handleTileClick (e) {
+	handleTileClick (e) {
 		if (typeof this.props.onClick !== 'function') {
 			if (this.props.installed) {
 				window.location = this.props.applicationUrl;
@@ -60,30 +127,30 @@ class AppTile extends React.Component {
 		}
 
 		this.props.appTileClicked(e, this.props.uninstalled);
-	}
+	},
 
 	// Don't apply multiline truncate class until modal is visible. Otherwise it will not work -cdmc
-	_getDescriptionClass () {
+	getDescriptionClass () {
 		return this.props.isVisible ? 'slds-multiline-truncate' : '';
-	}
+	},
 
-	_renderLockIcon () {
+	renderLockIcon () {
 		return (
 			<span className="locked-icon slds-icon__container slds-m-around--none slds-p-around--none">
 				<Icon category="utility" name="lock" className="slds-icon slds-icon--x-small" />
 			</span>
 		);
-	}
+	},
 
 	isEnabled () {
 		return this.props.isProvisioned && this.props.hasAccess;
-	}
+	},
 
 	componentDidMount () {
-		window.addEventListener('mousedown', this._pageClick, false);
-	}
+		window.addEventListener('mousedown', this.pageClick, false);
+	},
 
-	_pageClick () {
+	pageClick () {
 		if (this.mouseIsDownThis) {
 			return;
 		}
@@ -91,27 +158,27 @@ class AppTile extends React.Component {
 		const tooltipState = this.state.tooltip;
 		tooltipState.isOpen = false;
 		this.setState(tooltipState);
-	}
+	},
 
-	_mouseDownHandler () {
+	mouseDownHandler () {
 		this.mouseIsDownThis = true;
-	}
+	},
 
-	_mouseUpHandler () {
+	mouseUpHandler () {
 		this.mouseIsDownThis = false;
-	}
+	},
 
-	_renderMore (description, tooltipState) {
+	renderMore (description, tooltipState) {
 		const displayMore = (description.length > 50);
 		const more = (
 			<button
 				className="slds-icon_container slds-tile--board__icon slds-button"
-				ref={this._moreRendered}
-				onClick={this._handleMoreClick}
-				onMouseDown={this._mouseDownHandler}
-				onMouseUp={this._mouseUpHandler}
+				ref={this.moreRendered}
+				onClick={this.handleMoreClick}
+				onMouseDown={this.mouseDownHandler}
+				onMouseUp={this.mouseUpHandler}
 			>
-				<span>{this.props.localization.APP_LAUNCHER_MORE}</span>
+				<span>{this.props.localizedShowMoreTooltip}</span>
 				<Tooltip {...tooltipState}>
 					<span>{tooltipState.content}</span>
 				</Tooltip>
@@ -119,27 +186,27 @@ class AppTile extends React.Component {
 		);
 
 		return displayMore ? more : '';
-	}
+	},
 
 	render () {
 		const tooltipState = this.state.tooltip;
 		const tileStyle = this.isEnabled() ? 'enabled' : 'non-enabled';
-		const descriptionLearnMore = <span className="learn_more">{this.props.localization.APP_LAUNCHER_LEARN_MORE}</span>;
+		const descriptionLearnMore = <span className="learn_more">{this.props.localizedLearnMoreLink}</span>;
 		const description = this.isEnabled() ? this.props.appDescription : descriptionLearnMore;
-		const ribbon = this.isEnabled() ? '' : <div className="ribbon">{this.props.localization.APP_LAUNCHER_NOT_INSTALLED}</div>;
-		const lockedIcon = this.isEnabled() ? '' : this._renderLockIcon();
+		const ribbon = this.isEnabled() ? '' : <div className="ribbon">{this.props.localizedNotInstalledText}</div>;
+		const lockedIcon = this.isEnabled() ? '' : this.renderLockIcon();
 
 		const iconWrapperPadding = this.props.categoryIcon.noPadding ? 'slds-p-around--none' : 'slds-p-around--small';
 		const iconWrapperClasses = classNames('slds-icon__container slds-m-around--small', iconWrapperPadding);
 		const iconClasses = this.props.categoryIcon.noPadding ? 'slds-icon slds-icon--x-large' : 'slds-icon';
 
-		const more = this._renderMore(description, tooltipState);
+		const more = this.renderMore(description, tooltipState);
 
 		return (
 			<li className="slds-size--1-of-3">
 				<a
 					href="#"
-					onClick={this._handleTileClick}
+					onClick={this.handleTileClick}
 					className={classNames('slds-list__item slds-m-around--x-small slds-p-around--none', tileStyle)}
 				>
 					<div className="slds-media slds-tile--board slds-tile slds-m-around--xxx-small">
@@ -150,7 +217,7 @@ class AppTile extends React.Component {
 								className={iconWrapperClasses}
 								style={{ background: this.props.categoryIcon.background }}
 							>
-								<IconUtility
+								<Icon
 									{...this.props.categoryIcon}
 									className={iconClasses}
 								/>
@@ -159,7 +226,7 @@ class AppTile extends React.Component {
 						<div className="slds-media__body slds-m-vertical--x-small slds-m-horizontal--small">
 							<p className="tile__title slds-truncate slds-text-body--regular">{this.props.appName}</p>
 							<div className="slds-tile__detail slds-text-body--small slds-has-flexi-truncate">
-								<p className={this._getDescriptionClass()}>
+								<p className={this.getDescriptionClass()}>
 									<span className="slds-text-heading--label">{this.props.categoryName} </span>
 									{description}
 								</p>
@@ -171,25 +238,6 @@ class AppTile extends React.Component {
 			</li>
 		);
 	}
-}
+});
 
-AppTile.displayName = 'AppTile';
-
-AppTile.propTypes = {
-	localization: React.PropTypes.object.isRequired,
-	appName: React.PropTypes.string.isRequired,
-	appDescription: React.PropTypes.string.isRequired,
-	appTileClicked: React.PropTypes.func,
-	categoryName: React.PropTypes.string.isRequired,
-	categoryIcon: React.PropTypes.object.isRequired,
-	hasAccess: React.PropTypes.bool,
-	isProvisioned: React.PropTypes.bool,
-	isVisible: React.PropTypes.bool,
-	onClick: React.PropTypes.func,
-	applicationUrl: React.PropTypes.string,
-	productUrl: React.PropTypes.string
-};
-
-reactMixin(AppTile.prototype, PureRenderMixin);
-
-export default AppTile;
+module.exports = AppTile;
--- components/app-launcher/app-tiles.jsx
+++ components/app-launcher/app-tiles.jsx
@@ -1,28 +1,71 @@
-import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// # App Launcher Tiles Component
+
+// ## Dependencies
+
+// ### React
 import React from 'react';
-import reactMixin from 'react-mixin';
 
-import AppTile from './app-tile.js';
+// Removes the need for `React.PropTypes.prop`
+const { PropTypes } = React;
 
-class AppTiles extends React.Component {
-	constructor (props) {
-		super(props);
-		this.state = {};
-	}
+// ## Children
+import AppTile from './app-tile';
+
+// ## Constants
+import { APP_LAUNCHER_TILES } from '../../utilities/constants';
+
+const AppTiles = React.createClass({
+	// ### Display Name
+	// Always use the canonical component name as the React display name.
+	displayName: APP_LAUNCHER_TILES,
+
+	// ### Prop Types
+	propTypes: {
+		/**
+		 * The localized text for the "More to Explore" App Launcher section.
+		 */
+		localizedMoreToExploreText: PropTypes.string,
+		/**
+		 * The localized text for the "Not Installed" subheader.
+		 */
+		localizedNotInstalledText: PropTypes.string,
+		appTileClicked: PropTypes.func,
+		collection: PropTypes.array.isRequired,
+		isVisible: PropTypes.bool,
+		search: PropTypes.string,
+		filterForSearch: PropTypes.func.isRequired
+	},
+
+	getDefaultProps () {
+		return {
+			localizedMoreToExploreText: 'More to Explore',
+			localizedNotInstalledText: 'Not Installed'
+		};
+	},
 
 	renderAppTiles (collection) {
 		return collection.map(
 			(item, i) => (
 				<AppTile
 					appTileClicked={this.props.appTileClicked}
-					localization={this.props.localization}
 					isVisible={this.props.isVisible}
 					{...item}
 					key={i}
 				/>
 			)
 		);
-	}
+	},
 
 	render () {
 		let collection = this.props.collection;
@@ -42,8 +85,8 @@ class AppTiles extends React.Component {
 				</ul>
 				<hr />
 				<h2 className="slds-text-heading--small slds-m-left--small">
-					{this.props.localization.APP_LAUNCHER_MORE_TO_EXPLORE} ({uninstalledApps.length})
-					<div className="slds-text-heading--label slds-p-top--xxx-small">{this.props.localization.APP_LAUNCHER_NOT_INSTALLED}</div>
+					{this.props.localizedMoreToExploreText} ({uninstalledApps.length})
+					<div className="slds-text-heading--label slds-p-top--xxx-small">{this.props.localizedNotInstalledText}</div>
 				</h2>
 				<ul className="slds-list--horizontal slds-has-cards--space has-selections slds-wrap">
 					{this.renderAppTiles(uninstalledApps)}
@@ -51,19 +94,6 @@ class AppTiles extends React.Component {
 			</div>
 		);
 	}
-}
+});
 
-AppTiles.propTypes = {
-	appTileClicked: React.PropTypes.func,
-	collection: React.PropTypes.array.isRequired,
-	isVisible: React.PropTypes.bool,
-	localization: React.PropTypes.object.isRequired,
-	search: React.PropTypes.string,
-	filterForSearch: React.PropTypes.func.isRequired
-};
-
-AppTiles.displayName = 'AppTiles';
-
-reactMixin(AppTiles.prototype, PureRenderMixin);
-
-export default AppTiles;
+module.exports = AppTiles;
