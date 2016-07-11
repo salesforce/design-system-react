aa91863f44397a170d97d8ec06edee73185437e3
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-07-11 17:11:41 -0400

    Update the dropdown test to more accurate test keyboard behavior

--- tests/menu-picklist/picklistBase.test.jsx
+++ tests/menu-picklist/picklistBase.test.jsx
@@ -1,3 +1,6 @@
+/* eslint-env mocha */
+/* eslint-disable prefer-arrow-callback */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import assign from 'lodash.assign';
@@ -6,237 +9,238 @@ import { expect } from 'chai';
 
 import {SLDSMenuPicklist} from '../../components';
 const { Simulate,
-        scryRenderedDOMComponentsWithTag,
-        findRenderedDOMComponentWithClass } = TestUtils;
+				scryRenderedDOMComponentsWithTag,
+				findRenderedDOMComponentWithClass } = TestUtils;
 
-describe('SLDSMenuPicklist: ',  function(){
-  let body;
+describe.only('SLDSMenuPicklist: ',	function(){
+	let body;
 
-  const options = [
-    {
-      label: 'A Option Option Super Super Long',
-      value: 'A0', 
-      title: 'Greg'},
-    {
-      label: 'B Option',
-      value: 'B0'
-    }
-  ];
+	const options = [
+		{
+			label: 'A Option Option Super Super Long',
+			value: 'A0',
+			title: 'Greg'},
+		{
+			label: 'B Option',
+			value: 'B0'
+		}
+	];
 
-  const renderPicklist = inst => {
-    body = document.createElement('div');
-    document.body.appendChild(body);
-    return ReactDOM.render(inst, body);
-  }
+	const renderPicklist = inst => {
+		body = document.createElement('div');
+		document.body.appendChild(body);
+		return ReactDOM.render(inst, body);
+	}
 
-  function removePicklist () {
-    ReactDOM.unmountComponentAtNode(body);
-    document.body.removeChild(body);
-  }
-
-
-  const defaultProps = { 
-    modal: false,
-    options: options,
-    placeholder: 'Select a contact',
-    value: 'C0' 
-  }
-
-  const createPicklist = props => React.createElement(SLDSMenuPicklist, assign({}, defaultProps, props));
-
-  const getPicklist = props => renderPicklist(createPicklist(props));
-  const getMenu = dom => dom.querySelector('.slds-dropdown--left');
-
-  const clickOnItem = (cmp, index) => {
-    const items = scryRenderedDOMComponentsWithTag(cmp, 'a');
-    Simulate.click(items[index]);
-  }
-
-  describe('in modal mode', () => {
-    let cmp, btn;
-
-    beforeEach(() => {
-      cmp = getPicklist({modal: true})
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
-    })
-
-    afterEach(() => {
-      removePicklist();
-    })
-
-    it('expands/contracts the dropdown on click', (done) => {
-      expect(getMenu(document.body)).to.equal(null);
-      Simulate.click(btn, {});
-      setTimeout(() => {
-        expect(getMenu(document.body).className).to.include('slds-dropdown--left');
-        Simulate.click(btn, {});
-        expect(getMenu(document.body)).to.equal(null);
-        done();
-      }, 600);
-    })
-  });
+	function removePicklist () {
+		ReactDOM.unmountComponentAtNode(body);
+		document.body.removeChild(body);
+	}
 
 
-  describe('with click handler', () => {
-    let cmp, btn, clicked;
+	const defaultProps = {
+		modal: false,
+		options: options,
+		placeholder: 'Select a contact',
+		value: 'C0'
+	}
 
-    beforeEach(() => {
-      clicked = false;
-      cmp = getPicklist({onClick: () => clicked = true });
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
-    })
+	const createPicklist = props => React.createElement(SLDSMenuPicklist, assign({}, defaultProps, props));
 
-    afterEach(() => {
-      removePicklist();
-    })
+	const getPicklist = props => renderPicklist(createPicklist(props));
+	const getMenu = dom => dom.querySelector('.slds-dropdown--left');
 
-    it('gives the button correct aria properties', () => {
-      expect(btn.props['aria-haspopup']).to.equal("true");
-    })
+	const clickOnItem = (cmp, index) => {
+		const items = scryRenderedDOMComponentsWithTag(cmp, 'a');
+		Simulate.click(items[index]);
+	}
 
-    it('sets the placeholder', () => {
-      expect(btn.textContent).to.equal('Select a contact');
-    })
+	describe('in modal mode', () => {
+		let cmp, btn;
 
-    it('expands/contracts the dropdown on click', () => {
-      expect(getMenu(body)).to.equal(null)
-      Simulate.click(btn, {});
-      expect(getMenu(body).className).to.include('slds-dropdown');
-      Simulate.click(btn, {});
-      expect(getMenu(body)).to.equal(null);
-    })
+		beforeEach(() => {
+			cmp = getPicklist({modal: true})
+			btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
+		})
 
-    it('preserves click behavior', () => {
-      expect(clicked).to.be.false
-      Simulate.click(btn, {})
-      expect(clicked).to.be.true
-    })
-  })
+		afterEach(() => {
+			removePicklist();
+		})
 
-  describe('expanded with onSelect', () => {
-    let cmp, btn, selected;
-
-    beforeEach(() => {
-      selected = false;
-      cmp = getPicklist({onSelect: i => selected = i });
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
-      Simulate.click(btn, {});
-    })
-
-    afterEach(() => {
-      removePicklist();
-    })
+		it('expands/contracts the dropdown on click', (done) => {
+			expect(getMenu(document.body)).to.equal(null);
+			Simulate.click(btn, {});
+			setTimeout(() => {
+				expect(getMenu(document.body).className).to.include('slds-dropdown--left');
+				Simulate.click(btn, {});
+				expect(getMenu(document.body)).to.equal(null);
+				done();
+			}, 600);
+		})
+	});
 
 
-    it('selects an item', () => {
-      expect(selected).to.be.false;
-      const items = getMenu(body).querySelectorAll('.slds-dropdown__item');
-      Simulate.click(items[1].querySelector('a'), {});
-      expect(selected.value).to.equal('B0');
-    })
+	describe('with click handler', () => {
+		let cmp, btn, clicked;
 
-  })
+		beforeEach(() => {
+			clicked = false;
+			cmp = getPicklist({onClick: () => clicked = true });
+			btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
+		})
 
-  describe('disabled', () => {
-    let cmp, btn, clicked;
+		afterEach(() => {
+			removePicklist();
+		})
 
-    beforeEach(() => {
-      clicked = false;
-      cmp = getPicklist({disabled: true, onClick: () => clicked = true });
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
-    })
+		it('gives the button correct aria properties', () => {
+			expect(btn.props['aria-haspopup']).to.equal("true");
+		})
 
-    afterEach(() => {
-      removePicklist();
-    })
+		it('sets the placeholder', () => {
+			expect(btn.textContent).to.equal('Select a contact');
+		})
 
+		it('expands/contracts the dropdown on click', () => {
+			expect(getMenu(body)).to.equal(null)
+			Simulate.click(btn, {});
+			expect(getMenu(body).className).to.include('slds-dropdown');
+			Simulate.click(btn, {});
+			expect(getMenu(body)).to.equal(null);
+		})
 
-    it("doesn't open", () => {
-      Simulate.click(btn, {});
-      expect(getMenu(body)).to.equal(null);
-    })
+		it('preserves click behavior', () => {
+			expect(clicked).to.be.false
+			Simulate.click(btn, {})
+			expect(clicked).to.be.true
+		})
+	})
 
-    it('prevents click behavior', () => {
-      expect(clicked).to.be.false;
-      Simulate.click(btn, {})
-      expect(clicked).to.be.false;
-    })
-  })
+	describe('expanded with onSelect', () => {
+		let cmp, btn, selected;
 
-  describe('accessible markup', () => {
-    let cmp, btn, selected;
+		beforeEach(() => {
+			selected = false;
+			cmp = getPicklist({onSelect: i => selected = i });
+			btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
+			Simulate.click(btn, {});
+		})
 
-    beforeEach(() => {
-      selected = false;
-      cmp = getPicklist({onSelect: i => selected = i });
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
-    })
-
-    afterEach(() => {
-      removePicklist();
-    })
-
-
-    it('<ul> has role menu & aria-labelledby', () => {
-      Simulate.click(btn, {});
-      let ulRole = getMenu(body).querySelector('ul').getAttribute('role');
-      let ulAria = getMenu(body).querySelector('ul').getAttribute('aria-labelledby');
-      expect(ulRole).to.equal('menu');
-      expect(ulAria).to.equal('Picklist_Button');
-    })
-
-    it('<a> inside <li> has role menuitem', () => {
-      Simulate.click(btn, {});
-      const items = getMenu(body).querySelectorAll('.slds-dropdown__item a')
-      let anchorRole = items[1].getAttribute('role');
-      let match = (anchorRole === 'menuitem' || anchorRole === 'menuitemradio' || anchorRole === 'menuitemcheckbox');
-      expect(match).to.be.true;
-    })
-  })
-
-  describe('Keyboard behavior', () => {
-    let cmp, btn, selected;
-
-    beforeEach(() => {
-      selected = false;
-      cmp = getPicklist({onSelect: i => selected = i });
-      btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
-    })
-
-    afterEach(() => {
-      removePicklist();
-    })
-
-    it('opens menu with enter', () => {
-      expect(getMenu(body)).to.equal(null);
-      Simulate.keyDown(btn, {key: "Enter", keyCode: 13, which: 13});
-      expect(getMenu(body)).to.not.equal(null);
-    })
-
-    it('opens menu with down arrow key', () => {
-      expect(getMenu(body)).to.equal(null)
-      Simulate.keyDown(btn, {key: "Down", keyCode: 40, which: 40});
-      expect(getMenu(body)).to.not.equal(null)
-    })
-
-    it('selects an item with keyboard', () => {
-      Simulate.click(btn, {})
-      expect(selected).to.be.false;
-      let menuItems = getMenu(body).querySelectorAll('.slds-dropdown__item');
-      Simulate.keyDown(menuItems[1].querySelector('a'), {key: "Enter", keyCode: 13, which: 13});
-      expect(selected.value).to.equal('B0');
-    })
-
-    it('closes Menu on esc', () => {
-      expect(getMenu(body)).to.equal(null);
-      Simulate.click(btn, {});
-      expect(getMenu(body)).to.not.equal(null);
-      let menuItems = getMenu(body).querySelectorAll('.slds-dropdown__item');
-      Simulate.keyDown(menuItems[1].querySelector('a'), {key: "Esc", keyCode: 27, which: 27});
-      expect(getMenu(body)).to.equal(null);
-    })
-
-  })
+		afterEach(() => {
+			removePicklist();
+		})
 
 
+		it('selects an item', () => {
+			expect(selected).to.be.false;
+			const items = getMenu(body).querySelectorAll('.slds-dropdown__item');
+			Simulate.click(items[1].querySelector('a'), {});
+			expect(selected.value).to.equal('B0');
+		})
+
+	})
+
+	describe('disabled', () => {
+		let cmp, btn, clicked;
+
+		beforeEach(() => {
+			clicked = false;
+			cmp = getPicklist({disabled: true, onClick: () => clicked = true });
+			btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
+		})
+
+		afterEach(() => {
+			removePicklist();
+		})
+
+
+		it("doesn't open", () => {
+			Simulate.click(btn, {});
+			expect(getMenu(body)).to.equal(null);
+		})
+
+		it('prevents click behavior', () => {
+			expect(clicked).to.be.false;
+			Simulate.click(btn, {})
+			expect(clicked).to.be.false;
+		})
+	})
+
+	describe('accessible markup', () => {
+		let cmp, btn, selected;
+
+		beforeEach(() => {
+			selected = false;
+			cmp = getPicklist({onSelect: i => selected = i });
+			btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
+		})
+
+		afterEach(() => {
+			removePicklist();
+		})
+
+
+		it('<ul> has role menu & aria-labelledby', () => {
+			Simulate.click(btn, {});
+			let ulRole = getMenu(body).querySelector('ul').getAttribute('role');
+			let ulAria = getMenu(body).querySelector('ul').getAttribute('aria-labelledby');
+			expect(ulRole).to.equal('menu');
+			expect(ulAria).to.equal('Picklist_Button');
+		})
+
+		it('<a> inside <li> has role menuitem', () => {
+			Simulate.click(btn, {});
+			const items = getMenu(body).querySelectorAll('.slds-dropdown__item a')
+			let anchorRole = items[1].getAttribute('role');
+			let match = (anchorRole === 'menuitem' || anchorRole === 'menuitemradio' || anchorRole === 'menuitemcheckbox');
+			expect(match).to.be.true;
+		})
+	})
+
+	describe('Keyboard behavior', () => {
+		let cmp, btn, selected;
+
+		beforeEach(() => {
+			selected = false;
+			cmp = getPicklist({onSelect: i => selected = i });
+			btn = findRenderedDOMComponentWithClass(cmp, 'slds-button');
+		})
+
+		afterEach(() => {
+			removePicklist();
+		})
+
+		it('opens menu with enter', () => {
+			expect(getMenu(body)).to.equal(null);
+			Simulate.keyDown(btn, {key: "Enter", keyCode: 13, which: 13});
+			expect(getMenu(body)).to.not.equal(null);
+		})
+
+		it('opens menu with down arrow key', () => {
+			expect(getMenu(body)).to.equal(null)
+			Simulate.keyDown(btn, {key: "Down", keyCode: 40, which: 40});
+			expect(getMenu(body)).to.not.equal(null)
+		})
+
+		it('selects an item with keyboard', () => {
+			Simulate.click(btn, {});
+			expect(selected).to.be.false;
+
+			const menu = getMenu(body);
+			Simulate.keyDown(menu, { key: "Down", keyCode: 40, which: 40 });
+			Simulate.keyDown(menu, { key: "Down", keyCode: 40, which: 40 });
+			Simulate.keyDown(menu, {key: "Enter", keyCode: 13, which: 13});
+			expect(selected.value).to.equal('B0');
+		})
+
+		it('closes Menu on esc', () => {
+			expect(getMenu(body)).to.equal(null);
+			Simulate.click(btn, {});
+			expect(getMenu(body)).to.not.equal(null);
+			let menuItems = getMenu(body).querySelectorAll('.slds-dropdown__item');
+			Simulate.keyDown(menuItems[1].querySelector('a'), {key: "Esc", keyCode: 27, which: 27});
+			expect(getMenu(body)).to.equal(null);
+		})
+
+	})
 });
