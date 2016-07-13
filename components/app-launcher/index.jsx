05ddeef1cdbe803290e76b678c31e87008d6c44a
Eric Willhoit <iowillhoit@gmail.com>
2016-07-13 13:34:01 -0500

    app launcher wrapper

--- components/app-launcher/index.jsx
+++ components/app-launcher/index.jsx
@@ -9,7 +9,7 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-// # App Launcher Component
+// # App Launcher
 
 // ## Dependencies
 
@@ -19,18 +19,14 @@ import React from 'react';
 // Removes the need for `React.PropTypes.prop`
 const { PropTypes } = React;
 
+// ### isFunction
+import isFunction from 'lodash.isfunction';
+
 // ## Children
-import Modal from './modal';
 import Button from '../button';
-// import Dropdown from 'slds-for-react/dropdown';
-// import functionBinder from '../utilities/function-binder.js';
-// import Menu from 'slds-for-react/menu';
-// import PureRenderMixin from 'react/lib/ReactComponentWithPureRenderMixin';
-// import ReactDOM from 'react-dom';
-// import reactMixin from 'react-mixin';
-// import PreviewMenuItems from './preview-menu-items';
-// import Search from '../search';
-// import Trigger from 'slds-for-react/dropdown/button-trigger';
+import Icon from '../icon';
+import Modal from '../modal';
+import Search from '../forms/input/search';
 
 // ## Constants
 import { APP_LAUNCHER } from '../../utilities/constants';
@@ -43,232 +39,153 @@ const AppLauncher = React.createClass({
 	// ### Prop Types
 	propTypes: {
 		/*
+		 * Assistive text for app launcher icon
+		 */
+		appLauncherIconAssistiveText: PropTypes.string,
+		/*
+		 * Set the header button's text
+		 */
+		buttonLabel: PropTypes.string,
+		/*
+		 * Callback fired when button
+		 */
+		buttonOnClick: PropTypes.func,
+		/*
+		 * All of the App Launcher's children
+		 */
+		children: PropTypes.node.isRequired,
+		/*
+		 * Callback fired when search value changes
+		 */
+		onSearch: PropTypes.func.isRequired,
+		/*
 		 * Set the App Launcher's title text (for localization)
 		 */
-		appLauncherTitle: PropTypes.string,
+		title: PropTypes.string,
+		// ///////////////////////////
+		// ///////////////////////////
+		// ///////////////////////////
+		/*
+		 * Callback when the App Launcher icon is clicked
+		 */
+		onAppLauncherIconClick: PropTypes.func,
+
 		/*
 		 * Set the search input's placeholder text (for localization)
 		 */
 		searchPlaceholderText: PropTypes.string,
-		/*
-		 * Set the App Exchange Button's text (for localization)
-		 */
-		appExchangeButtonText: PropTypes.string,
-		appTileClicked: PropTypes.func,
-		collection: PropTypes.array.isRequired,
-		filterForSearch: PropTypes.func,
+		isOpen: PropTypes.bool
+		// collection: PropTypes.array.isRequired,
 		// localization: PropTypes.object.isRequired,
-		modalIsOpen: PropTypes.bool,
-		modalSearch: PropTypes.string,
-		onModalClose: PropTypes.func,
-		onModalOpen: PropTypes.func,
-		onDropdownClose: PropTypes.func,
-		onDropdownOpen: PropTypes.func,
-		onSearchChange: PropTypes.func,
-		onModalSearchChange: PropTypes.func,
-		search: PropTypes.string,
-		data: PropTypes.object
+		// onClose: PropTypes.func,
+		// onCancel: PropTypes.func,
+		// trigger: PropTypes.object,
+		// filterForSearch: PropTypes.func.isRequired,
+		// search: PropTypes.string,
 	},
 
-	handleDropdownClose () {
-		if (this.props.onDropdownClose) {
-			this.props.onDropdownClose();
+	// TODO: Optional header node
+
+// 			<AppLauncher
+// 				title="App Launcher"
+// 				searchLabel="Find an app"
+// 				onSearch={()=>{}}
+// 				buttonLabel="App Exchange"
+// 				buttonOnClick={()=>{}}
+// 				modalClassName=""
+// 			>
+// 				<AppLauncherSection title="All Apps" className="">
+// 					<AppLauncherTile
+// 						icon={<Icon />} or iconName="" or iconText=""
+// 						title=""
+// 						description=""
+// 						size="small"
+// 						moreLabel=""
+// 						onClick={}
+// 						className=""
+// 					>
+// 						{children}
+// 					</AppLauncherTile>
+// 					<AppLauncherTile />
+// 					<AppLauncherTile />
+// 					<AppLauncherTile />
+// 				</AppLauncherSection>
+// 			</AppLauncher>
+
+	getDefaultProps () {
+		// TODO: pass 'collapseSectionAssistiveText' down from parent
+		return {
+			appLauncherIconAssistiveText: 'Open App Launcher',
+			buttonLabel: 'App Exchange',
+			isOpen: false,
+			searchPlaceholderText: 'Find an app',
+			title: 'App Launcher'
+		};
+	},
+
+	getInitialState () {
+		return {
+			isOpen: this.props.isOpen
+		};
+	},
+
+	openAppLauncher () {
+		this.setState({ isOpen: true });
+
+		if (isFunction(this.props.onAppLauncherIconClick)) {
+			this.props.onAppLauncherIconClick();
 		}
 	},
 
-	handleDropdownOpen () {
-		if (this.props.onDropdownOpen) {
-			this.props.onDropdownOpen();
-		}
-		// TODO: Figure out why this doesn't work. -cdmc
-		ReactDOM.findDOMNode(this.refs['app-launcher__search'].refs.input).focus();
+	closeAppLauncher () {
+		this.setState({ isOpen: false });
 	},
 
-	handleSearchChange (value, e) {
-		e.stopPropagation();
-		// TODO: why does search act so wonky within a dropdown that three event handlers and this value check is needed...
-		if (value !== this.props.search && this.props.onSearchChange) {
-			this.props.onSearchChange(value, e);
-		}
-	},
-
-	handleSearchInputClick (e) {
-		e.nativeEvent.stopImmediatePropagation();
-	},
-
-	handleTriggerClick (e) {
-		if (this.props.onModalOpen) {
-			this.props.onModalOpen(e);
-		}
-	},
-
-	/*
-	 * itemCollection - collection of items to search/filter
-	 * searchableFields - which fields in the itemCollection should be searched against
-	 * queryString - text to use to perform search
-	 */
-	defaultFilterForSearch (itemCollection, searchableFields, queryString) {
-		let queryTerms = [queryString].concat(queryString.split(' '));
-
-		// strip out spaces and blank characters left by split -cdmc
-		queryTerms = queryTerms.filter((term) => (term !== ' ' && term.length > 0));
-
-		const queryExpressions = queryTerms.map((term) => new RegExp(term, 'gi'));
-
-		const results = [];
-		/*
-			Make sure we prioritize itemCollection instead of queryExpressions, otherwise
-			full string matches will take precedence over item order, meaning:
-
-			A search for "Social Studio" against ["Email Studio", "Social Studio"]
-			would return ["Social Studio", "Email Studio"] -cdmc
-		*/
-		itemCollection.forEach((item) => {
-			queryExpressions.forEach((queryExpression) => {
-				searchableFields.forEach((field) => {
-					if (item[field] && item[field].search(queryExpression) > -1) {
-						results.push(item);
-					}
-				});
-			});
-		});
-
-		// TODO: Sort each results array further? -cdmc
-
-		// Remove duplicates -cdmc
-		const rankedSearchResults = [...new Set(results)];
-
-		return rankedSearchResults;
-	},
-
-	/*
-	 * This filter is much more intelligent, in that it prioritizes not only full-string matches,
-	 * but also allows you to rank search-able fields in a weighted order.
-	 *
-	 * If you pass in [a, b] for searchableFields, matches against a will rank higher in search
-	 * than matches for b. -cdmc
-	 */
-	defaultRankedFilterForSearch (itemCollection, searchableFields, queryString) {
-		let queryTerms = [queryString].concat(queryString.split(' '));
-
-		// strip out spaces and blank characters left by split -cdmc
-		queryTerms = queryTerms.filter((term) => (term !== ' ' && term.length > 0));
-
-		const queryExpressions = queryTerms.map((term) => new RegExp(term, 'gi'));
-
-		// instantiate results object and create empty arrays for each field to match against
-		const results = {};
-		searchableFields.forEach((field) => { results[field] = []; });
-
-		/*
-			Make sure we prioritize queryExpressions instead of itemCollection, otherwise
-			item order will take precedence over full string matches, meaning:
-
-			A search for "Social Studio" against ["Email Studio", "Social Studio"]
-			would return ["Email Studio", "Social Studio"] because all regex
-			(/Social Studio/gi, /Social/gi, /Studio/gi) would be run against
-			"Email Studio" first, and then run against "Social Studio". Both
-			return matches, so, "Email Studio" would then appear first in the
-			search results, which is inappropriate.
-
-			Meaning: we need to loop through search Expressions and then
-			iterate over all itemCollection for each expression in order to get properly
-			prioritized results (the way this is written, a refactor could do
-			it differently I suppose) -cdmc
-		*/
-		queryExpressions.forEach((queryExpression) => {
-			itemCollection.forEach((item) => {
-				searchableFields.forEach((field) => {
-					if (item[field] && item[field].search(queryExpression) > -1) {
-						results[field].push(item);
-					}
-				});
-			});
-		});
-
-		// TODO: Sort each results array further? -cdmc
-
-		// Flatten out the results, keeping ranked-order intact -cdmc
-		let combinedResults = [];
-		for (const resultSet of Object.keys(results)) {
-			combinedResults = combinedResults.concat(results[resultSet]);
-		}
-
-		// Remove duplicates -cdmc
-		const rankedSearchResults = [...new Set(combinedResults)];
-
-		return rankedSearchResults;
-	},
-
-	/*
-	 * Contains business rules for whether an app is considered "installed" or not,
-	 * and if it should be displayed in waffle menu.
-	 */
-	populateAppVisibility (collection) {
-		collection.forEach((item) => {
-			const isInstalled = (item.hasAccess && item.isProvisioned && item.isVisibleImh);
-			const isUninstalled = ((!item.hasAccess || !item.isProvisioned) && item.isVisibleImh);
-
-			item.installed = isInstalled;
-			item.uninstalled = isUninstalled;
-			item.visibleInAppLauncherMenu = (
-				isInstalled &&
-				item.appName.toLowerCase() !== 'pulse' && //TODO: add attribute (onlyModal) to data structure MCGLOBALNV-269
-				item.appName.toLowerCase() !== 'playbooks'
-			);
-		});
-
-		return collection;
-	},
-
-	// getDefaultProps () {
-	// 	return {
-
-	// 	};
-	// },
-
-	// ### Render
 	render () {
-		const sharedFilterDefinition = this.props.filterForSearch || this.defaultFilterForSearch;
-
-		const collection = this.populateAppVisibility(this.props.collection);
-		// let filteredCollection = collection;
-
-		// if (this.props.search) {
-		// 	filteredCollection = sharedFilterDefinition(filteredCollection, ['appName', 'categoryName'], this.props.search);
-		// }
-
-		// const dropdownCollection = filteredCollection.filter((item) => (item.visibleInAppLauncherMenu));
-
-		// // max visible apps out at 10.
-		// if (dropdownCollection.length > 10) {
-		// 	dropdownCollection.length = 10;
-		// }
-
-		// const matchingText = (dropdownCollection.length === 1) ?
-		// 	this.props.localization.APP_LAUNCHER_MATCHING_ITEM :
-		// 	this.props.localization.APP_LAUNCHER_MATCHING_ITEMS;
-
-		// const dropdownTitle = (this.props.search.length > 0) ?
-		// 	`${dropdownCollection.length} ${matchingText}` :
-		// 	this.props.localization.APP_LAUNCHER_MARKETING_CLOUD_APPS;
-
 		return (
-			<div className="slds-global-nav__app-launcher">
+			<div>
+				<div className="slds-context-bar__icon-action">
+					<a
+						href="#"
+						aria-haspopup="true"
+						className="slds-button slds-button--icon slds-context-bar__button"
+						onClick={this.openAppLauncher}
+					>
+						<Icon
+							category="utility"
+							name="apps"
+							size="medium"
+							inverse={false}
+							assistiveText={this.props.appLauncherIconAssistiveText}
+						/>
+					</a>
+				</div>
+
 				<Modal
-					appLauncherTitle={this.props.appLauncherTitle}
-					searchPlaceholderText={this.props.searchPlaceholderText}
-					appExchangeButtonText={this.props.appExchangeButtonText}
-					appTileClicked={this.props.appTileClicked}
-					collection={collection}
-					data={this.props.data}
-					modalIsOpen={this.props.modalIsOpen}
-					onClose={this.props.onModalClose}
-					trigger={this.refs.trigger}
-					filterForSearch={sharedFilterDefinition}
-					search={this.props.modalSearch}
-					onSearchChange={this.props.onModalSearchChange}
-				/>
+					isOpen={this.state.isOpen}
+					onRequestClose={this.closeAppLauncher}
+					size="large"
+				>
+					<div className="slds-modal__header slds-app-launcher__header slds-grid slds-grid--align-spread slds-grid--vertical-align-center">
+						<h2 className="slds-text-heading--medium">{this.props.title}</h2>
+						<div className="slds-app-launcher__header-search">
+							<Search
+								id="app-launcher-search"
+								onChange={this.props.onSearch}
+								assistiveText={this.props.searchPlaceholderText}
+								placeholder={this.props.searchPlaceholderText}
+							/>
+						</div>
+						<Button
+							label={this.props.buttonLabel}
+							buttonOnClick
+						/>
+					</div>
+
+					<div className="slds-modal__content slds-app-launcher__content slds-p-around--medium">
+						{this.props.children}
+					</div>
+				</Modal>
 			</div>
 		);
 	}
--- stories/app-launcher/index.jsx
+++ stories/app-launcher/index.jsx
@@ -3,7 +3,11 @@ import { storiesOf, action } from '@kadira/storybook';
 
 import { APP_LAUNCHER } from '../../utilities/constants';
 
+const { PropTypes } = React;
+
+import AppLauncher from '../../components/app-launcher';
 import AppLauncherTile from '../../components/app-launcher/tile';
+import AppLauncherSection from '../../components/app-launcher/section';
 import Icon from '../../components/icon';
 
 // const DemoAppLauncher = React.createClass({
@@ -55,15 +59,19 @@ const smallTileDemoStyles = {
 const DemoAppLauncherTile = React.createClass({
 	displayName: 'DemoAppLauncherTile',
 
+	propTypes: {
+		search: PropTypes.string
+	},
+
 	render () {
 		return (
-			<div style={standardTileDemoStyles}>
-				<AppLauncherTile
-					title="Marketing Cloud"
-					description="Send emails, track emails, read emails! Emails!"
-					onClick={action('Tile clicked!')}
-				/>
-			</div>
+			<AppLauncherTile
+				title="Marketing Cloud"
+				iconText="MC"
+				description="Send emails, track emails, read emails! Emails!"
+				onClick={action('Tile clicked!')}
+				search={this.props.search}
+			/>
 		);
 	}
 });
@@ -73,13 +81,12 @@ const DemoAppLauncherSmallTile = React.createClass({
 
 	render () {
 		return (
-			<div style={smallTileDemoStyles}>
-				<AppLauncherTile
-					title="Journey Builder"
-					size="small"
-					onClick={action('Tiny tile clicked!')}
-				/>
-			</div>
+			<AppLauncherTile
+				title="Journey Builder"
+				iconText="JB"
+				size="small"
+				onClick={action('Tiny tile clicked!')}
+			/>
 		);
 	}
 });
@@ -87,35 +94,143 @@ const DemoAppLauncherSmallTile = React.createClass({
 const DemoAppLauncherTileWithIconNode = React.createClass({
 	displayName: 'DemoAppLauncherTileWithIconNode',
 
+	propTypes: {
+		search: PropTypes.string
+	},
 
 	render () {
 		const icon = <Icon name="map" category="action" className="slds-m-around--x-small" />;
 
 		return (
-			<div style={standardTileDemoStyles}>
-				<AppLauncherTile
-					title="Sales Cloud"
-					description="The primary internal Salesforce org."
-					iconNode={icon}
-					onClick={action('Tile with icon node clicked!')}
-				/>
+			<AppLauncherTile
+				title="Sales Cloud"
+				description="The primary internal Salesforce org."
+				iconNode={icon}
+				onClick={action('Tile with icon node clicked!')}
+				search={this.props.search}
+			/>
+		);
+	}
+});
+
+const DemoAppLauncherTileWithIconText = React.createClass({
+	displayName: 'DemoAppLauncherTileWithIconText',
+
+	propTypes: {
+		search: PropTypes.string
+	},
+
+	render () {
+		return (
+			<AppLauncherTile
+				title="Sales Cloud"
+				description="The primary internal Salesforce org."
+				iconText="SC"
+				onClick={action('Tile with icon text clicked!')}
+				search={this.props.search}
+			/>
+		);
+	}
+});
+
+const DemoAppLauncherTileWithTruncatedText = React.createClass({
+	displayName: 'DemoAppLauncherTileWithTruncatedText',
+
+	propTypes: {
+		search: PropTypes.string
+	},
+
+	render () {
+		return (
+			<AppLauncherTile
+				title="Call Center"
+				description="The key to call center and contact center is not to use too many words!"
+				iconText="CC"
+				onClick={action('Tile with icon text clicked!')}
+				search={this.props.search}
+			/>
+		);
+	}
+});
+
+const DemoAppLauncherTileWithSearchText = React.createClass({
+	displayName: 'DemoAppLauncherTileWithSearchText',
+
+	propTypes: {
+		search: PropTypes.string
+	},
+
+	getDefaultProps () {
+		return {
+			search: 'all'
+		};
+	},
+
+	render () {
+		return <DemoAppLauncherTileWithTruncatedText search={this.props.search} />;
+	}
+});
+
+const DemoAppLauncherSection = React.createClass({
+	displayName: 'DemoAppLauncherSection',
+
+	render () {
+		return (
+			<div>
+				<AppLauncherSection title="All Items" onToggleClick={action('Section `All Items` open -->')}>
+					<DemoAppLauncherTile />
+					<DemoAppLauncherTileWithIconText />
+					<DemoAppLauncherTileWithIconNode />
+				</AppLauncherSection>
+				<AppLauncherSection title="All Apps" onToggleClick={action('Section `All App` open -->')}>
+					<DemoAppLauncherTile />
+					<DemoAppLauncherTileWithIconNode />
+				</AppLauncherSection>
 			</div>
 		);
 	}
 });
-	// 	<AppLauncherTile
-	// 	icon={<Icon />} or iconName="" or iconText=""
-	// 	title=""
-	// 	description=""
-	// 	size="small"
-	// 	moreLabel=""
-	// 	onClick={}
-	// 	className=""
-	// >
+
+const DemoAppLauncher = React.createClass({
+	displayName: 'DemoAppLauncher',
+
+	getInitialState () {
+		return {
+			search: ''
+		};
+	},
+
+	onSearch (event) {
+		this.setState({ search: event.target.value });
+	},
+
+	render () {
+		return (
+			<AppLauncher
+				onSearch={this.onSearch}
+			>
+				<AppLauncherSection title="All Items">
+					<DemoAppLauncherTile search={this.state.search} />
+					<DemoAppLauncherTileWithIconNode search={this.state.search} />
+					<DemoAppLauncherTileWithIconText search={this.state.search} />
+				</AppLauncherSection>
+				<AppLauncherSection title="All Apps">
+					<DemoAppLauncherTile search={this.state.search} />
+					<DemoAppLauncherTileWithTruncatedText search={this.state.search} />
+				</AppLauncherSection>
+			</AppLauncher>
+		);
+	}
+});
 
 
 storiesOf(APP_LAUNCHER, module)
 	.addDecorator(getStory => <div className="slds-p-around--medium">{getStory()}</div>)
-	.add('Tile', () => <DemoAppLauncherTile />)
-	.add('Small Tile', () => <DemoAppLauncherSmallTile />)
-	.add('Tile with Icon node', () => <DemoAppLauncherTileWithIconNode />);
+	.add('Tile', () => <div style={standardTileDemoStyles}><DemoAppLauncherTile /></div>)
+	.add('Small Tile', () => <div style={smallTileDemoStyles}><DemoAppLauncherSmallTile /></div>)
+	.add('Tile with Icon node', () => <div style={standardTileDemoStyles}><DemoAppLauncherTileWithIconNode /></div>)
+	.add('Tile with icon text', () => <div style={standardTileDemoStyles}><DemoAppLauncherTileWithIconText /></div>)
+	.add('Tile with search text', () => <div style={standardTileDemoStyles}><DemoAppLauncherTileWithSearchText /></div>)
+	.add('Tile with truncated text', () => <div style={standardTileDemoStyles}><DemoAppLauncherTileWithTruncatedText /></div>)
+	.add('Section', () => <DemoAppLauncherSection />)
+	.add('App Launcher', () => <DemoAppLauncher />);
