a796c5b1fe1a967f1427a4fbd3ec4c9710d7592d
Donielle Berg <donielle.berg@gmail.com>
2016-07-18 15:50:38 -0700

    Add inverse prop to Button and refactor Button component more

--- stories/button/index.jsx
+++ stories/button/index.jsx
@@ -18,22 +18,39 @@ const getIconButton = props => getButton({ variant: 'icon', ...props });
 storiesOf(BUTTON, module)
 	.addDecorator(getStory => <div className="slds-p-around--medium">{getStory()}</div>)
 	.add('Base', () => getButton({ label: 'Base', variant: 'base' }))
-	.add('Neutral', () => getButton({ label: 'Neutral' }))
-	.add('Neutral Icon', () => getButton({ label: 'Neutral Icon', iconName: 'download', 'iconPosition': 'left' }))
-	.add('Disabled', () => getButton({ label: 'Disabled', variant: 'brand', disabled: true }))
+	.add('Neutral', () => getButton({ label: 'Neutral'}))
+  .add('Neutral Icon', () => getButton({
+    label: 'Neutral Icon',
+    iconName: 'download',
+    iconPosition: 'left' ,
+  }))
+	.add('Disabled', () => getButton({ label: 'Disabled', disabled: true }))
+	.add('Icon large', () => getIconButton({
+		assistiveText: 'Icon',
+    iconSize: 'large',
+		iconName: 'answer'
+	}))
+
+	.addDecorator(getStory => <div className="slds-p-around--medium slds-hint-parent" style={{"backgroundColor": "green"}}>{getStory()}</div>)
 	.add('Icon Container Small', () => getIconButton({
 		assistiveText: 'Icon border container small',
 		iconName: 'settings',
-		iconSize: 'small',
-		iconVariant: 'border'
+		iconSize: 'large',
+    iconVariant: 'border',
+    inverse: true
 	}))
-	.addDecorator(getStory => <div className="slds-p-around--medium" style={{"backgroundColor": "green"}}>{getStory()}</div>)
 	.add('Dropdown Icon inverse', () => getIconButton({
 		assistiveText: 'Dropdown Icon inverse',
-    variant: 'icon-inverse',
 		iconName: 'settings',
-		iconVariant: 'more'
+    iconVariant: 'more',
+    inverse: true
 	}))
-	.addDecorator(getStory => <div className="slds-p-around--medium slds-hint-parent" style={{"backgroundColor": "green"}}>{getStory()}</div>)
-	.add('Hint inverse', () => getIconButton({ assistiveTest: 'Hint', iconName: 'down', hint: true, variant: 'icon-inverse' }))
+  .add('Small Icon Hint inverse', () => getIconButton({
+    assistiveTest: 'Hint',
+    iconName: 'down',
+    iconVariant: 'border',
+    iconSize: 'small',
+    hint: true,
+    inverse: true
+  }))
 
