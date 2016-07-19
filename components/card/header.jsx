0ea56b2452bc6772ca39f41035a48af8ff3db86d
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-07-19 16:43:36 -0600

    Release commit for 0.1.1-es [ci skip]

---
+++ components/card/header.jsx
@@ -0,0 +1,123 @@
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// ### React
+// React is an external dependency of the project.
+import React from 'react';
+
+// ### classNames
+// [github.com/JedWatson/classnames](https://github.com/JedWatson/classnames)
+// This project uses `classnames`, "a simple javascript utility for conditionally
+// joining classNames together."
+import classnames from 'classnames';
+
+// ## Children
+import MediaObject from '../media-object';
+
+// Removes the need for `PropTypes`.
+const { PropTypes } = React;
+
+import { CARD_HEADER } from '../../utilities/constants';
+
+// Allow for predicatable DOM queries with `querySelectorAll(cssClasses.base)`
+const cssClasses = {
+	base: 'slds-card__header'
+};
+
+const idSuffixes = {
+	headerActions: '__header-actions',
+	heading: '__heading'
+};
+
+// ## CardHeaderDefinition
+const CardHeader = React.createClass({
+	// ### Display Name
+	// Always use the canonical component name as the React display name.
+	displayName: CARD_HEADER,
+	// ### Prop Types
+	propTypes: {
+		/**
+		 * Adds a filter input to the card header
+		 */
+		filter: PropTypes.node,
+		/**
+		 * Actions performed on selected items or that relate to the entire group of items such as "Add Item.""
+		 */
+		headerActions: PropTypes.node,
+		/**
+		 * The heading is the name of the related item group.
+		 */
+		heading: PropTypes.string.isRequired,
+		/**
+		 * Icon associated with grouped items
+		 */
+		icon: PropTypes.node,
+		/**
+		 * Set the HTML `id` of the card filter and header actions.
+		 */
+		id: PropTypes.string.isRequired
+	},
+
+	renderFilter () {
+		const filter = React.cloneElement(this.props.filter, {
+			id: this.props.id
+		});
+
+		return (
+			<div className="slds-input-has-icon slds-input-has-icon--left slds-size--1-of-3">
+				{filter}
+			</div>
+		);
+	},
+
+	renderMediaObjectBody () {
+		return (
+			<h2
+				id={this.props.id + idSuffixes.heading}
+				className="slds-text-heading--small slds-truncate"
+				title={this.props.heading}
+			>
+				{this.props.heading}
+			</h2>
+		);
+	},
+
+	render () {
+		let filter = null;
+		const hasFilter = this.props.filter ? true : null;
+
+		if (this.props.filter) {
+			filter = this.renderFilter();
+		}
+
+		return (
+			<div className={classnames(cssClasses.base, 'slds-grid')}>
+				<MediaObject
+					figure={this.props.icon}
+					body={this.renderMediaObjectBody()}
+					verticalCenter
+					canTruncate
+				/>
+				{filter}
+				<div
+					id={this.props.id + idSuffixes.headerActions}
+					className={classnames('slds-no-flex', { 'slds-size--1-of-3': hasFilter, 'slds-text-align--right': hasFilter })}
+				>
+					{this.props.headerActions}
+				</div>
+			</div>
+		);
+	}
+});
+
+module.exports = CardHeader;
+module.exports.cssClasses = cssClasses;
+module.exports.idSuffixes = idSuffixes;
---
+++ components/index.js
@@ -0,0 +1,111 @@
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+export SLDSSettings from './SLDSSettings';
+
+export SLDSButton from './button';
+export Button from './button';
+
+export SLDSButtonGroup from './button-group';
+export ButtonGroup from './button-group';
+
+export SLDSButtonStateful from './button-stateful';
+export ButtonStateful from './button-stateful';
+
+export SLDSBreadCrumb from './bread-crumb';
+export BreadCrumb from './bread-crumb';
+
+export SLDSCard from './card';
+export Card from './card';
+
+export SLDSCardEmpty from './card/empty';
+export CardEmpty from './card/empty';
+
+export SLDSCardFilter from './card/filter';
+export CardFilter from './card/filter';
+
+export SLDSCheckbox from './forms/checkbox';
+export Checkbox from './forms/checkbox';
+
+export SLDSGlobalNavigationBar from './global-navigation-bar';
+export GlobalNavigationBar from './global-navigation-bar';
+
+export SLDSGlobalNavigationBarAppLauncher from './global-navigation-bar/app-launcher';
+export GlobalNavigationBarAppLauncher from './global-navigation-bar/app-launcher';
+
+export SLDSGlobalNavigationBarRegion from './global-navigation-bar/region';
+export GlobalNavigationBarRegion from './global-navigation-bar/region';
+
+export SLDSGlobalNavigationBarLink from './global-navigation-bar/link';
+export GlobalNavigationBarLink from './global-navigation-bar/link';
+
+export SLDSGlobalNavigationBarDropdown from './global-navigation-bar/dropdown';
+export GlobalNavigationBarDropdown from './global-navigation-bar/dropdown';
+
+export SLDSDataTable from './data-table';
+export DataTable from './data-table';
+
+export SLDSDataTableCell from './data-table/cell';
+export DataTableCell from './data-table/cell';
+
+export SLDSDataTableColumn from './data-table/column';
+export DataTableColumn from './data-table/column';
+
+export SLDSDataTableRowActions from './data-table/row-actions';
+export DataTableRowActions from './data-table/row-actions';
+
+export SLDSDatepicker from './date-picker';
+export Datepicker from './date-picker';
+
+export SLDSIcon from './icon';
+export Icon from './icon';
+
+export SLDSButtonIcon from './icon/button-icon';
+export ButtonIcon from './icon/button-icon';
+
+export SLDSInlineEdit from './forms/input/inline';
+export InlineEdit from './forms/input/inline';
+
+export SLDSInput from './forms/input';
+export Input from './forms/input';
+
+export SLDSLookup from './lookup';
+export Lookup from './lookup';
+
+export SLDSMenuDropdown from './menu-dropdown';
+export Dropdown from './menu-dropdown';
+
+export SLDSMenuPicklist from './menu-picklist';
+export Picklist from './menu-picklist';
+
+export SLDSModal from './modal';
+export Modal from './modal';
+
+export SLDSModalTrigger from './modal/trigger';
+export ModalTrigger from './modal/trigger';
+
+export SLDSNotification from './notification';
+export Notification from './notification';
+
+export SLDSPageHeader from './page-header';
+export PageHeader from './page-header';
+
+export SLDSPopoverTooltip from './popover-tooltip';
+export PopoverTooltip from './popover-tooltip';
+
+export SLDSSpinner from './spinner';
+export Spinner from './spinner';
+
+export SLDSTimepicker from './time-picker';
+export Timepicker from './time-picker';
+
+export SLDSUtilityIcon from './utilities/utility-icon';
+export UtilityIcon from './utilities/utility-icon';
