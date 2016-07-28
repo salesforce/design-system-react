e903a40a8bd8e74968db29b3ea07d784ba692849
David Brainer-Banker <dbrainer-banker@exacttarget.com>
2016-07-28 09:09:26 -0600

    Add a function to get the text content of multiple items

--- components/utilities/truncate/index.js
+++ components/utilities/truncate/index.js
@@ -9,7 +9,7 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-export default function ({ inputString = '', maxLength = 140, truncationChars = '...', startingLength = 0 }) {
+function truncate ({ inputString = '', maxLength = 140, truncationChars = '...', startingLength = 0 }) {
 	let outputString;
 
 	if (inputString.length <= maxLength) {
@@ -33,3 +33,24 @@ export default function ({ inputString = '', maxLength = 140, truncationChars = 
 
 	return outputString;
 }
+
+function textContentArray (child) {
+	const text = [];
+
+	if (typeof child === 'string' || typeof child === 'number') {
+		text.push(child);
+	} else if (Array.isArray(child)) {
+		text.push(child.forEach(textContentArray));
+	} else if (child && child.props) {
+		const { children } = child.props;
+		text.push(textContentArray(children));
+	}
+
+	return text;
+}
+
+function textContent (child) {
+	return textContentArray(child).join('');
+}
+
+export default truncate;
