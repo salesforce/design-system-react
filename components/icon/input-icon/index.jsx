c62ea8a5704f8763fc99a06d3c9915f49320912e
Eric Willhoit <iowillhoit@gmail.com>
2016-08-15 12:15:42 -0500

    lints input-icon

--- components/icon/input-icon/index.jsx
+++ components/icon/input-icon/index.jsx
@@ -9,44 +9,57 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-import React from 'react';
+// ### React
+import React, { PropTypes } from 'react';
+
+// ## Children
 import SLDSUtilityIcon from '../../utilities/utility-icon';
 
-const displayName = 'InputIcon';
+// ## Constants
+import { ICON_INPUT } from '../../../utilities/constants';
 
-const propTypes = {
-  category: React.PropTypes.string,
-  icon: React.PropTypes.object,
-  name: React.PropTypes.string,
-  onClick: React.PropTypes.func,
+/**
+ * A wrapper for icons that will be rendered inside of an Input
+ */
+const InputIcon = (props) => (
+	<SLDSUtilityIcon
+		aria-hidden
+		category={props.category}
+		className="slds-input__icon slds-icon-text-default"
+		icon={props.icon}
+		name={props.name}
+		onClick={props.onClick}
+		style={props.style}
+	/>
+);
+
+InputIcon.displayName = ICON_INPUT;
+
+InputIcon.propTypes = {
+	/**
+	 * Icon category from [lightningdesignsystem.com/icons/](https://www.lightningdesignsystem.com/icons/)
+	 */
+	category: PropTypes.string,
+	/**
+	 * An SVG object to use instead of name / category, look in `design-system-react/icons` for examples
+	 */
+	icon: PropTypes.object,
+	/**
+	 * Name of the icon. Visit <a href='http://www.lightningdesignsystem.com/resources/icons'>Lightning Design System Icons</a> to reference icon names.
+	 */
+	name: PropTypes.string,
+	/**
+	 * This event fires when the icon is clicked.
+	 */
+	onClick: PropTypes.func,
+	/**
+	 * Custom styles to be passed to the SVG. Could be used to change icon or background color.
+	 */
+	style: PropTypes.object
 };
-const defaultProps = {
-  category: 'utility',
+
+InputIcon.defaultProps = {
+	category: 'utility'
 };
-
-class InputIcon extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const className = 'slds-input__icon slds-icon-text-default';
-    return <SLDSUtilityIcon
-              aria-hidden='true'
-              category={this.props.category}
-              className={className}
-              icon={this.props.icon}
-              name={this.props.name}
-              onClick={this.props.onClick}
-              style={this.props.style}
-            />;
-  }
-}
-
-InputIcon.displayName = displayName;
-InputIcon.propTypes = propTypes;
-InputIcon.defaultProps = defaultProps;
 
 module.exports = InputIcon;
