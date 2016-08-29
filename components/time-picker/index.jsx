deacea626509259d1c8426aa46dc1862fec1d334
Stephen James <llama@interactivellama.com>
2016-08-29 17:24:44 -0400

    Release commit for 0.3.6-es [ci skip]

---
+++ components/time-picker/index.jsx
@@ -0,0 +1,216 @@
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+// # Timepicker Component
+
+// ## Dependencies
+
+// ### React
+import React, { PropTypes } from 'react';
+
+// ### isDate
+import isDate from 'lodash.isdate';
+
+// ### Dropdown
+import MenuDropdown from '../menu-dropdown';
+import TimepickerDropdownTrigger from './dropdown-trigger';
+
+// ## Constants
+import { TIME_PICKER } from '../../utilities/constants';
+
+/**
+*  Component description.
+*/
+const Timepicker = React.createClass({
+	// ### Display Name
+	// Always use the canonical component name as the React display name.
+	displayName: TIME_PICKER,
+
+	// ### Prop Types
+	propTypes: {
+		constrainToScrollParent: PropTypes.bool,
+		/**
+		 * Disables the input and prevents editing the contents.
+		 */
+		disabled: PropTypes.bool,
+		/**
+		 * Time formatting function
+		 */
+		formatter: PropTypes.func,
+		inheritTargetWidth: PropTypes.bool,
+		/**
+		 * This label appears above the input.
+		 */
+		label: PropTypes.string,
+		/**
+		 * Custom element that overrides the default Menu Item component.
+		 */
+		listItemRenderer: PropTypes.func,
+		/**
+		 * Renders menu within an absolutely positioned container at an elevated z-index.
+		 */
+		modal: PropTypes.bool,
+		/**
+		 * Receives the props `(dateValue, stringValue)`
+		 */
+		onDateChange: PropTypes.func,
+		/**
+		 * Parsing date string into Date
+		 */
+		parser: PropTypes.func,
+		/**
+		 * Text that will appear in an empty input.
+		 */
+		placeholder: PropTypes.string,
+		/**
+		 * If true, adds asterisk next to input label to indicate it is a required field.
+		 */
+		required: PropTypes.bool,
+		stepInMinutes: PropTypes.number,
+		strValue: PropTypes.string,
+		/**
+		 * Date
+		 */
+		value: PropTypes.instanceOf(Date)
+	},
+
+	getDefaultProps () {
+		return {
+			formatter (date) {
+				if (date) {
+					return date.toLocaleTimeString(navigator.language, { hour: '2-digit', minute: '2-digit' });
+				}
+
+				return null;
+			},
+			modal: false,
+			parser (timeStr) {
+				const date = new Date();
+				const dateStr = date.toLocaleString(navigator.language, { year: 'numeric', month: 'numeric', day: 'numeric' });
+				return new Date(`${dateStr} ${timeStr}`);
+			},
+			placeholder: 'Pick Time',
+			value: null,
+			stepInMinutes: 30
+		};
+	},
+
+	getInitialState () {
+		return {
+			value: this.props.value,
+			strValue: this.props.strValue,
+			options: this.getOptions()
+		};
+	},
+
+	getOptions () {
+		const baseDate = new Date();
+		const options = [];
+
+		baseDate.setHours(0);
+		baseDate.setMinutes(0);
+		baseDate.setSeconds(0);
+		baseDate.setMilliseconds(0);
+
+		const curDate = new Date(baseDate);
+
+		while (baseDate.getDate() === curDate.getDate()) {
+			const formatted = this.props.formatter(curDate);
+
+			options.push({
+				label: formatted,
+				value: new Date(curDate)
+			});
+
+			curDate.setMinutes(curDate.getMinutes() + this.props.stepInMinutes);
+		}
+
+		return options;
+	},
+
+	parseDate (strValue) {
+		const newDate = this.props.parser(strValue);
+
+		if (isDate(newDate)) {
+			if (!isNaN(newDate.getTime())) {
+				return newDate;
+			}
+		}
+
+		return new Date();
+	},
+
+	// ### Render
+	render () {
+		return (
+			<MenuDropdown
+				checkmark={false}
+				constrainToScrollParent={this.props.constrainToScrollParent}
+				disabled={this.props.disabled}
+				inheritTargetWidth={this.props.inheritTargetWidth}
+				label={this.props.label}
+				listItemRenderer={this.props.listItemRenderer}
+				// inline style override
+				menuStyle={{
+					maxHeight: '20em',
+					overflowX: 'hidden',
+					minWidth: '100%'
+				}}
+				modal={this.props.modal}
+				onSelect={this.handleSelect}
+				options={this.state.options}
+			>
+				<TimepickerDropdownTrigger
+					onChange={this.handleInputChange}
+					iconCategory="utility"
+					iconName="clock"
+					iconPosition="right"
+					placeholder={this.props.placeholder}
+					required={this.props.required}
+					type="text"
+					value={this.state.strValue}
+				/>
+			</MenuDropdown>
+		);
+	},
+
+	handleChange (date, strValue) {
+		this.setState({
+			value: date,
+			strValue
+		});
+
+		if (this.props.onDateChange) {
+			this.props.onDateChange(date, strValue);
+		}
+	},
+
+	handleSelect (val) {
+		if (val && val.value) {
+			this.handleChange(val.value, val.label);
+		}
+	},
+
+	handleInputChange (event) {
+		const strValue = event.target.value;
+
+		this.setState({
+			strValue
+		});
+
+		if (this.props.onDateChange) {
+			const parsedDate = this.props.parser(strValue);
+			this.props.onDateChange(parsedDate, strValue);
+		}
+	}
+});
+
+module.exports = Timepicker;
---
+++ icons/standard/answer_best.js
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+module.exports = {"viewBox":"0 0 24 24","title":{},"path":[{"d":"M11.9 5.2c-4 0-7.2 3-7.2 6.7 0 1.2.3 2.3.9 3.3.1.1.1.2 0 .4L5 17.7c-.2.4.2.7.6.6l2.1-.8c.1 0 .3 0 .4.1 1.1.6 2.4 1 3.8 1 4 0 7.2-3 7.2-6.7s-3.2-6.7-7.2-6.7zm3.4 5.2l-3.6 3.7c-.2.1-.3.2-.5.2s-.4-.1-.5-.2l-1.8-1.8c-.2-.2-.2-.4 0-.5l.5-.5c.1-.2.3-.2.5 0l1.3 1.3 3.1-3.2c.2-.2.4-.2.5 0l.5.5c.2.1.2.4 0 .5z"},{"fill-opacity":".65","d":"M24 24v-9.1L14.9 24H24zm-1-3.2l-.8.7v.1l.2 1.1c0 .1-.1.1-.1.1l-.9-.5c-.1-.1-.1-.1-.1 0l-1 .5s-.1 0-.1-.1l.2-1.1v-.1l-.8-.7c0-.1 0-.2.1-.2l1-.2h.1l.4-1c0-.1.1-.1.2 0l.4 1h.1l1 .2c.1 0 .1.1.1.2z"}]};
