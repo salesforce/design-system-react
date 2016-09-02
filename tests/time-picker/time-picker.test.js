f0b6999aa1aebc5943bb12593b4c47fe4d9e0bfc
Jonathon Hibbard <jonathon.hibbard@salesforce.com>
2016-09-02 16:58:12 -0400

    Fix test for timepicker

--- tests/time-picker/time-picker.test.js
+++ tests/time-picker/time-picker.test.js
@@ -14,45 +14,17 @@ import { SLDSTimepicker } from '../../components';
 
 const mockCallback = sinon.spy();
 
-const formatter = (date) => (date.getMonth() + 1 ) + '/' + date.getDate() + '/' + date.getFullYear();
+const formatter = (date) => date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
 const dateTimeNow = new Date();
 const defaultStrValue = formatter(dateTimeNow);
+const defaultProps = {
+    onDateChange: () => {},
+    value: dateTimeNow,
+    strValue: defaultStrValue
+};
 
 describe('SLDSTimepicker: ', () => {
-    let body;
-
-    const defaultProps = {
-        onDateChange: () => {},
-        value: dateTimeNow,
-        strValue: defaultStrValue
-    };
-
-    const renderTimepicker = inst => {
-        body = document.createElement('div');
-        document.body.appendChild(body);
-
-        return ReactDOM.render(inst, body);
-    };
-
-    const removeTimepicker = () => {
-        ReactDOM.unmountComponentAtNode(body);
-        document.body.removeChild(body);
-    };
-
-    const createTimepicker = (props) => React.createElement(SLDSTimepicker, assign({}, defaultProps, props));
-    const getTimepicker = (props) => removeTimepicker(createTimepicker(props));
-
     describe('Timepicker Value Prop Change', () => {
-        let cmp;
-        let picker;
-
-        beforeEach(() => {
-            cmp = getTimepicker(defaultProps);
-            picker = findRenderedDOMComponentWithClass(cmp, 'slds-form-element');
-        });
-
-        afterEach(() => removeTimepicker());
-
         it("displays a modified state upon changing props", () => {
             const TestTimepicker = createFactory(
                 createClass({
