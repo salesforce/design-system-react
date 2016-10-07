fb520f4db7e9c54b3f150aff7e17ccf814e5e796
Stephen James <llama@interactivellama.com>
2016-10-06 21:50:26 -0400

    Release commit for 0.3.16-amd [ci skip]

---
+++ components/data-table/column.js
@@ -0,0 +1,51 @@
+define(['module', 'react', '../../utilities/constants'], function (module, _react, _constants) {
+	'use strict';
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) {
+		return obj && obj.__esModule ? obj : {
+			default: obj
+		};
+	}
+
+	var PropTypes = _react2.default.PropTypes;
+
+
+	/**
+  * Columns define the structure of the data displayed in the DataTable.
+  */
+	var DataTableColumn = _react2.default.createClass({
+		// ### Display Name
+		// Always use the canonical component name as the React display name.
+		displayName: _constants.DATA_TABLE_COLUMN,
+
+		// ### Prop Types
+		propTypes: {
+			children: _react2.default.PropTypes.element,
+			/**
+    * The column label.
+    */
+			label: PropTypes.string,
+			/**
+    * The property which corresponds to this column.
+    */
+			property: PropTypes.string,
+			/**
+    * Whether or not the column is sortable.
+    */
+			sortable: PropTypes.bool,
+			/**
+    * The current sort direction. If left out the component will track this internally.
+    */
+			sortDirection: PropTypes.oneOf(['desc', 'asc']),
+			truncate: PropTypes.bool
+		},
+
+		render: function render() {
+			return null;
+		}
+	});
+
+	module.exports = DataTableColumn;
+});
---
+++ icons/utility/ban.js
@@ -0,0 +1,16 @@
+define(["module"], function (module) {
+  "use strict";
+
+  /*
+  Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+  
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+  Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+  
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  */
+
+  module.exports = { "viewBox": "0 0 24 24", "path": { "d": "M12 .9C5.9.9.9 5.9.9 12s5 11.1 11.1 11.1 11.1-5 11.1-11.1S18.1.9 12 .9zM3.7 12c0-4.6 3.7-8.3 8.3-8.3 1.8 0 3.5.5 4.8 1.5L5.2 16.8c-1-1.3-1.5-3-1.5-4.8zm8.3 8.3c-1.8 0-3.5-.5-4.8-1.5L18.8 7.2c1 1.3 1.5 3 1.5 4.8 0 4.6-3.7 8.3-8.3 8.3z" } };
+});
