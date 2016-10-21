19793ce1ce9d951d483f931de9da3527bd53a241
Stephen James <llama@interactivellama.com>
2016-10-21 15:29:38 -0400

    Release commit for 0.4.1-es [ci skip]

---
+++ icons/custom/custom45.js
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+module.exports = {"viewBox":"0 0 24 24","path":{"d":"M7.9 10.3h8.2v3.4H7.9zm11.8-.4V8.6c0-.8-.7-1.4-1.5-1.4H5.8c-.8 0-1.5.6-1.5 1.4v1.3c0 .2.1.3.3.4.5.4.9 1 .9 1.7s-.4 1.3-.9 1.6c-.2.1-.3.3-.3.4v1.4c0 .8.7 1.4 1.5 1.4h12.4c.8 0 1.5-.6 1.5-1.4v-1.3c0-.2-.1-.3-.3-.4-.5-.4-.9-1-.9-1.7s.4-1.3.9-1.6c.2-.2.3-.3.3-.5zM17 15.1H7c-.3 0-.5-.2-.5-.5V9.4c0-.3.2-.5.5-.5h10c.3 0 .5.2.5.5v5.2c0 .3-.2.5-.5.5z"}};
---
+++ utilities/warning/only-one-of-properties.js
@@ -0,0 +1,35 @@
+/*
+Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+/* eslint-disable indent, import/no-mutable-exports */
+
+// This function will deliver an warning message to the browser console if extraneous properties are defined (falsey).
+import warning from 'warning';
+
+let onlyOneOf = function () {};
+
+if (process.env.NODE_ENV !== 'production') {
+	const hasWarned = {};
+
+	onlyOneOf = function (control, selectedProps, comment) {
+		const additionalComment = comment ? ` ${comment}` : '';
+		let keys = Object.keys(selectedProps);
+		keys = keys.filter((key) => selectedProps[key]);
+
+		if (!hasWarned[control]) {
+			/* eslint-disable max-len */
+			warning((keys.length <= 1), `[Design System React] Only one of the following props is needed by ${control}: [${keys.join()}].${additionalComment}`);
+			/* eslint-enable max-len */
+			hasWarned[control] = !!selectedProps;
+		}
+	};
+}
+
+export default onlyOneOf;
