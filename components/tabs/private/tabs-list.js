c5de4bc00ecb941c642599bc932cf0aba7f78b94
Stephen James <llama@interactivellama.com>
2016-12-14 08:51:35 -0700

    Release commit for 0.5.4-commonjs [ci skip]

---
+++ components/tabs/private/tabs-list.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+var _constants = require('../../../utilities/constants');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; } /*
+                                                                                                                                                                                                                             Copyright (c) 2015, salesforce.com, inc. All rights reserved.
+                                                                                                                                                                                                                             
+                                                                                                                                                                                                                             Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+                                                                                                                                                                                                                             Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+                                                                                                                                                                                                                             Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+                                                                                                                                                                                                                             Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+                                                                                                                                                                                                                             
+                                                                                                                                                                                                                             THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+                                                                                                                                                                                                                             */
+
+// # Tabs Component
+
+// Implements the [Tabs design pattern](https://www.lightningdesignsystem.com/components/tabs/) in React.
+
+// ## Dependencies
+
+// ### React
+
+
+// ### classNames
+
+
+var TabsList = function TabsList(_ref) {
+	var id = _ref.id;
+	var className = _ref.className;
+	var children = _ref.children;
+	var variant = _ref.variant;
+
+	var attributes = _objectWithoutProperties(_ref, ['id', 'className', 'children', 'variant']);
+
+	return _react2.default.createElement(
+		'ul',
+		{
+			id: id + '-slds-tabs__nav',
+			className: (0, _classnames2.default)(className, {
+				'slds-tabs--default__nav': variant === 'default',
+				'slds-tabs--scoped__nav': variant === 'scoped'
+			}),
+			role: 'tablist',
+			variant: variant
+		},
+		children
+	);
+};
+
+TabsList.displayName = _constants.TABS_LIST;
+
+TabsList.propTypes = {
+	/**
+  * Inherits the `id` from the parent `<Tabs />` component and appends `-tabs__nav`. Becomes the HTML `id` attribute of UL element that has the class `.slds-tabs--default__nav` on it.
+  */
+	id: _react.PropTypes.string,
+
+	/**
+  * Class names to be added to the tabs list element.
+  */
+	className: _react.PropTypes.oneOfType([_react.PropTypes.array, _react.PropTypes.object, _react.PropTypes.string]),
+
+	/**
+  * The `children` are the actual tabs to be rendered as `li` elements. They get created by [tabs/index.jsx](./index.jsx) in the `renderTabsList` function.
+  */
+	children: _react.PropTypes.oneOfType([_react.PropTypes.object, _react.PropTypes.array]),
+
+	/**
+  * If the Tabs should be scopped, defaults to false
+  */
+	variant: _react2.default.PropTypes.oneOf(['default', 'scoped'])
+};
+
+module.exports = TabsList;
