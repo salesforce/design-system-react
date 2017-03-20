6dd439a8a78e170d7a8d882358d31f878abe236c
Chris Corwin <ccorwin@salesforce.com>
2017-03-20 11:18:53 -0400

    WIP checkbox snapshots

--- tests/checkbox/checkbox.snapshot-test.jsx
+++ tests/checkbox/checkbox.snapshot-test.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import renderer from 'react-test-renderer';
+import jsBeautify from 'js-beautify';
+
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
@@ -6,23 +10,36 @@ import CheckboxBase from '../../examples/forms/checkbox/default';
 import CheckboxToggle from '../../examples/forms/checkbox/toggle';
 import CheckboxError from '../../examples/forms/checkbox/error';
 
-test('Checkbox Base Snapshot', () => {
-	const domTree = toJson(shallow(
-		<CheckboxBase />
-	));
+
+test('Checkbox Base DOM Snapshot', () => {
+	const domTree = renderer.create(
+		<CheckboxBase />,
+	).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
 
-test('Checkbox Toggle Snapshot', () => {
-	const domTree = toJson(shallow(
-		<CheckboxToggle />
-	));
+
+test('Checkbox Base HTML Snapshot', () => {
+	const domTree = String(
+		jsBeautify.html(ReactDOMServer.renderToStaticMarkup(<CheckboxBase />), {}),
+		'utf-8'
+	);
 	expect(domTree).toMatchSnapshot();
 });
 
-test('Checkbox Base (error) Snapshot', () => {
-	const domTree = toJson(shallow(
-		<CheckboxError />
-	));
+test('Checkbox Toggle DOM Snapshot', () => {
+	const domTree = renderer.create(
+		<CheckboxToggle />,
+	).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
+
+
+test('Checkbox Toggle HTML Snapshot', () => {
+	const domTree = String(
+		jsBeautify.html(ReactDOMServer.renderToStaticMarkup(<CheckboxToggle />), {}),
+		'utf-8'
+	);
+	expect(domTree).toMatchSnapshot();
+});
+
