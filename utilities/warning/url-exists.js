de10c9062807a993a8bd2ff325de34eefa92c2ea
Stephen James <llama@interactivellama.com>
2017-04-03 00:26:30 -0600

    Add more conditions for url exists checkProp
    Using XMLHttpRequest can cause problems in non-browser environments. This should be completely removed in production environment and should not execute in a testing environment.

--- utilities/warning/url-exists.js
+++ utilities/warning/url-exists.js
@@ -9,7 +9,7 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 /* eslint-disable import/no-mutable-exports */
-/* global XMLHttpRequest */
+/* global XMLHttpRequest, window */
 
 // This function does an "AJAX" request to warn users on how to setup their icon path.
 import warning from 'warning';
@@ -20,8 +20,13 @@ if (process.env.NODE_ENV !== 'production') {
 	const hasWarned = {};
 	let hasExecuted;
 
+	// Using XMLHttpRequest can cause problems in non-browser environments. This should be completely removed in production environment and should not execute in a testing environment.
 	urlExists = function (control, url, comment) {
-		if (!hasExecuted && !hasWarned[`${control}-path`] && XMLHttpRequest) {
+		if (!hasExecuted
+			&& !hasWarned[`${control}-path`]
+			&& window
+			&& XMLHttpRequest
+			&& process.env.NODE_ENV !== 'test') {
 			const http = new XMLHttpRequest();
 			http.open('HEAD', url, false);
 			http.send();
