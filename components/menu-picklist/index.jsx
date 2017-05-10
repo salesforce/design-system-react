9398e0f415481550aa3f5fabdf3e73310b7b8e99
Donielle Berg <donielle.berg@gmail.com>
2017-05-10 11:47:14 -0700

    Fixes from PR review and ES6-ify menu-picklist/index.jsx and menu-picklist/private/picklist-trigger.jsx

--- components/menu-picklist/index.jsx
+++ components/menu-picklist/index.jsx
@@ -9,7 +9,8 @@
 // Based on SLDS v2.1.0-rc.2
 
 // ### React
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // ### shortid
 // [npmjs.com/package/shortid](https://www.npmjs.com/package/shortid)
@@ -26,77 +27,76 @@ import PicklistTrigger from './private/picklist-trigger';
 
 import { MENU_PICKLIST } from '../../utilities/constants';
 
+// ### Display Name
+// Always use the canonical component name as the React display name.
+const displayName = MENU_PICKLIST;
+
+// ### Prop Types
+const propTypes = {
+	/**
+	 * Callback that passes in the DOM reference of the `<button>` DOM node within this component. Primary use is to allow `focus` to be called. You should still test if the node exists, since rendering is asynchronous. `buttonRef={(component) => { if(component) console.log(component); }}`
+	 */
+	buttonRef: PropTypes.func,
+	className: PropTypes.string,
+	/**
+	 * If true, renders checkmark icon on the selected Menu Item.
+	 */
+	checkmark: PropTypes.bool,
+	disabled: PropTypes.bool,
+	/**
+	 * Message to display when the input is in an error state. When this is present, also visually highlights the component as in error.
+	 */
+	errorText: PropTypes.string,
+	/**
+	 * A unique ID is needed in order to support keyboard navigation, ARIA support, and connect the dropdown to the triggering button.
+	 */
+	id: PropTypes.string,
+	/**
+	 * Renders menu within the wrapping trigger as a sibling of the button. By default, you will have an absolutely positioned container at an elevated z-index.
+	 */
+	isInline: PropTypes.bool,
+	label: PropTypes.string,
+	/**
+	 * Custom element that overrides the default Menu Item component.
+	 */
+	listItemRenderer: PropTypes.func,
+	onClick: PropTypes.func,
+	onSelect: PropTypes.func,
+	/**
+	 * Menu item data.
+	 */
+	options: PropTypes.array.isRequired,
+	placeholder: PropTypes.string,
+	required: PropTypes.bool,
+	/**
+	 * Current selected item.
+	 */
+	value: PropTypes.node
+};
+
+const defaultProps = {
+	inheritTargetWidth: true,
+	placeholder: 'Select an Option',
+	checkmark: true
+};
+
 /**
  * The MenuPicklist component is a variant of the Lightning Design System Menu component.
  */
-const MenuPicklist = React.createClass({
-	// ### Display Name
-	// Always use the canonical component name as the React display name.
-	displayName: MENU_PICKLIST,
-
-	// ### Prop Types
-	propTypes: {
-		/**
-		 * Callback that passes in the DOM reference of the `<button>` DOM node within this component. Primary use is to allow `focus` to be called. You should still test if the node exists, since rendering is asynchronous. `buttonRef={(component) => { if(component) console.log(component); }}`
-		 */
-		buttonRef: PropTypes.func,
-		className: PropTypes.string,
-		/**
-		 * If true, renders checkmark icon on the selected Menu Item.
-		 */
-		checkmark: PropTypes.bool,
-		disabled: PropTypes.bool,
-		/**
-		 * Message to display when the input is in an error state. When this is present, also visually highlights the component as in error.
-		 */
-		errorText: PropTypes.string,
-		/**
-		 * A unique ID is needed in order to support keyboard navigation, ARIA support, and connect the dropdown to the triggering button.
-		 */
-		id: PropTypes.string,
-		/**
-		 * Renders menu within the wrapping trigger as a sibling of the button. By default, you will have an absolutely positioned container at an elevated z-index.
-		 */
-		isInline: PropTypes.bool,
-		label: PropTypes.string,
-		/**
-		 * Custom element that overrides the default Menu Item component.
-		 */
-		listItemRenderer: PropTypes.func,
-		onClick: PropTypes.func,
-		onSelect: PropTypes.func,
-		/**
-		 * Menu item data.
-		 */
-		options: PropTypes.array.isRequired,
-		placeholder: PropTypes.string,
-		required: PropTypes.bool,
-		/**
-		 * Current selected item.
-		 */
-		value: PropTypes.node
-	},
-
-	getDefaultProps () {
-		return {
-			inheritTargetWidth: true,
-			placeholder: 'Select an Option',
-			checkmark: true
-		};
-	},
-
-	getInitialState () {
-		return {
+class MenuPicklist extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {
 			selectedItem: null
 		};
-	},
+	}
 
 	componentWillMount () {
 		// `checkProps` issues warnings to developers about properties (similar to React's built in development tools)
 		checkProps(MENU_PICKLIST, this.props);
 		this.dropdownId = this.props.id || shortid.generate();
 		this.triggerId = this.props.triggerId || shortid.generate();
-	},
+	}
 
 	render () {
 		const {
@@ -129,7 +129,11 @@ const MenuPicklist = React.createClass({
 			/>
 		</Dropdown>);
 	}
+}
 
-});
+MenuPicklist.displayName = displayName;
+MenuPicklist.propTypes = propTypes;
+MenuPicklist.defaultProps = defaultProps;
 
 export default MenuPicklist;
+
