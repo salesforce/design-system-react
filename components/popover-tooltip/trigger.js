c4d1514d46edb631b42a03f3929d27c1ef61c06c
Donielle Berg <donielle.berg@gmail.com>
2017-05-26 18:54:24 -0700

    Release commit for 0.6.8-commonjs [ci skip]

---
+++ components/popover-tooltip/trigger.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _trigger = require('../tooltip/trigger');
+
+var _trigger2 = _interopRequireDefault(_trigger);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _trigger2.default; /* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+// Alias
