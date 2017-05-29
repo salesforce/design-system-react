07cc72f822e6f3f9be4a1aaaa9ad024136fe0bea
Donielle Berg <donielle.berg@gmail.com>
2017-05-28 23:34:34 -0700

    Release commit for 0.6.9-commonjs [ci skip]

---
+++ components/menu-list/list-item.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _item = require('../utilities/list/item');
+
+var _item2 = _interopRequireDefault(_item);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _item2.default; // Alias
