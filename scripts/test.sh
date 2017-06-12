387fbe2114a017774cbfab7d4d5644101dbec840
Stephen James <llama@interactivellama.com>
2017-06-12 12:22:25 -0500

    Add production code lint check to TravisCI
    also makes npm run lint check all JS files

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,20 @@
 # Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
 # Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
 
+# Mocha framework tests that focus on user interaction
 NODE_ENV=test node_modules/.bin/karma start --single-run "$@"
 if [ $? -eq 0 ]
 then
+  # Jest markup snapshot tests
 	NODE_ENV=test npm run snapshot-test
 	if [ $? -eq 0 ]
 	then
-		NODE_ENV=test npm run build-docs
+		# ESlint tests on files within components and utilities folders. Doc examples and tests are currently excluded.
+		NODE_ENV=test npm run lint-prod
+		if [ $? -eq 0 ]
+		then
+			# React DocGen library build of source comments into a JSON file for documentation site
+			NODE_ENV=test npm run build-docs
+		fi
 	fi
 fi
