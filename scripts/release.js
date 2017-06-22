a55eb3046a5876907e0ebadcbaa99b42f8e0b44d
Stephen James <llama@interactivellama.com>
2017-06-22 11:11:32 -0600

    Add icon build to release process

--- scripts/release.js
+++ scripts/release.js
@@ -21,48 +21,53 @@ const executeTasks = () => {
 	async.series([
 		// Does a remote named upstream exist? Origin should point to fork.
 		(done) => {
-			console.log('Checking to see if an upstream remote exists');
+			console.log('# Checking to see if an upstream remote exists');
 			exec(['git ls-remote upstream --exit-code --quiet', rootPath], done);
 		},
 		// Checkout master branch
 		(done) => {
-			console.log('Checking out master branch. Please ignore "RELEASENOTES.md - Your branch and \'upstream/master\' have diverged."');
+			console.log('# Checking out master branch. Please ignore "RELEASENOTES.md - Your branch and \'upstream/master\' have diverged."');
 			exec(['git checkout master', rootPath], done);
 		},
 		// Remove libraries not in package.json
 		(done) => {
-			console.log('Pruning NPM dependencies');
+			console.log('# Pruning NPM dependencies');
 			exec(['npm prune', rootPath], done);
 		},
 		// npm install
 		(done) => {
-			console.log('Running NPM install');
+			console.log('# Running NPM install');
 			exec(['npm install', rootPath], done);
+		},
+		// Update inline icons
+		(done) => {
+			console.log('# Build icons from latest icon module');
+			exec(['npm run icons', rootPath], done);
 		},
 		// Run tests
 		(done) => {
-			console.log('Running test suite');
+			console.log('# Running test suite');
 			exec(['npm test', rootPath], done);
 		},
 		// Update package.json version
 		(done) => {
-			console.log('Bumping version in package.json, committing RELEASENOTES.md, and checking that there are no files in local working copy. Published package is based on current state of local files, not versioned files.');
+			console.log('# Bumping version in package.json, committing RELEASENOTES.md, and checking that there are no files in local working copy. Published package is based on current state of local files, not versioned files.');
 			exec([`npm --no-git-tag-version version ${argv.release}`, rootPath], done, rootPath);
 		},
 		// Builds three disconnected git trees based on package.json version and publishes as tags on upstream remote
 		(done) => {
-			console.log('Building and publishing tag to upstream remote');
+			console.log('# Building and publishing tag to upstream remote');
 			exec(['npm run publish-to-upstream', rootPath], done);
 			done();
 		},
 		// Push to master the package.json and release notes
 		(done) => {
-			console.log('Pushing local master branch to upstream remote');
+			console.log('# Pushing local master branch to upstream remote');
 			exec(['git push master upstream', rootPath], done);
 			done();
 		},
 		(done) => {
-			console.log('\n\nPlease add the following to your release notes at https://github.com/salesforce-ux/design-system-react/releases');
+			console.log('\n\n# Please add the following to your release notes at https://github.com/salesforce-ux/design-system-react/releases');
 			console.log(currentVersionReleaseNotes);
 			done();
 		}
@@ -85,7 +90,7 @@ prompt.start();
 prompt.get(releaseNotesSchema, (err, releaseNotesResult) => {
 	if (releaseNotesResult.releasenotes.toUpperCase() === 'N') {
 		exec(['open RELEASENOTES.MD', rootPath], () => {});
-		console.log('Please do not commit changes to RELEASENOTES.MD. Unversioned changes must be present in RELEASENOTES.MD or release will fail. Changes will be versioned for you. Please run script again.');
+		console.log('# Please do not commit changes to RELEASENOTES.MD. Unversioned changes must be present in RELEASENOTES.MD or release will fail. Changes will be versioned for you. Please run script again.');
 		process.exit();
 	} else {
 		fs.readFile(path.resolve(rootPath, 'RELEASENOTES.MD'), 'utf8', (err2, contents) => {
