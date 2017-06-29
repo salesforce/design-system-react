a91c0bad27c90061c20e7ca09f4cff4813abadf7
Stephen James <llama@interactivellama.com>
2017-06-29 16:25:51 -0600

    Release commit for 0.6.13-commonjs [ci skip]

---
+++ components/lookup/menu/item/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _item = require('../../private/item');
+
+var _item2 = _interopRequireDefault(_item);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _item2.default; // Alias
