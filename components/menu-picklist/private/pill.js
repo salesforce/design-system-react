7e6780fabb70b99db2caf0d52943f49ed0326596
Build Server <llama@interactivellama.com>
2017-07-31 23:11:17 +0000

    Release commit for 0.6.17-amd [ci skip]

---
+++ components/menu-picklist/private/pill.js
@@ -0,0 +1,115 @@
+define(['exports', 'react', 'prop-types', '../../../components/utilities/utility-icon', 'lodash.assign', 'airbnb-prop-types'], function (exports, _react, _propTypes, _utilityIcon, _lodash, _airbnbPropTypes) {
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _propTypes2 = _interopRequireDefault(_propTypes);
+
+	var _utilityIcon2 = _interopRequireDefault(_utilityIcon);
+
+	var _lodash2 = _interopRequireDefault(_lodash);
+
+	function _interopRequireDefault(obj) {
+		return obj && obj.__esModule ? obj : {
+			default: obj
+		};
+	}
+
+	var propTypes = {
+		/**
+   * **Assistive text for accessibility**
+   * This object is merged with the default props object on every render.
+   * * `pressDeleteOrBackspace`: Informs user of keyboard keys to press in order to remove a pill
+   */
+		assistiveText: (0, _airbnbPropTypes.shape)({
+			pressDeleteOrBackspace: _propTypes2.default.string
+		}),
+		/*
+   * Pills are often used for selection of a type of entity such as days in a daypicker. This prop allows you to pass in data that will be passed back to the event handler.
+   */
+		eventData: _propTypes2.default.object,
+		/*
+   * Pill Label
+   */
+		labels: (0, _airbnbPropTypes.shape)({
+			label: _propTypes2.default.string,
+			remove: _propTypes2.default.string,
+			title: _propTypes2.default.string
+		}),
+		/*
+   * Pill Title
+   */
+		title: _propTypes2.default.string,
+		/*
+   * Callback called when pill is clicked, delete is pressed, or backspace is pressed.
+   */
+		events: (0, _airbnbPropTypes.shape)({
+			onRequestRemove: _propTypes2.default.func
+		})
+	}; /* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+	/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+	var defaultProps = {
+		assistiveText: /* eslint-disable jsx-a11y/no-static-element-interactions */(0, _airbnbPropTypes.shape)({
+			pressDeleteOrBackspace: 'Press delete or backspace to remove'
+		}),
+		labels: {
+			remove: 'Remove'
+		},
+		events: {}
+	};
+
+	var Pill = function Pill(props) {
+		var assistiveText = (0, _lodash2.default)({}, defaultProps.assistiveText, props.assistiveText);
+		var labels = (0, _lodash2.default)({}, defaultProps.labels, props.labels);
+
+		return _react2.default.createElement(
+			'span',
+			{
+				className: 'slds-pill',
+				role: 'option',
+				tabIndex: 0,
+				'aria-selected': 'true'
+			},
+			_react2.default.createElement(
+				'span',
+				{ className: 'slds-pill__label', title: labels.title },
+				labels.label
+			),
+			props.events.onRequestRemove ? _react2.default.createElement(
+				'span',
+				{
+					className: 'slds-icon_container slds-pill__remove',
+					title: labels.remove,
+					onClick: function onClick(event) {
+						props.events.onRequestRemove(event, {
+							events: props.events,
+							eventData: props.eventData
+						});
+					}
+				},
+				_react2.default.createElement(_utilityIcon2.default, {
+					'aria-hidden': 'true',
+					category: 'utility',
+					className: 'slds-icon slds-icon--x-small slds-icon-text-default',
+					name: 'close'
+				}),
+				_react2.default.createElement(
+					'span',
+					{ className: 'slds-assistive-text' },
+					assistiveText.pressDeleteOrBackspace
+				)
+			) : null
+		);
+	};
+
+	Pill.displayName = 'Pill';
+	Pill.propTypes = propTypes;
+	Pill.defaultProps = defaultProps;
+
+	exports.default = Pill;
+});
---
+++ icons/action/new_custom12.js
@@ -0,0 +1,8 @@
+define(["module"], function (module) {
+  "use strict";
+
+  /* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+  /* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+  module.exports = { "viewBox": "0 0 24 24", "circle": { "cx": "12", "cy": "12", "r": "11.077" } };
+});
