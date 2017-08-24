86f380ca927fe28583c285067e85c46448f961c2
Donielle Berg <donielle.berg@gmail.com>
2017-08-24 11:39:46 -0700

    Make Input examples more usable

--- examples/forms/input/stories.jsx
+++ examples/forms/input/stories.jsx
@@ -40,7 +40,7 @@ storiesOf(FORMS_INPUT, module)
 		<section>
 			<ol>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with left icon</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">1. Base Input with left icon</h1>
 					<Input
 						id="with-left-icon"
 						label="My label"
@@ -49,7 +49,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with Clickable left icon</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">2. Base Input with Clickable left icon</h1>
 					<Input
 						id="with-left-clickable-icon"
 						label="My Label"
@@ -58,7 +58,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with right icon</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">3. Base Input with right icon</h1>
 					<Input
 						id="with-right-icon"
 						label="My Label"
@@ -67,7 +67,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with Clickable right icon</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">4. Base Input with Clickable right icon</h1>
 					<Input
 						id="with-right-clickable-icon"
 						label="My Label"
@@ -76,7 +76,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with left and right icons</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">5. Base Input with left and right icons</h1>
 					<Input
 						label="My Label"
 						iconLeft={searchIcon}
@@ -85,7 +85,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with Clickable left and right icon</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">6. Base Input with Clickable left and right icon</h1>
 					<Input
 						label="My label"
 						iconLeft={searchIconClickable}
@@ -94,7 +94,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with left and clickable right icon</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">7. Base Input with left and clickable right icon</h1>
 					<Input
 						label="My Label"
 						iconLeft={searchIcon}
@@ -103,7 +103,7 @@ storiesOf(FORMS_INPUT, module)
 					/>
 				</li>
 				<li className="slds-p-bottom--large">
-					<h1 className="slds-text-title_caps slds-p-vertical--medium">Base Input with left, clickable right icon, and loading spinner</h1>
+					<h1 className="slds-text-title_caps slds-p-vertical--medium">8. Base Input with left, clickable right icon, and loading spinner</h1>
 					<Input
 						name="right-clickable-icon"
 						label="My Label"
@@ -117,44 +117,59 @@ storiesOf(FORMS_INPUT, module)
 		</section>
 	))
 	.add('Fixed Text', () => (
-		<Input
-			name="fixed-text"
-			label="My Label"
-			fixedTextLeft="$"
-			placeholder="My placeholder"
-		/>
+		<section>
+			<h1 className="slds-text-title_caps slds-p-vertical--medium">Input with Fixed Text</h1>
+			<Input
+				name="fixed-text"
+				label="My Label"
+				fixedTextLeft="$"
+				placeholder="My placeholder"
+			/>
+		</section>
 	))
 	.add('Read Only', () => (
-		<Input
-			name="read-only"
-			label="My Label"
-			readOnly
-			value="Read Only Value"
-		/>
+		<section>
+			<h1 className="slds-text-title_caps slds-p-vertical--medium">Read only Input</h1>
+			<Input
+				name="read-only"
+				label="My Label"
+				readOnly
+				value="Read Only Value"
+			/>
+		</section>
 	))
 	.add('Static Input', () => (
-		<Input
-			name="static-input"
-			label="My Label"
-			isStatic
-			value="Static value"
-		/>
+		<section>
+			<h1 className="slds-text-title_caps slds-p-vertical--medium">Static Input</h1>
+			<Input
+				name="static-input"
+				label="My Label"
+				isStatic
+				value="Static value"
+			/>
+		</section>
 	))
 	.add('Disabled Input', () => (
-		<Input
-			name="disabled-input"
-			label="My Label"
-			disabled
-			value="Disabled value"
-		/>
+		<section>
+			<h1 className="slds-text-title_caps slds-p-vertical--medium">Disabled Input</h1>
+			<Input
+				name="disabled-input"
+				label="My Label"
+				disabled
+				value="Disabled value"
+			/>
+		</section>
 	))
 	.add('Required Input in Error State', () => (
-		<Input
-			aria-describedby="error-1"
-			name="required-input-error"
-			label="My Label"
-			required
-			errorText="Error Message"
-			placeholder="My placeholder"
-		/>
+		<section>
+			<h1 className="slds-text-title_caps slds-p-vertical--medium">Required Input with Error</h1>
+			<Input
+				aria-describedby="error-1"
+				name="required-input-error"
+				label="My Label"
+				required
+				errorText="Error Message"
+				placeholder="My placeholder"
+			/>
+		</section>
 	));
