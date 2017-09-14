7c6c62cf0bac4bfd543330adb0b89eaa107a16e3
Lexis Hanson <lhanson@salesforce.com>
2017-09-14 14:19:31 -0700

    WIP fallback tests

--- tests/avatar/avatar.test.jsx
+++ tests/avatar/avatar.test.jsx
@@ -3,7 +3,7 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import assign from 'lodash.assign';
 import TestUtils from 'react-addons-test-utils';
 import { expect } from 'chai';
@@ -15,7 +15,7 @@ describe('SLDSAvatar: ', () => {
 		let wrapper;
 
 		beforeEach(() => {
-			wrapper = shallow(<SLDSAvatar />);
+			wrapper = mount(<SLDSAvatar />);
 		});
 
 		it('avatar renders with image', () => {
@@ -24,14 +24,23 @@ describe('SLDSAvatar: ', () => {
 			const img = wrapper.find('img');
 			const src = img.prop('src');
 			expect(src).to.equal(expectedSrc);
-			// expect(avatar).ton.not.equal(undefined);
 		});
 
-		it('renders proper icon size class', function () {
-			wrapper.setProps({ size: 'large' });
-			const avatar = wrapper.find('.slds-avatar .slds-avatar_large');
-			expect(avatar).to.not.be.undefined;
+		it('calls error handler if image is not valid', () => {
+			const expectedSrc = 'invalid-url';
+			wrapper.setProps({ imgSrc: expectedSrc });
+			const img = wrapper.find('img');
+			img.simulate('error');
+			console.log(wrapper.state('imgLoadError'));
+			expect(wrapper.state('imgLoadError')).to.be.true;
 		});
+
+		it('renders proper icon size class', () => {
+			wrapper.setProps({ size: 'large' });
+			const avatar = wrapper.find('.slds-avatar_large');
+			expect(avatar.node).to.not.be.undefined;
+		});
+
 
 		describe('variant is a user', () => {
 			beforeEach(() => {
@@ -39,45 +48,35 @@ describe('SLDSAvatar: ', () => {
 			});
 
 			it('displays as a circle', () => {
-				const avatar = wrapper.find('.slds-avatar .slds-avatar_circle');
-				expect(avatar).to.not.be.undefined;
-			});
-		});
-
-		describe('variant is an entity', () => {
-			beforeEach(() => {
-				wrapper.setProps({ variant: 'entity' });
-			});
-
-			it('displays as an account', () => {
-				const avatar = wrapper.find('.slds-avatar .slds-avatar_account');
-				expect(avatar).to.not.be.undefined;
+				const avatar = wrapper.find('.slds-avatar_circle');
+				expect(avatar.node).to.not.be.undefined;
 			});
 		});
 	});
 
 	describe('Avatar fallback check', () => {
-		it('calls error handler if image is not valid', () => {
-			const avatar = shallow(<SLDSAvatar
-				imgSrc="invalid-url"
-			/>);
-			const img = avatar.find('img');
-			img.simulate('error');
-			console.log(avatar.state('imgLoadError'));
-			expect(avatar.state('imgLoadError')).to.be.true;
+		let wrapper;
+		beforeEach(() => {
+			wrapper = shallow(<SLDSAvatar />);
 		});
+
+		it('renders fallback initials abbr node if label exists', () => {
+			const avatar = wrapper.setProps({ label: 'test' });
+			const abbr = wrapper.find('abbr');
+			expect(abbr).to.not.be.undefined;
+			expect(abbr).to.not.be.null;
+		});
+		it('renders first two letters of one word if label is one word', () => {
+			const avatar = wrapper.setProps({ label: 'Salesforce' });
+			const abbr = wrapper.find('abbr');
+			expect(abbr.node.props.children).to.equal('Sa');
+		});
+		//
+		// it('renders icon fallback avatar if no label exists', () => {
+		//
+		// });
 	});
-	// it('invokes renderInitialsAvatar fallback if label is passed', () => {
-	//   Simulate.{eventName}(
-	//   element,
-	//   [eventData]
-	// )
-	// 	expect();
-	// });
-	// it('invokes renderInitialsAvatar if no label is passed', () => {
-	// 	expect();
-	// });
-	//
+
 	// describe('Initals avatar fallback', () => {
 	// 	it('builds expected initials', () => {
 	// 		expect();
