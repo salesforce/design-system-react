46a049cf0ec7e73d4fb364009ec984d0a95afc83
Build Server <sjames@salesforce.com>
2017-09-14 23:09:47 +0000

    Release commit for 0.6.21-commonjs [ci skip]

---
+++ components/menu-list/list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _index = require('../utilities/menu-list/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _index2.default; // Alias
---
+++ icons/utility/right.js
@@ -0,0 +1,6 @@
+"use strict";
+
+/* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+module.exports = { "viewBox": "0 0 24 24", "path": { "d": "M6.5 20.2V3.8c0-.4.6-.8 1-.4l9.8 8c.3.3.3.9 0 1.2l-9.8 8c-.4.4-1 .1-1-.4z" } };
