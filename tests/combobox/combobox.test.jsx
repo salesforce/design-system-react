6e3f0b20b1e249aab682359466df21bc7ff94aa1
Stephen James <llama@interactivellama.com>
2017-09-14 00:01:51 -0600

    Comobox: Add initial Mocha test

---
+++ tests/combobox/combobox.test.jsx
@@ -0,0 +1,202 @@
+// Import your external dependencies
+import React from 'react';
+import PropTypes from 'prop-types';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
+
+/* Enzyme Helpers that can mount and unmount React component instances to
+ * the DOM and set `this.wrapper` and `this.dom` within Mocha's `this`
+ * context [full source here](tests/enzyme-helpers.js). `this` can
+ * only be referenced if inside `function () {}`.
+ */
+import { createMountNode, destroyMountNode } from '../enzyme-helpers';
+
+// Import your internal dependencies (for example):
+import Combobox from '../../components/combobox';
+import Icon from '../../components/icon';
+import filter from '~/components/combobox/filter';
+import KEYS, { keyObjects } from '../../utilities/key-code';
+
+/* Set Chai to use chaiEnzyme for enzyme compatible assertions:
+ * https://github.com/producthunt/chai-enzyme
+ */
+chai.use(chaiEnzyme());
+
+const accounts = [
+	{ id: '1', label: 'Acme', subTitle: 'Account • San Francisco', type: 'account' },
+	{ id: '2', label: 'Salesforce.com, Inc.', subTitle: 'Account • San Francisco', type: 'account' },
+	{ id: '3', label: 'Paddy\'s Pub', subTitle: 'Account • Boston, MA', type: 'account' },
+	{ id: '4', label: 'Tyrell Corp', subTitle: 'Account • San Francisco, CA', type: 'account' },
+	{ id: '5', label: 'Paper St. Soap Company', subTitle: 'Account • Beloit, WI', type: 'account' },
+	{ id: '6', label: 'Nakatomi Investments', subTitle: 'Account • Chicago, IL', type: 'account' },
+	{ id: '7', label: 'Acme Landscaping', type: 'account' },
+	{ id: '8', label: 'Acme Construction', subTitle: 'Account • Grand Marais, MN', type: 'account' }
+];
+
+const accountsWithIcon = accounts.map((elem) => Object.assign(elem, {
+	icon: <Icon
+		assistiveText="Account"
+		category="standard"
+		name={elem.type}
+	/> })
+);
+
+const defaultProps = {
+	id: 'combobox-unique-id',
+	isInline: true,
+	labels: {
+		label: 'Search',
+		placeholder: 'Search Salesforce'
+	}
+};
+
+/* A re-usable demo component fixture outside of `describe` sections
+ * can accept props within each test and be unmounted after each tests.
+ * This wrapping component will be similar to your wrapping component
+ * you will create in the React Storybook for manual testing.
+ */
+const DemoComponent = React.createClass({
+	displayName: 'ComboboxDemoComponent',
+	propTypes: {
+		isOpen: PropTypes.bool
+	},
+
+	getDefaultProps () {
+		return defaultProps;
+	},
+
+	getInitialState () {
+		return {
+			inputValue: '',
+			selection: []
+		};
+	},
+
+	render () {
+		return (
+			<Combobox
+				events={{
+					onChange: (event, { value }) => {
+						this.setState({	inputValue: value });
+					},
+					onPillFocus: (event, data) => {
+						console.log('testonPillFocus');
+					},
+					onRequestRemoveSelectedOption: (event, data) => {
+						this.setState({
+							inputValue: '',
+							selection: data.selection
+						});
+					},
+					onSubmit: (event, { value }) => {
+						this.setState({
+							inputValue: '',
+							selection: [...this.state.selection, {
+								label: value,
+								icon: <Icon
+									assistiveText="Account"
+									category="standard"
+									name="account"
+								/> }] });
+					},
+					onSelect: (event, data) => {
+						this.setState({
+							inputValue: '',
+							selection: data.selection
+						});
+					}
+				}}
+				multiple
+				options={filter({
+					inputValue: this.state.inputValue,
+					options: accountsWithIcon,
+					selection: this.state.selection
+				})}
+				selection={this.state.selection}
+				value={this.state.inputValue}
+				{...this.props}
+			/>
+		);
+	}
+});
+
+const getNodes = ({ wrapper }) => ({
+	combobox: wrapper.find('.slds-combobox'),
+	input: wrapper.find('.slds-combobox input'),
+	menuListbox: wrapper.find('.slds-combobox .slds-listbox.slds-dropdown'),
+	selectedListbox: wrapper.find(`#${defaultProps.id}-selected-listbox .slds-listbox`)
+});
+
+/* All tests for component being tested should be wrapped in a root `describe`,
+ * which should be named after the component being tested.
+ * When read aloud, the cumulative `describe` and `it` names should form a coherent
+ * sentence, eg "Date Picker default structure and css is present with expected
+ * attributes set". If you are having trouble constructing a cumulative short
+ * sentence, this may be an indicator that your test is poorly structured.
+ * String provided as first parameter names the `describe` section. Limit to nouns
+ * as much as possible/appropriate.`
+ */
+describe('SLDSCombobox', function () {
+	let mountNode;
+	let wrapper;
+
+	// const comboboxSelector = '.slds-combobox';
+	// const inputSelector = '.slds-combobox input';
+	// const menuListboxSelector = '.slds-combobox .slds-listbox.slds-dropdown';
+	// const selectedListboxSelector = `.slds-combobox #${defaultProps.id}-selected-listbox .slds-listbox`;
+
+	describe('Assistive technology and keyboard interactions', () => {
+		/* Detect if presence of accessibility features such as ARIA
+		 * roles and screen reader text is present in the DOM.
+		 */
+		beforeEach(() => {
+			mountNode = createMountNode({ context: this });
+		});
+
+		afterEach(() => {
+			destroyMountNode({ wrapper, mountNode });
+		});
+
+		it('has aria-haspopup, aria-expanded is false when closed, aria-expanded is true when open, ', function () {
+			wrapper = mount(<DemoComponent />, { attachTo: mountNode });
+			const nodes = getNodes({ wrapper });
+			expect(nodes.combobox.node.getAttribute('aria-haspopup')).to.equal('listbox');
+			// closed
+			expect(nodes.combobox.node.getAttribute('aria-expanded')).to.equal('false');
+			// open
+			nodes.input.simulate('click', {});
+			expect(nodes.combobox.node.getAttribute('aria-expanded')).to.equal('true');
+		});
+
+		it('menu filters to second item, menu listbox menu item 2 aria-selected is true, input activedescendent has item 2 id, after pressing down arrow, enter selects item 2', function () {
+			wrapper = mount(<DemoComponent isOpen />, { attachTo: mountNode });
+			let nodes = getNodes({ wrapper });
+			nodes.input.simulate('focus');
+			nodes.input.simulate('change', { target: { value: accounts[1].label } });
+			nodes.input.simulate('keyDown', keyObjects.DOWN);
+			expect(nodes.menuListbox.node.firstChild.firstChild.getAttribute('aria-selected')).to.equal('true');
+			expect(nodes.input.node.getAttribute('aria-activedescendant')).to.equal(`${defaultProps.id}-listbox-option-2`);
+			// select
+			nodes.input.simulate('keyDown', keyObjects.ENTER);
+			nodes = getNodes({ wrapper });
+			expect(nodes.input.node.getAttribute('value')).to.equal('');
+			expect(nodes.selectedListbox.find('.slds-pill__label').text()).to.equal(accounts[1].label);
+		});
+
+		// NOT IMPLEMENTED
+		// it('Selected Listbox: remove initial first pill, remove third initial item, cycles focus (first to last), removes last and initial fifth pill, cycles focus (last to first), remove inital second and fourth pill', function () {
+		// 	wrapper = mount(<DemoComponent
+		// 		selection={[accounts[0], accounts[1], accounts[2], accounts[3], accounts[4]]}
+
+		// 	/>, { attachTo: mountNode });
+		// 	const nodes = getNodes({ wrapper });
+		// 	nodes.input.simulate('focus');
+		// 	nodes.input.simulate('keyDown', keyObjects.TAB);
+	
+		// 	nodes.selectedListbox.children().at(0).childAt(0).simulate('keyDown', keyObjects.DELETE);
+
+		// 	expect('test').to.equal('test');
+		// });
+	});
+});
