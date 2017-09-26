14dccc5fbbd4b5ccab2c6e70ae39ee5c19d14b10
Donielle Berg <donielle.berg@gmail.com>
2017-09-25 17:19:00 -0700

    Tree: Fix bug in Down Icon in expanded state

--- components/tree/private/branch.jsx
+++ components/tree/private/branch.jsx
@@ -161,7 +161,7 @@ const renderBranch = (children, props) => {
 				{/* eslint-enable jsx-a11y/no-static-element-interactions */}
 				<Button
 					assistiveText="Toggle"
-					iconName={props.node.expanded ? 'chevrondown' : 'chevronright'}
+					iconName="chevronright"
 					iconSize="small"
 					variant="icon"
 					className="slds-m-right--small"
