89d3ae2fc55f4a2361eafb35ad60acf98b989cba
Stephen James <llama@interactivellama.com>
2017-09-27 18:25:42 -0600

    Additional createReactClass use

--- examples/forms/input/default.jsx
+++ examples/forms/input/default.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Input from '~/components/forms/input'; // `~` is replaced with design-system-react at runtime
+import createReactClass from 'create-react-class';
 
-const Example = React.createClass({
+const Example = createReactClass({
 	displayName: 'BaseInputExample',
 
 	render () {
--- examples/forms/input/inactiveInputs.jsx
+++ examples/forms/input/inactiveInputs.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import createReactClass from 'create-react-class';
 import Input from '~/components/forms/input'; // `~` is replaced with design-system-react at runtime
 
-const Example = React.createClass({
+const Example = createReactClass({
 	displayName: 'InactiveInputExamples',
 
 	render () {
