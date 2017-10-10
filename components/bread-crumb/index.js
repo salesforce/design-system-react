93a01ba0ca02979ba700e297d78c55fb492c19f9
Build Server <sjames@salesforce.com>
2017-10-10 22:01:18 +0000

    Release commit for 0.7.1-amd [ci skip]

---
+++ components/bread-crumb/index.js
@@ -0,0 +1,17 @@
+define(['exports', '../breadcrumb'], function (exports, _breadcrumb) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  var _breadcrumb2 = _interopRequireDefault(_breadcrumb);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  exports.default = _breadcrumb2.default;
+});
