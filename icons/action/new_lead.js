b99e4d178ce628e25feca928559545222b756db5
Build Server <sjames@salesforce.com>
2017-10-10 22:01:13 +0000

    Release commit for 0.7.1-es [ci skip]

---
+++ icons/action/new_lead.js
@@ -0,0 +1,5 @@
+/* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+
+export default {"viewBox":"0 0 24 24","xmlns":"http://www.w3.org/2000/svg","circle":{"cx":"12","cy":"4.246","r":"3.323"},"path":{"d":"M22.3 9.8H1.7c-.8 0-1.1.9-.5 1.3l5.4 3.5c.3.2.5.5.3.8l-2 6.8c-.2.7.8 1.2 1.3.6l5.3-5.5c.3-.3.8-.3 1.1 0l5.3 5.5c.5.6 1.4.1 1.3-.6l-2.1-6.8c-.1-.3 0-.7.3-.9l5.4-3.4c.6-.4.3-1.3-.5-1.3z"}};
---
+++ icons/utility/copy_to_clipboard.js
@@ -0,0 +1,5 @@
+/* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+
+export default {"viewBox":"0 0 24 24","xmlns":"http://www.w3.org/2000/svg","path":{"d":"M8 5.4h8c.4 0 .8-.4.8-.8V3.1c0-1.2-1-2.2-2.2-2.2H9.5c-1.2 0-2.2 1-2.2 2.2v1.5c0 .4.3.8.7.8zm12-2.6h-.8c-.2 0-.3.1-.3.3v1.5c0 1.6-1.3 3-2.9 3H8c-1.6 0-2.9-1.4-2.9-3V3.1c0-.2-.1-.3-.3-.3H4c-1.2 0-2.2 1-2.2 2.2v15.9c0 1.2 1 2.2 2.2 2.2h16c1.2 0 2.2-1 2.2-2.2V5c0-1.2-1-2.2-2.2-2.2z"}};
