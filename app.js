c38e2a129a7d635a6055d9938f2a8726572d0e65
Stephen James <llama@interactivellama.com>
2017-10-18 17:12:19 -0600

    Remove auth from deploy apps

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /* eslint-disable consistent-return */
 /* eslint-disable no-console */
-const auth = require('basic-auth');
 const compression = require('compression');
 const express = require('express');
 
@@ -8,28 +7,6 @@ const port = process.env.PORT || 9001;
 
 // Create server
 const app = express();
-
-if (process.env.NODE_ENV === 'production') {
-	// Authentication
-	app.use((req, res, next) => {
-		const user = auth(req);
-		const username = process.env.AUTH_USERNAME;
-		const password = process.env.AUTH_PASSWORD;
-
-		const unauthorized = function () {
-			res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-			res.sendStatus(401);
-		};
-
-		if (!username || !password) {
-			return unauthorized();
-		}	else if (!user || user.name !== username || user.pass !== password) {
-			return unauthorized();
-		}
-
-		next();
-	});
-}
 
 // Compress all files
 app.use(compression());
