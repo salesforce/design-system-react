3a9dae0c888bfb0e389ea82b29f15bc045676859
lexis <lexishanson@gmail.com>
2017-10-19 12:26:32 -0700

    DOM and HTML testing for avatar snapshots. Update examples to 'createReactClass'

--- tests/avatar/avatar.snapshot-test.jsx
+++ tests/avatar/avatar.snapshot-test.jsx
@@ -9,38 +9,27 @@ import SnapshotEntityInitials from '../../examples/avatar/entity-initials';
 import SnapshotUserIcon from '../../examples/avatar/user-icon';
 import SnapshotUserInitials from '../../examples/avatar/user-initials';
 
-
 test('Avatar Base DOM Snapshot', () => {
-	const domTree = renderer.create(
-		<SnapshotBase />
-	).toJSON();
+	const domTree = renderer.create(<SnapshotBase />).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
 
 test('Avatar Entity Icon Snapshot', () => {
-	const domTree = renderer.create(
-		<SnapshotEntityIcon />
-	).toJSON();
+	const domTree = renderer.create(<SnapshotEntityIcon />).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
 
 test('Avatar Entity Initials Snapshot', () => {
-	const domTree = renderer.create(
-		<SnapshotEntityInitials />
-	).toJSON();
+	const domTree = renderer.create(<SnapshotEntityInitials />).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
 
 test('Avatar User Icon Snapshot', () => {
-	const domTree = renderer.create(
-		<SnapshotUserIcon />
-	).toJSON();
+	const domTree = renderer.create(<SnapshotUserIcon />).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
 
 test('Avatar User Initials Snapshot', () => {
-	const domTree = renderer.create(
-		<SnapshotUserInitials />
-	).toJSON();
+	const domTree = renderer.create(<SnapshotUserInitials />).toJSON();
 	expect(domTree).toMatchSnapshot();
 });
