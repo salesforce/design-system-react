ca19977e7380706482b479b7d5b56e947c1b8a85
Kek T <tankevin86@gmail.com>
2017-11-24 10:27:46 +0100

    focus to input only if Dialog was previously opened

--- components/date-picker/date-picker.jsx
+++ components/date-picker/date-picker.jsx
@@ -313,10 +313,10 @@ class Datepicker extends React.Component {
 
 		if (this.getIsOpen()) {
 			this.setState({ isOpen: false });
-		}
-
-		if (this.inputRef) {
-			this.inputRef.focus();
+			
+			if (this.inputRef) {
+				this.inputRef.focus();
+			}
 		}
 	}
 
