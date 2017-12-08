e20669f9601f0527b1af94158da9b502c3d7a182
Christopher McCulloh <cmcculloh@salesforce.com>
2017-12-08 16:17:10 -0500

    (GH1099) adds jest image snapshot capabilities to project

--- tests/snapshot-helpers.jsx
+++ tests/snapshot-helpers.jsx
@@ -3,6 +3,12 @@ import ReactDOMServer from 'react-dom/server';
 import jsBeautify from 'js-beautify';
 import Settings from './settings';
 import renderer from 'react-test-renderer';
+
+const { Chrome } = require('navalia');
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+expect.extend({ toMatchImageSnapshot });
+
 /*
  * Render React components to DOM state as a String
  *
@@ -23,13 +29,33 @@ const renderMarkup = (Component, props) => String(
 	'utf-8'
 	);
 
-const testDOMandHTML = ({ name, test, Component }) => {
+const testDOMandHTML = ({ name, test, Component, ComponentKind }) => {
 	test(`${name} DOM Snapshot`, () => {
 		expect(renderDOM(Component)).toMatchSnapshot();
 	});
 
 	test(`${name} HTML Snapshot`, () => {
 		expect(renderMarkup(Component)).toMatchSnapshot();
+	});
+
+	describe(`${name} Visual Snapshot`, () => {
+		let chrome = null;
+
+		beforeEach(() => {
+			chrome = new Chrome();
+		});
+
+		afterEach(() => {
+			chrome.done();
+		});
+
+		const url = `http://localhost:9001/?selectedKind=${encodeURIComponent(ComponentKind)}&selectedStory=${encodeURIComponent(name)}&full=0&down=1&left=1&panelRight=0&downPanel=storybook%2Factions%2Factions-panel`;
+		console.log('url', url);
+		it('should still look the same', () =>
+			chrome.goto(url)
+				.then(() => chrome.screenshot())
+				.then((image) => expect(image).toMatchImageSnapshot())
+		);
 	});
 };
 
