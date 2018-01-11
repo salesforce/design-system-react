7ae2d5a31847e23ded80a11ceeaef3d884d44772
Build Server <sjames@salesforce.com>
2018-01-11 09:52:22 +0000

    Release commit for 0.8.2-es [ci skip]

---
+++ icons/standard/quip.js
@@ -0,0 +1,5 @@
+/* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+
+export default {"viewBox":"0 0 24 24","xmlns":"http://www.w3.org/2000/svg","title":{},"path":{"fill-rule":"evenodd","clip-rule":"evenodd","d":"M19.1 17.9l-1.2 1.2c-.1.1-.4.1-.5 0L12 13.7V12h1.7l1.6 1.6V8.7H8.7v6.6H12l2.4 2.4H7.1c-.5 0-.8-.3-.8-.8V7.1c0-.5.3-.8.8-.8h9.8c.5 0 .9.3.9.8V16l1.3 1.4c.1.1.1.4 0 .5z"}};
---
+++ icons/utility/process.js
@@ -0,0 +1,5 @@
+/* Copyright (c) 2015-present, salesforce.com, inc. All rights reserved */
+/* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
+
+
+export default {"viewBox":"0 0 24 24","xmlns":"http://www.w3.org/2000/svg","path":{"d":"M7.5 10.7l3.9-4.9c.3-.4.8-.4 1.1 0l3.9 5c.2.1.4.3.6.3h4.4c.4 0 .8-.3.8-.7V3.7c0-1-.9-1.9-1.9-1.9H3.7c-1 0-1.9.9-1.9 1.9v6.7c0 .4.4.7.7.7h4.4c.3 0 .4-.2.6-.4zm9 2.6l-3.9 4.9c-.3.4-.9.4-1.2 0l-3.9-5c-.2-.1-.3-.3-.6-.3H2.5c-.3 0-.7.3-.7.7v6.7c0 1 .9 1.9 1.9 1.9h16.6c1 0 1.9-.9 1.9-1.9v-6.7c0-.4-.4-.7-.7-.7H17c-.2 0-.4.2-.5.4z"}};
