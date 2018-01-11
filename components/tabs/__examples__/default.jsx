49c755b1b26a45ae5a0e1dc3484567fc35b2896b
lexis <lexishanson@gmail.com>
2018-01-11 14:49:53 -0800

    add popover to docs site examples

--- components/tabs/__examples__/default.jsx
+++ components/tabs/__examples__/default.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
 import IconSettings from '~/components/icon-settings';
+import Popover from '~/components/popover';
 import Tabs from '~/components/tabs'; // `~` is replaced with design-system-react at runtime
 import TabsPanel from '~/components/tabs/panel';
 
 const Example = createReactClass({
 	displayName: 'TabsExample',
-	
+
 	render () {
 		return (
 			<IconSettings iconPath="/assets/icons">
@@ -19,6 +20,19 @@ const Example = createReactClass({
 					</TabsPanel>
 					<TabsPanel label="Item Three">
 						Item Three Content
+					</TabsPanel>
+					<TabsPanel
+						label="Popover"
+						popover={
+							<Popover
+								body={'This is a sample popover'}
+								align="right"
+								position="absolute"
+								heading="Popover"
+							/>
+						}
+					>
+						Item Content With Popover.
 					</TabsPanel>
 					<TabsPanel disabled label="Disabled">
 						Disabled Content
