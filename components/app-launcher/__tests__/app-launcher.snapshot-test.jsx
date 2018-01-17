4ea13635869cb7d5af28e2ff6ccc25e7eb5a1a9d
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-17 15:57:01 -0500

    (GH1099) app launcher tests

--- components/app-launcher/__tests__/app-launcher.snapshot-test.jsx
+++ components/app-launcher/__tests__/app-launcher.snapshot-test.jsx
@@ -10,8 +10,60 @@ describe(APP_LAUNCHER, () => {
 	// 	testDOMandHTML(Base);
 	// });
 
-	test('Base Image Snapshot looks the same', async () => {
-		await testImageSnapshot(APP_LAUNCHER, 'Base');
+	test('App Launcher (open) Image Snapshot looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'App Launcher (open)');
+	});
+
+	test('App Launcher Image Snapshot looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'App Launcher');
+	});
+
+	test('App Launcher no header button Image Snapshot looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'App Launcher no header button');
+	});
+
+	test('App Launcher no search Image Snapshot looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'App Launcher no search');
+	});
+
+	test('App Launcher with several sections (no toggle) looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'App Launcher with several sections (no toggle)');
+	});
+
+	test('Tile looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Tile looks the same');
+	});
+
+	test('Small Tile looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Small Tile');
+	});
+
+	test('Tile with Icon node looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Tile with Icon node');
+	});
+
+	test('Tile with icon text looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Tile with icon text');
+	});
+
+	test('Tile with search text looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Tile with search text');
+	});
+
+	test('Tile with truncated text looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Tile with truncated text');
+	});
+
+	test('Tile with description heading looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Tile with description heading');
+	});
+
+	test('Section looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Section');
+	});
+
+	test('Section with small tiles looks the same', async () => {
+		await testImageSnapshot(APP_LAUNCHER, 'Section with small tiles');
 	});
 });
 
