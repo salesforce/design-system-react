79c646f80096584d0b7c046d6ae6bf0716fb4fb0
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-17 15:59:31 -0500

    (GH1099) components/global-header tests

---
+++ components/global-header/__tests__/global-header.snapshot-test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { testDOMandHTML, testImageSnapshot } from '../../../tests/snapshot-helpers';
+
+import Default from '../__examples__/default';
+
+import { GLOBAL_HEADER } from '../../../utilities/constants';
+
+describe(GLOBAL_HEADER, () => {
+	// test('Default DOM & HTML Snapshots look the same', () => {
+	// 	testDOMandHTML(Default);
+	// });
+
+	test('Basic (Fluid Layout) Image Snapshot looks the same', async () => {
+		await testImageSnapshot(GLOBAL_HEADER, 'Basic (Fluid Layout)');
+	});
+
+	test('Advanced (Fixed Layout) Image Snapshot looks the same', async () => {
+		await testImageSnapshot(GLOBAL_HEADER, 'Advanced (Fixed Layout)');
+	});
+});
