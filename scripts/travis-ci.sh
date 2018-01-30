3fb1ee5df300dcf28f2970792b7f2720c4296d3c
Christopher McCulloh <cmcculloh@salesforce.com>
2018-01-30 14:22:25 -0500

    breadcrumb trail for travis ci scripts

---
+++ scripts/travis-ci.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+# Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
+./scripts/test.sh $@
