8efbc7aacf15fe6f6b98c8dff592b4aa8ae6ffbc
Sam Varga <svarga@salesforce.com>
2018-04-10 15:24:39 -0400

    Add colorpicker component
    # Conflicts:
    #	components/storybook-stories.js

--- utilities/constants.js
+++ utilities/constants.js
@@ -24,6 +24,7 @@ export const CARD_EMPTY = 'SLDSCardEmpty';
 export const CARD_FILTER = 'SLDSCardFilter';
 export const CARD_FOOTER = 'SLDSCardFooter';
 export const CARD_HEADER = 'SLDSCardHeader';
+export const COLOR_PICKER = 'SLDSColorPicker';
 export const COMBOBOX = 'SLDSCombobox';
 export const DATA_TABLE = 'SLDSDataTable';
 export const DATA_TABLE_CELL = 'SLDSDataTableCell';
