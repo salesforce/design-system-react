802b73e5140e0a1804dfbe59c9214e34f59a4e9c
David <david.lygagnon@salesforce.com>
2018-04-24 17:17:00 -0700

    add check props and move setting default prop iconCategory

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -10,6 +10,7 @@ import requiredIf from 'react-required-if';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ButtonIcon from '../icon/button-icon';
+import checkProps from './check-props';
 import PopoverTooltip from '../popover-tooltip';
 
 import { BUTTON } from '../../utilities/constants';
@@ -193,11 +194,15 @@ const Button = createReactClass({
 			disabled: false,
 			hint: false,
 			iconSize: 'medium',
-			iconCategory: 'utility',
 			responsive: false,
 			type: 'button',
 			variant: 'neutral',
 		};
+	},
+
+	componentWillMount () {
+		// `checkProps` issues warnings to developers about properties (similar to React's built in development tools)
+		checkProps(BUTTON, this.props);
 	},
 
 	getClassName () {
@@ -257,7 +262,7 @@ const Button = createReactClass({
 				: this.props.iconSize;
 		return (
 			<ButtonIcon
-				category={this.props.iconCategory}
+				category={this.props.iconCategory || 'utility'} // BREAKING CHANGE we will introduce in 1.0. For the moment, set default prop here if none specified.
 				className={classNames(
 					{
 						'slds-global-header__icon':
