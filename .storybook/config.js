19abda9b39e6d29e503db0781c8ed5a35edd28da
Stephen James <sjames@salesforce.com>
2018-05-03 15:25:20 -0600

    Add a11y panel to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
 
 function loadStories () {
 	require('../components/storybook-stories'); // eslint-disable-line global-require
 }
+
+addDecorator(checkA11y)
 
 configure(loadStories, module);
