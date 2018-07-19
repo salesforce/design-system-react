6c4ecc85f88f4f70d36b858b0c3f0874e1747768
David Woodward <dwoodward@salesforce.com>
2018-07-19 14:47:18 -0400

    Configure Enzyme for React 16.

--- tests/enzyme-helpers.js
+++ tests/enzyme-helpers.js
@@ -1,4 +1,7 @@
-import { mount } from 'enzyme';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 /*
 	Setup and takedown
