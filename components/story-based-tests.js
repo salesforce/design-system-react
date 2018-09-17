f3bfd40506cbc843a795871a67c497e79155f640
Stephen James <sjames@salesforce.com>
2018-09-17 00:45:07 -0600

    Move Snapshot Tests: Checkbox
    # Conflicts:
    #	components/checkbox/__tests__/__snapshots__/checkbox.snapshot-test.jsx.snap

--- components/story-based-tests.js
+++ components/story-based-tests.js
@@ -25,7 +25,7 @@ export Button from '../components/button/__docs__/storybook-stories';
 export ButtonGroup from '../components/button-group/__docs__/storybook-stories';
 export ButtonStateful from '../components/button-stateful/__docs__/storybook-stories';
 export Card from '../components/card/__docs__/storybook-stories';
-// export Checkbox from '../components/checkbox/__docs__/storybook-stories';
+export Checkbox from '../components/checkbox/__docs__/storybook-stories';
 // export Combobox from '../components/combobox/__docs__/storybook-stories';
 // export Filter from '../components/filter/__docs__/storybook-stories';
 export DataTable from '../components/data-table/__docs__/storybook-stories';
