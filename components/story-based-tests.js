4cac3f6cfb1badb45b69a0a6d41a7faa38e4a62a
Stephen James <sjames@salesforce.com>
2018-09-16 23:45:42 -0600

    Move Snapshot Tests: Avatar

--- components/story-based-tests.js
+++ components/story-based-tests.js
@@ -18,7 +18,7 @@
 export Blank from '../tests/initial-blank-stories';
 export Accordion from '../components/accordion/__docs__/storybook-stories';
 // export Alert from '../components/alert/__docs__/storybook-stories';
-// export Avatar from '../components/avatar/__docs__/storybook-stories';
+export Avatar from '../components/avatar/__docs__/storybook-stories';
 export BrandBand from '../components/brand-band/__docs__/storybook-stories';
 // export Breadcrumb from '../components/breadcrumb/__docs__/storybook-stories';
 // export Button from '../components/button/__docs__/storybook-stories';
