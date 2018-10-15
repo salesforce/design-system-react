05ddeef4f3b48d129a120a8b76c0ed6214b017d0
Cheng Liu <liucheng.tech@outlook.com>
2018-10-15 17:25:52 +0800

    fix typo in spinner example

--- components/spinner/__examples__/default.jsx
+++ components/spinner/__examples__/default.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Spinner from '~/components/slider';
+import Spinner from '~/components/spinner';
 
 class Example extends React.Component {
 	static displayName = 'SpinnerExample';
