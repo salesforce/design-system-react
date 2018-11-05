78a6a941b4ba422c2f2ae5f0e9a04c0d9927975e
kparkerson <kparkerson@salesforce.com>
2018-11-05 11:07:25 -0700

    tests updated, looks like everything is passing now

--- components/input/__examples__/counter-input.jsx
+++ components/input/__examples__/counter-input.jsx
@@ -19,13 +19,14 @@ class Example extends React.Component {
 					<h1 className="slds-text-title_caps slds-p-vertical_medium">
 						1. Simple counter input
 					</h1>
-					<Input label="My Label" variant="counter" />
+					<Input id="counter-input-1" label="My Label" variant="counter" />
 				</div>
 				<div>
 					<h1 className="slds-text-title_caps slds-p-vertical_medium">
 						2. Controlled counter input
 					</h1>
 					<Input
+						id="counter-input-2"
 						label="My Label"
 						onChange={(event, data) => {
 							this.setState({ counter2value: data.value });
@@ -39,6 +40,7 @@ class Example extends React.Component {
 						3. Controlled counter input with min/max values and custom step size
 					</h1>
 					<Input
+						id="counter-input-3"
 						label="My Label"
 						minValue={2}
 						maxValue={30}
@@ -56,6 +58,7 @@ class Example extends React.Component {
 					</h1>
 					<Input
 						defaultValue={10.0}
+						id="counter-input-4"
 						label="My Label"
 						step={0.1}
 						variant="counter"
@@ -65,19 +68,19 @@ class Example extends React.Component {
 					<h1 className="slds-text-title_caps slds-p-vertical_medium">
 						5. Disabled counter input
 					</h1>
-					<Input disabled label="My Label" variant="counter" value="10" />
+					<Input disabled id="counter-input-5" label="My Label" variant="counter" value="10" />
 				</div>
 				<div>
 					<h1 className="slds-text-title_caps slds-p-vertical_medium">
 						6. Static counter input
 					</h1>
-					<Input isStatic label="My Label" variant="counter" value="10" />
+					<Input isStatic id="counter-input-6" label="My Label" variant="counter" value="10" />
 				</div>
 				<div>
 					<h1 className="slds-text-title_caps slds-p-vertical_medium">
 						7. Readonly counter input
 					</h1>
-					<Input label="My Label" readOnly variant="counter" value="10" />
+					<Input id="counter-input-7" label="My Label" readOnly variant="counter" value="10" />
 				</div>
 			</IconSettings>
 		);
