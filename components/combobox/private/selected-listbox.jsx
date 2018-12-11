0db0b87a0b6b8728092afad3c513a831ac60de68
Stephen James <sjames@salesforce.com>
2018-12-10 23:16:25 -0600

    Combobox: Add isEqual back

--- components/combobox/private/selected-listbox.jsx
+++ components/combobox/private/selected-listbox.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import classNames from 'classnames';
+import isEqual from 'lodash.isequal';
 
 import Pill from '../../utilities/pill';
 
