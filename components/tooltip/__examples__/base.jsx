043ab83281cb43b471b84afa77986f585ec54c19
Stephen James <sjames@salesforce.com>
2018-12-27 17:34:44 -0600

    Tooltip: Require onClickTrigger for learn more pattern
    If learnMore Tooltip variant is used without onClickTrigger, then the “no click” basic info icon tooltip will be used with a “disabled” button as trigger. If onClickTrigger is defined, a link will be rendered

--- components/tooltip/__examples__/base.jsx
+++ components/tooltip/__examples__/base.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import IconSettings from '~/components/icon-settings';
 import Tooltip from '~/components/tooltip'; // `~` is replaced with design-system-react at runtime
+import Button from '~/components/button'; // `~` is replaced with design-system-react at runtime
 import Icon from '~/components/icon';
 
 class Example extends React.Component {
@@ -13,16 +14,8 @@ class Example extends React.Component {
 				<Tooltip
 					align="top left"
 					content="Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi."
-				>
-					<a href="javascript:void(0)">
-						<Icon
-							assistiveText={{ label: 'Tooltip with icon' }}
-							category="utility"
-							name="info"
-							size="x-small"
-						/>
-					</a>
-				</Tooltip>
+					variant="learnMore"
+				/>
 			</IconSettings>
 		);
 	}
