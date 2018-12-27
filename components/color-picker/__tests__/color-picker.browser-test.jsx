0c401b78069cf495691a3bbf2603ce496747c00e
Stephen James <sjames@salesforce.com>
2018-12-27 15:54:41 -0600

    Lint: Add max lines per file rule

--- components/color-picker/__tests__/color-picker.browser-test.jsx
+++ components/color-picker/__tests__/color-picker.browser-test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import React from 'react';
 import createReactClass from 'create-react-class';
 import PropTypes from 'prop-types';
--- components/color-picker/index.jsx
+++ components/color-picker/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines */
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
