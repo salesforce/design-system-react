b3a9e85db9d58d2960e9480e6bb63811cfe9a687
Stephen James <sjames@salesforce.com>
2019-01-10 12:26:34 -0700

    DataTable: Add Advanced Single Select Fixed Header Story

--- components/data-table/__docs__/storybook-stories.jsx
+++ components/data-table/__docs__/storybook-stories.jsx
@@ -7,6 +7,7 @@ import { DATA_TABLE } from '../../../utilities/constants';
 
 import Advanced from '../__examples__/advanced';
 import AdvancedSingleSelect from '../__examples__/advanced-single-select';
+import AdvancedSingleSelectFixedHeader from '../__examples__/advanced-single-select-fixed-header';
 import Basic from '../__examples__/basic';
 import BasicFixedLayout from '../__examples__/basic-fixed-layout';
 import FixedHeader from '../__examples__/fixed-header';
@@ -22,5 +23,8 @@ storiesOf(DATA_TABLE, module)
 	.add('Advanced (Fixed Layout)', () => <Advanced log={action} />)
 	.add('Advanced Single Select (Fixed Layout)', () => (
 		<AdvancedSingleSelect log={action} />
+	))
+	.add('Advanced Single Select (Fixed Header)', () => (
+		<AdvancedSingleSelectFixedHeader log={action} />
 	))
 	.add('Fixed Header', () => <FixedHeader />);
