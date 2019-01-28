0a232acf59647dbf32bf6b22126d479085fa4a51
David Ly-Gagnon <david.lygagnon@salesforce.com>
2019-01-27 22:37:15 -0800

    refactor example

--- components/input/__examples__/inline-edit.jsx
+++ components/input/__examples__/inline-edit.jsx
@@ -7,8 +7,18 @@ class Example extends React.Component {
         super(props);
         this.state = {
             isOpen: false,
-            value: "My editable input"
+            inputValue1: null, // stores firstName in edit input field
+            inputValue2: null, // stores lastName in edit input field
+            fullName: "John Smith"
         };
+    }
+
+    onChange = (inputName) => (event, {value}) => {
+        if (inputName === 'input1') {
+            this.setState({inputValue1: value});
+        } else {
+            this.setState({inputValue2: value});
+        }
     }
 
 	handleOpen = () => {
@@ -28,9 +38,17 @@ class Example extends React.Component {
 		}
     };
     handleSave = (event, data) => {
-        this.setState({value: "new value", isOpen: false});
+        this.setState({fullName: `${this.state.input1} ${this.state.input2}`, isOpen: false});
     };
+
 	render() {
+        // Content of EditDialogPopover that is shown when clicking on edit (pencil icon)
+        const editDialogPopoverBody =
+            <div className="slds-col_padded">
+                <Input id="first-name" label="First Name" value={this.state.inputValue1} onChange={this.onChange('input1')}/>
+                <Input id="last-name" label="Last Name" value={this.state.inputValue2} onChange={this.onChange('input2')}/>
+            </div>;
+
 		return (
 			<div>
 				<h1 className="slds-text-title_caps slds-p-vertical_medium">
@@ -40,20 +58,18 @@ class Example extends React.Component {
                     id="inline-edit"
                     editDialogPopover={
                         <PopoverEditDialog
-                            body="Lorem ipsum dolor sit amet, consectetur adipisici"
+                            body={editDialogPopoverBody}
+                            heading="Edit First Name"
+                            isOpen={this.state.isOpen}
                             onCancel={this.handleRequestClose}
                             onClose={this.handleClose}
                             onRequestClose={this.handleRequestClose}
                             onSave={this.handleSave}
-                            heading="Edit First Name"
-                            isOpen={this.state.isOpen}
                             position='absolute'
-                            // onCancel={closeThePopover}
-                            // onSave={changeParentStateAndUpdateInputValue}
                         />}
                     onClickEditButton={this.handleOpen}
-                    label="My Label"
-                    value={this.state.value}
+                    label="Full Name"
+                    value={this.state.fullName}
                     variant="edit-dialog"
 				/>
 			</div>
