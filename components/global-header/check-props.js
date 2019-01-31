2e825a78d0afc59f289d84c5ff3856f2e2400705
kparkerson <kparkerson5@gmail.com>
2019-01-30 19:39:10 -0700

    deprecated GlobalHeaderButton and GlobalHeaderDropdown

--- components/global-header/check-props.js
+++ components/global-header/check-props.js
@@ -2,29 +2,40 @@
 /* Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license */
 /* eslint-disable import/no-mutable-exports */
 
+import componentIsDeprecated from '../../utilities/warning/component-is-deprecated';
 import deprecatedProperty from '../../utilities/warning/deprecated-property';
 import getComponentDocFn from '../../utilities/get-component-doc';
+
+// ## Constants
+import { GLOBAL_HEADER_BUTTON, GLOBAL_HEADER_DROPDOWN } from '../../utilities/constants';
 
 let checkProps = function() {};
 
 if (process.env.NODE_ENV !== 'production') {
 	checkProps = function(COMPONENT, props, jsonDoc) {
-		const createDocUrl = getComponentDocFn(jsonDoc);
+		if (COMPONENT === GLOBAL_HEADER_BUTTON || COMPONENT === GLOBAL_HEADER_DROPDOWN) {
+			componentIsDeprecated(
+				COMPONENT,
+				`${COMPONENT} has been deprecated in favor of more-specific global header subcomponents. Please see docs for updated examples.`
+			);
+		} else {
+			const createDocUrl = getComponentDocFn(jsonDoc);
 
-		deprecatedProperty(
-			COMPONENT,
-			props.skipToContentAssistiveText,
-			'skipToContentAssistiveText',
-			"assistiveText['skipToContent']",
-			createDocUrl('assistiveText')
-		);
-		deprecatedProperty(
-			COMPONENT,
-			props.skipToNavAssistiveText,
-			'skipToNavAssistiveText',
-			"assistiveText['skipToNav']",
-			createDocUrl('assistiveText')
-		);
+			deprecatedProperty(
+				COMPONENT,
+				props.skipToContentAssistiveText,
+				'skipToContentAssistiveText',
+				"assistiveText['skipToContent']",
+				createDocUrl('assistiveText')
+			);
+			deprecatedProperty(
+				COMPONENT,
+				props.skipToNavAssistiveText,
+				'skipToNavAssistiveText',
+				"assistiveText['skipToNav']",
+				createDocUrl('assistiveText')
+			);
+		}
 	};
 }
 
