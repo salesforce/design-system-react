09c59e1ab37886572c4f8bee62af6f6916e4aa33
Stephen James <sjames@salesforce.com>
2019-02-01 21:33:28 -0700

    Jest aXe: Change to JSDOM to allow for aXe testing
    Add svg-transformer to allow non-webpack SVG import

--- components/icon-settings/__docs__/storybook-stories.jsx
+++ components/icon-settings/__docs__/storybook-stories.jsx
@@ -12,5 +12,5 @@ storiesOf(ICON_SETTINGS, module)
 		<div className="slds-p-around_medium">{getStory()}</div>
 	))
 	.add('Base: Icon path', () => <IconPath />)
-	.add('Base: Sprite imports', () => <Sprite />)
-	.add('Base: OnRequestIconPath', () => <OnRequestIconPath />);
+	.add('Base: Sprite imports RemoveTest', () => <Sprite />)
+	.add('Base: OnRequestIconPath RemoveTest', () => <OnRequestIconPath />);
