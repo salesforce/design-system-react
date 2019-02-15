2c7f6d7ab4f1f1f890d9b6c255a2c7bbba2fed49
Lee White <leewhite@salesforce.com>
2019-02-14 20:08:10 -0800

    added not popover -> options required

--- components/combobox/check-props.js
+++ components/combobox/check-props.js
@@ -3,12 +3,13 @@
 /* eslint-disable import/no-mutable-exports */
 
 import deprecatedProperty from '../../utilities/warning/deprecated-property';
+import oneOfRequiredProperty from '../../utilities/warning/one-of-required-property';
 import getComponentDocFn from '../../utilities/get-component-doc';
 
-let checkProps = function() {};
+let checkProps = function () { };
 
 if (process.env.NODE_ENV !== 'production') {
-	checkProps = function(COMPONENT, props, jsonDoc) {
+	checkProps = function (COMPONENT, props, jsonDoc) {
 		const createDocUrl = getComponentDocFn(jsonDoc);
 		/* eslint-disable max-len */
 		deprecatedProperty(
@@ -19,7 +20,22 @@ if (process.env.NODE_ENV !== 'production') {
 			createDocUrl('menuPosition')
 		);
 		/* eslint-enable max-len */
+
+		if (props.variant !== 'popover') {
+			oneOfRequiredProperty(
+				COMPONENT,
+				{
+					options: props.options,
+				},
+				createDocUrl('options')
+			)
+		}
 	};
 }
 
 export default checkProps;
+// create a new required prop, that checks if no options when not popover (look at deprecated property)
+// don't know when this error would happen though (should check)
+
+// add a check for label if popover variant
+// if A, then require B
