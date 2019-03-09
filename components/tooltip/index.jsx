af01099cac3527c5a796dc248fb56aacf8be0fae
Stephen James <sjames@salesforce.com>
2019-03-08 22:05:37 -0700

    Tooltip: Remove key from children clone
    The original key should be passed on to the new element: https://reactjs.org/docs/react-api.html#cloneelement

--- components/tooltip/index.jsx
+++ components/tooltip/index.jsx
@@ -196,9 +196,8 @@ class Tooltip extends React.Component {
 			children = this.props.children;
 		}
 
-		return React.Children.map(children, (child, i) =>
+		return React.Children.map(children, (child) =>
 			React.cloneElement(child, {
-				key: i, // eslint-disable-line react/no-array-index-key
 				'aria-describedby': this.getId(),
 				onBlur: this.handleMouseLeave,
 				onFocus: this.handleMouseEnter,
