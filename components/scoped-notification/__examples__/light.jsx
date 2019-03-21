a6f0059f5b2a7f38cc1297083826fa1671b90b2a
aswinshenoy <aswinshenoy65@gmail.com>
2019-03-21 17:33:59 +0530

    Update with Review Suggestions

--- components/scoped-notification/__examples__/light.jsx
+++ components/scoped-notification/__examples__/light.jsx
@@ -4,19 +4,12 @@ import ScopedNotification from '~/components/scoped-notification';
 class Example extends React.Component {
 	render() {
 		return (
-			<div
-				style={{
-					padding: '4rem 1rem 0px',
-					background: 'rgb(244, 246, 249)',
-				}}
-			>
-				<ScopedNotification theme="light">
-					<p>
-						It looks as if duplicates exist for this lead.{' '}
-						<a href="javascript:void(0);">View Duplicates.</a>
-					</p>
-				</ScopedNotification>
-			</div>
+			<ScopedNotification theme="light">
+				<p>
+					It looks as if duplicates exist for this lead.{' '}
+					<a href="javascript:void(0);">View Duplicates.</a>
+				</p>
+			</ScopedNotification>
 		);
 	}
 }
