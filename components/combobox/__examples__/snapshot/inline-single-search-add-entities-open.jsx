7bb4e2150d800d34d7182f6e99391de00b3a5d5f
David Ly-Gagnon <david.lygagnon@salesforce.com>
2019-05-18 23:10:07 -0700

    reduce number of items in tests/example

--- components/combobox/__examples__/snapshot/inline-single-search-add-entities-open.jsx
+++ components/combobox/__examples__/snapshot/inline-single-search-add-entities-open.jsx
@@ -23,32 +23,7 @@ const accounts = [
 		label: "Paddy's Pub",
 		subTitle: 'Account • Boston, MA',
 		type: 'account',
-	},
-	{
-		id: '4',
-		label: 'Tyrell Corp',
-		subTitle: 'Account • San Francisco, CA',
-		type: 'account',
-	},
-	{
-		id: '5',
-		label: 'Paper St. Soap Company',
-		subTitle: 'Account • Beloit, WI',
-		type: 'account',
-	},
-	{
-		id: '6',
-		label: 'Nakatomi Investments',
-		subTitle: 'Account • Chicago, IL',
-		type: 'account',
-	},
-	{ id: '7', label: 'Acme Landscaping', type: 'account' },
-	{
-		id: '8',
-		label: 'Acme Construction',
-		subTitle: 'Account • Grand Marais, MN',
-		type: 'account',
-	},
+	}
 ];
 
 const accountsWithIcon = accounts.map((elem) => ({
