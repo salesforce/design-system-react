38593ea282c1a7946f3fdbe867422eb7567b5d0a
ashwin <aswinshenoy65@gmail.com>
2019-06-02 03:14:50 +0530

    Convert children to variants of Radio & Checkbox Components

--- components/visual-picker/__examples__/coverable.jsx
+++ components/visual-picker/__examples__/coverable.jsx
@@ -1,28 +1,128 @@
 import React from 'react';
 import VisualPicker from '~/components/visual-picker';
-import Coverable from '~/components/visual-picker/private/Coverable';
+import { Radio, Checkbox } from '~/components';
+import Icon from '~/components/icon';
+import IconSettings from '~/components/icon-settings';
 
 class Example extends React.Component {
 	render() {
 		return (
-			<div
-				style={{
-					padding: '4rem 1rem 0px',
-					background: 'rgb(244, 246, 249)',
-				}}
-			>
-				<h1>Radio</h1>
-				<VisualPicker label="Please Any One Select" inputType="radio">
-					<Coverable title="Connected App" icon="connected_apps" />
-					<Coverable title="Connected App" icon="custom_apps" />
-				</VisualPicker>
-				<hr />
-				<h1>Checkbox</h1>
-				<VisualPicker label="Please Select" inputType="checkbox">
-					<Coverable title="Connected App" icon="connected_apps" />
-					<Coverable title="Connected App" icon="custom_apps" />
-				</VisualPicker>
-			</div>
+			<IconSettings iconPath="/assets/icons">
+				<div
+					style={{
+						padding: '4rem 1rem 0px',
+						background: 'rgb(244, 246, 249)',
+					}}
+				>
+					<h1>Radio</h1>
+					<VisualPicker label="Select any one" coverable>
+						<Radio
+							label="Connected App"
+							onRenderVisualPickerSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="check"
+									colorVariant="base"
+									size="large"
+								/>
+							}
+							onRenderVisualPickerNotSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="connected_apps"
+									size="large"
+								/>
+							}
+						/>
+						<Radio
+							label="Custom App"
+							onRenderVisualPickerSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="check"
+									colorVariant="base"
+									size="large"
+								/>
+							}
+							onRenderVisualPickerNotSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="custom_apps"
+									size="large"
+								/>
+							}
+						/>
+					</VisualPicker>
+					<hr />
+					<h1>Checkbox</h1>
+					<VisualPicker label="Add the following object(s)" coverable>
+						<Checkbox
+							label="Account"
+							onRenderVisualPickerSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="check"
+									colorVariant="base"
+									size="large"
+								/>
+							}
+							onRenderVisualPickerNotSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="standard"
+									name="account"
+									size="large"
+								/>
+							}
+						/>
+						<Checkbox
+							label="Lead"
+							onRenderVisualPickerSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="check"
+									colorVariant="base"
+									size="large"
+								/>
+							}
+							onRenderVisualPickerNotSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="standard"
+									name="lead"
+									size="large"
+								/>
+							}
+						/>
+						<Checkbox
+							label="Orders"
+							onRenderVisualPickerSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="utility"
+									name="check"
+									colorVariant="base"
+									size="large"
+								/>
+							}
+							onRenderVisualPickerNotSelected={
+								<Icon
+									assistiveText={this.props.assistiveText}
+									category="standard"
+									name="orders"
+									size="large"
+								/>
+							}
+						/>
+					</VisualPicker>
+				</div>
+			</IconSettings>
 		);
 	}
 }
