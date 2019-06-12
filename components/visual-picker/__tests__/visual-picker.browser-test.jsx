2e4bd8dbfc90b416b7c24655cc0b074fbf4a071b
ashwin <aswinshenoy65@gmail.com>
2019-06-12 06:03:14 +0530

    Add Testing for Checkbox Variant

--- components/visual-picker/__tests__/visual-picker.browser-test.jsx
+++ components/visual-picker/__tests__/visual-picker.browser-test.jsx
@@ -18,7 +18,7 @@ import IconSettings from '../../icon-settings';
 import Icon from '../../icon';
 import VisualPicker from '../../visual-picker';
 import Radio from '../../radio';
-// import Checkbox from '../../checkbox';
+import Checkbox from '../../checkbox';
 
 chai.use(chaiEnzyme());
 
@@ -35,63 +35,173 @@ class VisualPickerExample extends React.Component {
 			<IconSettings iconPath="/assets/icons">
 				<VisualPicker
 					label={this.props.label}
-					size={this.props.size}
-					coverable={this.props.coverable}
-					vertical={this.props.vertical}
-					id="visual-picker-coverable-radio"
+					id={this.props.id}
+					size="medium"
+					coverable
 				>
-					<Radio
-						labels={{
-							label: 'Connected App',
-						}}
-						id="visual-picker-coverable-radio-1"
-						value="1"
-						checked={this.state.checked === '1'}
-						onChange={() => this.setState({ checked: '1' })}
-						onRenderVisualPickerSelected={
-							<Icon
-								assistiveText={{ label: 'selected-icon' }}
-								category="utility"
-								name="check"
-								colorVariant="base"
-								size="large"
+					{this.props.type === 'radio' ? (
+						<>
+							<Radio
+								labels={{
+									label: 'Connected App',
+								}}
+								id={`${this.props.id}-1`}
+								value="1"
+								checked={this.state.checked === '1'}
+								onChange={() => this.setState({ checked: '1' })}
+								onRenderVisualPickerSelected={
+									<Icon
+										assistiveText={{ label: 'selected-icon' }}
+										category="utility"
+										name="check"
+										colorVariant="base"
+										size="large"
+									/>
+								}
+								onRenderVisualPickerNotSelected={
+									<Icon
+										assistiveText={{ label: 'connected_apps-icon' }}
+										category="utility"
+										name="connected_apps"
+										size="large"
+									/>
+								}
 							/>
-						}
-						onRenderVisualPickerNotSelected={
-							<Icon
-								assistiveText={{ label: 'connected_apps-icon' }}
-								category="utility"
-								name="connected_apps"
-								size="large"
+							<Radio
+								labels={{
+									label: 'Custom App',
+								}}
+								id={`${this.props.id}-2`}
+								value="2"
+								checked={this.state.checked === '2'}
+								onChange={() => this.setState({ checked: '2' })}
+								onRenderVisualPickerSelected={
+									<Icon
+										assistiveText={{ label: 'selected-icon' }}
+										category="utility"
+										name="check"
+										colorVariant="base"
+										size="large"
+									/>
+								}
+								onRenderVisualPickerNotSelected={
+									<Icon
+										assistiveText={{ label: 'custom_apps-icon' }}
+										category="utility"
+										name="custom_apps"
+										size="large"
+									/>
+								}
 							/>
-						}
-					/>
-					<Radio
-						labels={{
-							label: 'Custom App',
-						}}
-						id="visual-picker-coverable-radio-2"
-						value="2"
-						checked={this.state.checked === '2'}
-						onChange={() => this.setState({ checked: '2' })}
-						onRenderVisualPickerSelected={
-							<Icon
-								assistiveText={{ label: 'selected-icon' }}
-								category="utility"
-								name="check"
-								colorVariant="base"
-								size="large"
+						</>
+					) : (
+						<>
+							<Checkbox
+								labels={{
+									label: 'Account',
+								}}
+								value="1"
+								checked={this.state.checked.includes('1')}
+								onChange={() => {
+									if (this.state.checked.includes('1'))
+										this.setState({
+											checked: this.state.checked.filter(
+												(item) => item !== '1'
+											),
+										});
+									else this.setState({ checked: [...this.state.checked, '1'] });
+								}}
+								id="visual-picker-coverable-checkbox-1"
+								onRenderVisualPickerSelected={
+									<Icon
+										assistiveText={this.props.assistiveText}
+										category="utility"
+										name="check"
+										colorVariant="base"
+										size="large"
+									/>
+								}
+								onRenderVisualPickerNotSelected={
+									<Icon
+										assistiveText={this.props.assistiveText}
+										category="standard"
+										name="account"
+										size="large"
+									/>
+								}
 							/>
-						}
-						onRenderVisualPickerNotSelected={
-							<Icon
-								assistiveText={{ label: 'custom_apps-icon' }}
-								category="utility"
-								name="custom_apps"
-								size="large"
+							<Checkbox
+								labels={{
+									label: 'Lead',
+								}}
+								value="2"
+								checked={this.state.checked.includes('2')}
+								onChange={() => {
+									if (this.state.checked.includes('2'))
+										this.setState({
+											checked: this.state.checked.filter(
+												(item) => item !== '2'
+											),
+										});
+									else this.setState({ checked: [...this.state.checked, '2'] });
+								}}
+								id="visual-picker-coverable-checkbox-2"
+								onRenderVisualPickerSelected={
+									<Icon
+										assistiveText={this.props.assistiveText}
+										category="utility"
+										name="check"
+										colorVariant="base"
+										size="large"
+									/>
+								}
+								onRenderVisualPickerNotSelected={
+									<Icon
+										assistiveText={this.props.assistiveText}
+										category="standard"
+										name="lead"
+										size="large"
+									/>
+								}
 							/>
-						}
-					/>
+							<Checkbox
+								labels={{
+									label: 'Orders',
+								}}
+								value="3"
+								checked={this.state.checked.includes('3')}
+								onChange={() => {
+									if (this.state.checked.includes('3'))
+										this.setState({
+											checked: this.state.checked.filter(
+												(item) => item !== '3'
+											),
+										});
+									else {
+										this.setState({ checked: [...this.state.checked, '3'] });
+									}
+								}}
+								id="visual-picker-coverable-checkbox-3"
+								onRenderVisualPickerSelected={
+									<Icon
+										assistiveText={this.props.assistiveText}
+										category="utility"
+										name="check"
+										colorVariant="base"
+										size="large"
+									/>
+								}
+								onRenderVisualPickerNotSelected={
+									<Icon
+										assistiveText={this.props.assistiveText}
+										category="standard"
+										name="orders"
+										size="large"
+									/>
+								}
+							/>
+						</>
+					)}
 				</VisualPicker>
 			</IconSettings>
 		);
@@ -100,14 +210,13 @@ class VisualPickerExample extends React.Component {
 
 VisualPickerExample.propTypes = {
 	label: PropTypes.string,
-	checked: PropTypes.string,
-	coverable: PropTypes.bool,
-	vertical: PropTypes.bool,
-	size: PropTypes.oneOf(['medium', 'large']),
+	id: PropTypes.string,
+	checked: PropTypes.any,
+	type: PropTypes.oneOf(['radio', 'checkbox']),
 };
 
 VisualPickerExample.defaultProps = {
-	size: 'medium',
+	type: 'radio',
 	checked: null,
 };
 
@@ -125,24 +234,9 @@ describe('SLDS Visual Picker', function() {
 		destroyMountNode({ wrapper, mountNode });
 	});
 
-	it('renders coverable visual picker radio variant', () => {
+	it('radio switching works correctly', () => {
 		wrapper = mount(
-			<VisualPickerExample label="Select any one" coverable checked="1" />,
-			{ attachTo: mountNode }
-		);
-		const radios = wrapper.find(Radio);
-		expect(radios).to.have.lengthOf(2, 'there are two radio inputs');
-		const legend = wrapper.find('legend');
-		const div = wrapper.find({ value: '1' }).find('label div');
-		expect(div.props().className).to.equal(
-			'slds-visual-picker__figure slds-visual-picker__icon slds-align_absolute-center'
-		);
-		expect(legend.text()).to.equal('Select any one', 'there is a label');
-	});
-
-	it('radio option switching works correctly', () => {
-		wrapper = mount(
-			<VisualPickerExample label="Select any one" coverable checked="1" />,
+			<VisualPickerExample label="Select any one" type="radio" checked="1" />,
 			{ attachTo: mountNode }
 		);
 		// Get the radio options
@@ -159,5 +253,36 @@ describe('SLDS Visual Picker', function() {
 		// Check if only the second option is selected
 		expect(radio1).to.have.prop('checked', false);
 		expect(radio2).to.have.prop('checked', true);
+	});
+
+	it('checkbox selection works correctly', () => {
+		wrapper = mount(
+			<VisualPickerExample
+				label="Select any one"
+				type="checkbox"
+				checked={['1']}
+			/>,
+			{ attachTo: mountNode }
+		);
+		// Get the checkbox options
+		let cb1 = wrapper.find({ value: '1' }).find('input');
+		let cb2 = wrapper.find({ value: '2' }).find('input');
+		let cb3 = wrapper.find({ value: '3' }).find('input');
+		// Check if the first option alone is selected initially
+		expect(cb1).to.have.prop('checked', true);
+		expect(cb2).to.have.prop('checked', false);
+		expect(cb3).to.have.prop('checked', false);
+		// Click on each of the checkbox options
+		cb1.simulate('change');
+		cb2.simulate('change');
+		cb3.simulate('change');
+		// Get the checkbox options again after the update
+		cb1 = wrapper.find({ value: '1' }).find('input');
+		cb2 = wrapper.find({ value: '2' }).find('input');
+		cb3 = wrapper.find({ value: '3' }).find('input');
+		// Check if all options except first option is selected
+		expect(cb1).to.have.prop('checked', false);
+		expect(cb2).to.have.prop('checked', true);
+		expect(cb3).to.have.prop('checked', true);
 	});
 });
