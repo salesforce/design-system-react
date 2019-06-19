c82d2f1d9ed5309e15c4226d809f9024718eedd6
Pulkit Gupta <pulkonet@gmail.com>
2019-06-19 22:54:32 +0530

    TrialBar: exposed subcomponents

--- components/index.js
+++ components/index.js
@@ -312,6 +312,11 @@ export Tree from './tree';
 export SLDSTrialBar from './trial-bar';
 export TrialBar from './trial-bar';
 
+export SLDSTrialBarDropdown from './trial-bar/dropdown';
+export TrialBarDropdown from './trial-bar/dropdown';
+export SLDSTrialBarButton from './trial-bar/button';
+export TrialBarButton from './trial-bar/button';
+
 export SLDSVerticalNavigation from './vertical-navigation';
 export VerticalNavigation from './vertical-navigation';
 
