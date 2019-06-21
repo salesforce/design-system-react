aef3d501e9fb9147c1c4cb40af793248b77f0f0c
Stephen James <sjames@salesforce.com>
2019-06-21 15:39:49 -0600

    Refactor Story-based Jest Test Suite
    * Tests are done by default. Components must be excluded, not included.
    * Now there is only one storybook build. There used to be two.
    * Remove story-based-tests.js and uses tests/exclude-story-config.js instead.
    * Refactors NPM test scripts so that DOM snapshot tests can be tested in isolation since they are crazy fast.
    * Image Snapshots take a few minues. a11y tests take 10 minutes, but themselves, but adds them to the library’s CI.
    * Adds a11y tests for components that currently pass, in order prevent regression in the future.

---
+++ tests/story-based-tests.dom-snapshot-test.js
@@ -0,0 +1,47 @@
+/*
+ * STORY-BASED DOM SNAPSHOT TESTING
+ *
+ * This uses StoryShots to iterate over stories created in Storybook to
+ * automatically create DOM and image snapshot tests.
+ *
+ * For more information, please visit:
+ * https://github.com/storybooks/storybook/tree/master/addons/storyshots
+ */
+// Ran in Jest Node environment
+
+import initStoryshots, {
+	multiSnapshotWithOptions,
+} from '@storybook/addon-storyshots';
+
+// Add tests to this file to exclude them from testing
+import excludeFromTests from './exclude-story-config';
+import { getExcludeKindRegex, skipStoryShotTest } from './storyshots-helpers';
+
+console.log(`
+★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
+QUEUEING: STORY-BASED DOM SNAPSHOT TESTING
+★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★
+
+EXECUTING /tests/story-based-tests.snapshot-test.js
+BASED ON STORYBOOK STORIES FOUND IN /components/storybook-stories.js
+
+This script uses Jest to call Storyshots 
+https://github.com/storybooks/storybook/tree/next/addons/storyshots on each 
+Storybook story found at http://localhost:9001. This stores a copy of the 
+DOM on load in a text file. If you need an open menu tested, then you will 
+need to open the menu with the \`isOpen\` prop.
+
+For more information, please review: https://github.com/salesforce/design-system-react/blob/master/tests/README.md
+`);
+
+// Create DOM snapshot tests from Storybook stories
+initStoryshots({
+	configPath: '.storybook',
+	storyNameRegex: new RegExp(`^((?!.*?(${skipStoryShotTest})).)*$`, 'g'),
+	storyKindRegex: getExcludeKindRegex({
+		arrayOfStoryKind: excludeFromTests.domSnapshot.storyKind,
+	}),
+	suite: 'DOM snapshots',
+	integrityOptions: { cwd: __dirname }, // start searching from the current directory
+	test: multiSnapshotWithOptions({}),
+});
