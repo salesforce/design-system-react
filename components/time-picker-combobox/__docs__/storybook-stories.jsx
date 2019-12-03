707f68920848cfe9e843ec101eb147719ab02287
Lee White <leewhite@salesforce.com>
2019-12-02 16:26:45 -0800

    removed unused imports

--- components/time-picker-combobox/__docs__/storybook-stories.jsx
+++ components/time-picker-combobox/__docs__/storybook-stories.jsx
@@ -1,14 +1,11 @@
 /* eslint-disable indent */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import IconSettings from '../../icon-settings';
 import { TIME_PICKER_COMBOBOX } from '../../../utilities/constants';
-import TimepickerCombobox from '../../time-picker-combobox';
 
 import Default from '../__examples__/default';
 
-const getTimepicker = (props) => <TimepickerCombobox {...props} />;
 storiesOf(TIME_PICKER_COMBOBOX, module)
 	.addDecorator((getStory) => (
 		<div className="slds-p-around_medium">
