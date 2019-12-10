5926a451046cf7bc624e6277eac166cc85283550
Lee White <leewhite@salesforce.com>
2019-12-10 13:24:15 -0800

    redid optional prop tests so not dependent on each other

--- components/time-picker-combobox/__tests__/time-picker-combobox.browser-test.jsx
+++ components/time-picker-combobox/__tests__/time-picker-combobox.browser-test.jsx
@@ -11,8 +11,8 @@ import { mount } from 'enzyme';
  */
 
 import {
-  createMountNode,
-  destroyMountNode,
+	createMountNode,
+	destroyMountNode,
 } from '../../../tests/enzyme-helpers';
 
 import TimepickerCombobox from '..';
@@ -21,81 +21,153 @@ import IconSettings from '../../../components/icon-settings';
 chai.use(chaiEnzyme());
 
 const propTypes = {
-  initialDate: PropTypes.instanceOf(Date),
-  stepInMinutes: PropTypes.number,
+	formatter: PropTypes.func,
+	initialDate: PropTypes.instanceOf(Date),
+	stepInMinutes: PropTypes.number,
 };
 
 class DemoComponent extends React.Component {
-  render() {
-    return (
-      <IconSettings iconPath="/assets/icons">
-        <TimepickerCombobox {...this.props} />
-      </IconSettings>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { selection: [] };
+	}
+
+	render() {
+		return (
+			<IconSettings iconPath="/assets/icons">
+				<TimepickerCombobox {...this.props}
+					id="test-timepicker"
+					isOpen
+					events={{
+						onSelect: (event, data) => {
+							console.log('onSelect ', data);
+							this.setState({
+								selection: data.selection,
+							});
+						},
+					}} />
+			</IconSettings>
+		);
+	}
 }
 
 DemoComponent.displayName = 'DemoComponent';
 DemoComponent.propTypes = propTypes;
 
+const customFormatter = (date) => {
+	if (date) {
+		return date.toLocaleTimeString('en', {
+			hour: 'numeric',
+			minute: 'numeric',
+		});
+	}
+	return null;
+}
+
+const getCustomOptions = () => {
+	const options = [];
+	const baseDate = new Date();
+	baseDate.setHours(0);
+	baseDate.setMinutes(0);
+	baseDate.setSeconds(0);
+	baseDate.setMilliseconds(0);
+
+	const curDate = new Date(baseDate);
+	let index = 0;
+
+	while (baseDate.getDate() === curDate.getDate()) {
+		const formatted = customFormatter(curDate);
+
+		options.push({
+			id: index.toString(),
+			label: formatted,
+			value: curDate,
+		});
+
+		curDate.setMinutes(curDate.getMinutes() + Math.random() * 59 + 1);
+		index += 1;
+	}
+	return options;
+}
+
+const resetTime = (date) => {
+	date.setHours(1);
+	date.setMinutes(0);
+	date.setSeconds(0);
+	date.setMilliseconds(0);
+	return date;
+}
+
 describe('Timepicker Combobox: ', function describeFunction() {
-  describe('Default', () => {
-    let mountNode;
-    let wrapper;
+	let mountNode;
+	let wrapper;
+	describe('Default', () => {
+		beforeEach(() => {
+			mountNode = createMountNode({ context: this });
+			wrapper = mount(<DemoComponent />, { attachTo: mountNode });
+		});
 
-    beforeEach(() => {
-      mountNode = createMountNode({ context: this });
-      wrapper = mount(<DemoComponent />, { attachTo: mountNode });
-    });
+		afterEach(() => {
+			destroyMountNode({ wrapper, mountNode });
+		});
 
-    afterEach(() => {
-      destroyMountNode({ wrapper, mountNode });
-    });
+		it('renders a timepicker', () => {
+			const timepicker = wrapper.find(TimepickerCombobox);
+			expect(timepicker).to.be.present;
+		});
 
-    it('renders a timepicker', () => {
-      const timepicker = wrapper.find(TimepickerCombobox);
-      expect(timepicker).to.be.present;
-    });
+		it('generates default number of options', () => {
+			const listElements = wrapper.find('.slds-listbox__option');
+			expect(listElements).to.have.lengthOf(48);
+		});
+	});
 
-    it('generates default number of options', () => {
-      const timepicker = wrapper.find(TimepickerCombobox);
-      expect(timepicker.state('options')).to.have.lengthOf(48);
-    });
-  });
+	describe('Optional Props', () => {
+		beforeEach(() => {
+			mountNode = createMountNode({ context: this });
+		});
 
-  describe('Timepicker Optional Props', () => {
-    let mountNode;
-    let wrapper;
+		afterEach(() => {
+			destroyMountNode({ wrapper, mountNode });
+		});
 
-    const futureDateTime = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
-    futureDateTime.setHours(0);
-    futureDateTime.setMinutes(0);
-    futureDateTime.setSeconds(0);
-    futureDateTime.setMilliseconds(0);
+		it('changes the number of options when pass stepInMinutes', () => {
+			wrapper = mount(
+				<DemoComponent stepInMinutes={60} />, { attachTo: mountNode }
+			);
+			const listElements = wrapper.find('.slds-listbox__option');
+			expect(listElements).to.have.lengthOf(24);
+		});
 
-    const customProps = {
-      initialDate: futureDateTime,
-      stepInMinutes: 60,
-    };
+		it('changes the format of options when pass formatter', () => {
+			wrapper = mount(
+				<DemoComponent formatter={customFormatter} />, { attachTo: mountNode }
+			);
+			const baseDate = resetTime(new Date());
+			const formattedDate = customFormatter(baseDate);
+			wrapper.find('#test-timepicker-listbox-option-2').simulate('click');
+			const selectedLabel = wrapper.state('selection')[0].label;
+			expect(selectedLabel).to.equal(formattedDate);
+		});
 
-    beforeEach(() => {
-      mountNode = createMountNode({ context: this });
-      wrapper = mount(<DemoComponent {...customProps} />, { attachTo: mountNode });
-    });
+		it('changes the day when pass initialDate', () => {
+			const futureDate = resetTime(new Date(new Date().getTime() + 24 * 60 * 60 * 1000));
+			wrapper = mount(
+				<DemoComponent initialDate={futureDate} />, { attachTo: mountNode }
+			);
+			wrapper.find('#test-timepicker-listbox-option-2').simulate('click');
+			const selectedTime = wrapper.state('selection')[0].value.getTime();
+			expect(selectedTime).to.equal(futureDate.getTime());
+		});
 
-    afterEach(() => {
-      destroyMountNode({ wrapper, mountNode });
-    });
-
-    it('stepInMinutes changes the number of options', () => {
-      const timepicker = wrapper.find(TimepickerCombobox);
-      expect(timepicker.state('options')).to.have.lengthOf(24);
-    });
-
-    it('initialDate changes the date of the options', () => {
-      const timepicker = wrapper.find(TimepickerCombobox);
-      const firstValue = timepicker.state('options')[0].value;
-      expect(firstValue.getTime()).to.equal(futureDateTime.getTime());
-    });
-  });
+		it('uses custom options when pass options', () => {
+			const customOptions = getCustomOptions();
+			wrapper = mount(<DemoComponent options={customOptions} />, {
+				attachTo: mountNode,
+			});
+			wrapper.find('#test-timepicker-listbox-option-2').simulate('click');
+			const selectedTime = wrapper.state('selection')[0].value.getTime();
+			expect(selectedTime).to.equal(customOptions[2].value.getTime())
+		});
+	});
 });
