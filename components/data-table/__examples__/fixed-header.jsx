073ae9dc4ca57abb9c4ea7aeba091d9e7b27193a
Stephen James <sjames@salesforce.com>
2020-01-16 11:14:43 -0700

    DataTable: Add Infinite Scroll pattern example

--- components/data-table/__examples__/fixed-header.jsx
+++ components/data-table/__examples__/fixed-header.jsx
@@ -6,6 +6,130 @@ import DataTableColumn from '~/components/data-table/column';
 import DataTableCell from '~/components/data-table/cell';
 import DataTableRowActions from '~/components/data-table/row-actions';
 import IconSettings from '~/components/icon-settings';
+import log from '~/utilities/log';
+
+const additionalItems = [
+	{
+		id: '896a6a60b',
+		opportunityName: 'Acme - 1,200 Widgets',
+		accountName: 'Acme',
+		closeDate: '4/10/15',
+		stage: 'Value Proposition',
+		confidence: '70%',
+		amount: '$25,000,000',
+		contact: 'jrogers@acme.com',
+	},
+	{
+		id: '44da9dcdb',
+		opportunityName: 'Acme - 200 Widgets',
+		accountName: 'Acme',
+		closeDate: '1/31/15',
+		stage: 'Prospecting',
+		confidence: '30%',
+		amount: '$5,000,000',
+		contact: 'bob@acme.com',
+	},
+	{
+		id: 'f988a721b',
+		opportunityName: 'salesforce.com - 1,000 Widgets',
+		accountName: 'salesforce.com',
+		closeDate: '1/31/15 3:45PM',
+		stage: 'Id. Decision Makers',
+		confidence: '60%',
+		amount: '$25,000',
+		contact: 'nathan@salesforce.com',
+	},
+	{
+		id: 'd7679cddb',
+		opportunityName: 'Acme - 800 Widgets',
+		accountName: 'Acme',
+		closeDate: '6/11/18',
+		stage: 'Value Proposition',
+		confidence: '85%',
+		amount: '$970,000',
+		contact: 'jrogers@acme.com',
+	},
+	{
+		id: '0085f7a0b',
+		opportunityName: 'Acme - 744 Widgets',
+		accountName: 'Acme',
+		closeDate: '4/15/17',
+		stage: 'Prospecting',
+		confidence: '56%',
+		amount: '$3,110,000',
+		contact: 'bob@acme.com',
+	},
+	{
+		id: 'b7acc778b',
+		opportunityName: 'salesforce.com - 847 Widgets',
+		accountName: 'salesforce.com',
+		closeDate: '5/23/18 1:02PM',
+		stage: 'Id. Decision Makers',
+		confidence: '62%',
+		amount: '$18,000',
+		contact: 'nathan@salesforce.com',
+	},
+	{
+		id: '17991de5b',
+		opportunityName: 'Acme - 1,621 Widgets',
+		accountName: 'Acme',
+		closeDate: '5/16/17',
+		stage: 'Value Proposition',
+		confidence: '70%',
+		amount: '$23,600,000',
+		contact: 'jrogers@acme.com',
+	},
+	{
+		id: '09b2cee9b',
+		opportunityName: 'Acme - 430 Widgets',
+		accountName: 'Acme',
+		closeDate: '6/12/18',
+		stage: 'Prospecting',
+		confidence: '42%',
+		amount: '$4,222,222',
+		contact: 'bob@acme.com',
+	},
+	{
+		id: 'd0035700b',
+		opportunityName: 'salesforce.com - 677 Widgets',
+		accountName: 'salesforce.com',
+		closeDate: '7/21/17 10:11AM',
+		stage: 'Id. Decision Makers',
+		confidence: '42%',
+		amount: '$24,200',
+		contact: 'nathan@salesforce.com',
+	},
+	{
+		id: '4a526f0cb',
+		opportunityName: 'Acme - 1,444 Widgets',
+		accountName: 'Acme',
+		closeDate: '3/18/18',
+		stage: 'Value Proposition',
+		confidence: '66%',
+		amount: '$22,000,000',
+		contact: 'jrogers@acme.com',
+	},
+	{
+		id: '9a5dbdb2b',
+		opportunityName: 'Acme - 320 Widgets',
+		accountName: 'Acme',
+		closeDate: '1/31/18',
+		stage: 'Prospecting',
+		confidence: '36%',
+		amount: '$4,322,000',
+		contact: 'bob@acme.com',
+	},
+	{
+		id: '356dbb52b',
+		opportunityName: 'salesforce.com - 4,000 Widgets',
+		accountName: 'salesforce.com',
+		closeDate: '2/21/17 8:33PM',
+		stage: 'Id. Decision Makers',
+		confidence: '72%',
+		amount: '$15,000',
+		contact: 'nathan@salesforce.com',
+	},
+];
 
 const CustomDataTableCell = ({ children, ...props }) => (
 	<DataTableCell title={children} {...props}>
@@ -25,6 +149,7 @@ class Example extends React.Component {
 	static displayName = 'DataTableExample';
 
 	state = {
+		additionalItemsAdded: false,
 		sortColumn: 'opportunityName',
 		sortColumnDirection: {
 			opportunityName: 'asc',
@@ -163,6 +288,24 @@ class Example extends React.Component {
 		console.log(item, action);
 	};
 
+	handleScroll = (event, data) => {
+		log({
+			action: this.props.action,
+			event,
+			eventName: 'Tree scrolled',
+			data,
+		});
+
+		if (data.percentage > 75 && !this.state.additionalItemsAdded) {
+			this.setState((state) => {
+				return {
+					items: [].concat(state.items, additionalItems),
+					additionalItemsAdded: true,
+				};
+			});
+		}
+	};
+
 	handleSort = (sortColumn, ...rest) => {
 		if (this.props.log) {
 			this.props.log('sort')(sortColumn, ...rest);
@@ -222,6 +365,7 @@ class Example extends React.Component {
 						id="DataTableExample-FixedHeaders"
 						onRowChange={this.handleChanged}
 						onSort={this.handleSort}
+						onScrollVertical={this.handleScroll}
 						selection={this.state.selection}
 						selectRows="checkbox"
 					>
