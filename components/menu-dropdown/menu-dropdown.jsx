c9e6e28d32ee50a53d508edfdb708ddef0e0b675
Medha Srivastava <80929416+srivastava-medha@users.noreply.github.com>
2021-03-22 05:22:14 -0700

    Fix props passed to getNavigableItems for menu dropdown

--- components/menu-dropdown/menu-dropdown.jsx
+++ components/menu-dropdown/menu-dropdown.jsx
@@ -460,7 +460,7 @@ class MenuDropdown extends React.Component {
 		}
 
 		if (!isEqual(prevProps.options, this.props.options)) {
-			this.navigableItems = getNavigableItems(prevProps.options);
+			this.navigableItems = getNavigableItems(this.props.options);
 		}
 	}
 
