7d2ef3ba3ae188050f0965c683467145ae9519b9
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2021-03-29 17:32:25 +0200

    fix(menu-dropdown): selection state
    The selection state was using the previous props.

--- components/menu-dropdown/menu-dropdown.jsx
+++ components/menu-dropdown/menu-dropdown.jsx
@@ -450,7 +450,7 @@ class MenuDropdown extends React.Component {
 
 	componentDidUpdate(prevProps) {
 		if (prevProps.value !== this.props.value) {
-			const nextState = this.getCurrentSelectedIndices(prevProps);
+			const nextState = this.getCurrentSelectedIndices(this.props);
 			// eslint-disable-next-line react/no-did-update-set-state
 			this.setState(nextState);
 		}
