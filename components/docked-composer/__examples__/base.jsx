fb2e424dfd1adfb8e9924a51e9332e48dc9325a8
Stephen James <sjames@salesforce.com>
2023-08-17 15:48:45 -0600

    Update Docked Composer doc examples for website (#3130)
    * Update docked composer doc examples
    
    * Add Icon path
    
    * bump package
    
    * update snapshot

--- components/docked-composer/__examples__/base.jsx
+++ components/docked-composer/__examples__/base.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-console, react/prop-types */
 import React from 'react';
 
+import IconSettings from '~/components/icon-settings';
 import DockedComposer from '~/components/docked-composer';
-import Accordion from '~/components/accordion/__examples__/base';
 
 class Example extends React.Component {
 	static displayName = 'DockedComposerExample';
 
-	state = {
-		isExpanded: true,
-		isOpen: true,
-	};
+	constructor() {
+		super();
+		this.state = {
+			isExpanded: true,
+			isOpen: true,
+		};
+	}
 
 	handleMinimize = (event, data) => {
 		this.setState({ isExpanded: false });
@@ -26,19 +29,27 @@ class Example extends React.Component {
 
 	render() {
 		return this.state.isOpen ? (
-			<DockedComposer
-				header="What's New this Release?"
-				body={<Accordion />}
-				id="docked-composer"
-				events={{
-					onRequestMinimize: this.handleMinimize,
-					onRequestExpand: this.handleExpand,
-					onRequestClose: this.handleClose,
-				}}
-				isOpen={this.state.isExpanded}
-			/>
+			<IconSettings iconPath="/assets/icons">
+				<div
+					style={{
+						height: '500px',
+						position: 'relative',
+					}}
+				>
+					<DockedComposer
+						header="What's New this Release?"
+						id="docked-composer"
+						events={{
+							onRequestMinimize: this.handleMinimize,
+							onRequestExpand: this.handleExpand,
+							onRequestClose: this.handleClose,
+						}}
+						isOpen={this.state.isExpanded}
+					/>
+				</div>
+			</IconSettings>
 		) : null;
 	}
 }
 
-export default Example;
+export default Example; // export is replaced with `ReactDOM.render(<Example />, mountNode);` at runtime
