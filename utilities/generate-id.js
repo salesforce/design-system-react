becb77f155aee8bee94bdec75d8afc6412747e9f
Stephen James <sjames@salesforce.com>
2025-01-10 15:47:17 -0700

    Add nanoid, remove deprecated shortId (#3170)
    fixing test issues
    
    Co-authored-by: Sanal Panicker <dev.nerd87@gmail.com>

--- utilities/generate-id.js
+++ utilities/generate-id.js
@@ -1,4 +1,4 @@
-import shortid from 'shortid';
+import { nanoid } from 'nanoid';
 
 /**
  * Generate unique ID that is also a valid CSS identifier.
@@ -6,24 +6,23 @@ import shortid from 'shortid';
  */
 function generateId() {
 	/**
-	 * `shortid.generate()` by itself will generates IDs that
+	 * `nanoid()` by itself will generates IDs that
 	 * are not always valid [1] CSS identifiers (e.g. will start
 	 * with a digit). So, when other parts of the codebase will
 	 * use those IDs as a selector, a `SyntaxError`² will be thrown
 	 * (i.e. `'#...' is not a valid selector`).
 	 *
 	 * The `slds-` prefix is used so to keep things simple.
-	 * `shortid.characters()` requires "a string of all 64 unique
+	 * `nanoid()` requires "a string of all 64 unique
 	 * characters"³, which means that in order to generate valid
 	 * CSS identifiers a-z, A-Z, and some "ISO 10646 characters
 	 * U+00A0 and higher"¹ will need to be included.
 	 *
 	 * [1] https://www.w3.org/TR/CSS21/syndata.html#value-def-identifier
 	 * [2] https://dom.spec.whatwg.org/#selectors
-	 * [3] https://github.com/dylang/shortid/blob/937ce2c8dd7001baec1785cb8dce6e6fe1bcf61f/README.md#shortidcharactersstring
 	 */
 
-	return `slds-${shortid.generate()}`;
+	return `slds-${nanoid()}`;
 }
 
 export default generateId;
